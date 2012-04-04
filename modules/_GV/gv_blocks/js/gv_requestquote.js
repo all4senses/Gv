a0aa9b675caefa02756b358eeaf20e8adb141114
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-04 20:14:50 +0300

    Request a quote

--- modules/_GV/gv_blocks/js/gv_requestquote.js
+++ modules/_GV/gv_blocks/js/gv_requestquote.js
@@ -10,11 +10,10 @@
           validationOptions: {
 						messages: {
               surname: "Please specify your nameeee",
-              myemail: "Please specify your nameeee"
-//              email: {
-//                required: "Weeeee need your email address to contact you",
-//                email: "Your email address must be in the format of name@domain.com"
-//              }
+              myemail: {
+                required: "Weeeee need your email address to contact you",
+                email: "Your email address must be in the format of name@domain.com"
+              }
             }
           },
 				 	formOptions :{
