668bbc395b19890179083b0b76d5269e194c7cf9
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-04 19:57:18 +0300

    Request a quote

--- modules/_GV/gv_blocks/js/gv_requestquote.js
+++ modules/_GV/gv_blocks/js/gv_requestquote.js
@@ -5,8 +5,17 @@
        
         $("#demoForm").formwizard({ 
 				 	formPluginEnabled: true,
-				 	validationEnabled: false,
+				 	validationEnabled: true,
 				 	focusFirstInput : true,
+          validationOptions :{
+						messages: {
+              name: "Please specify your nameeee",
+              email: {
+                required: "Weeeee need your email address to contact you",
+                email: "Your email address must be in the format of name@domain.com"
+              }
+            }
+          },
 				 	formOptions :{
 						success: function(data){$("#status").fadeTo(500,1,function(){ $(this).html("You are now registered!").fadeTo(5000, 0); })},
 						beforeSubmit: function(data){$("#data").html("data sent to the server: " + $.param(data));},
