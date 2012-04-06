ffc47084000c6c3e65683ea45edd9dc2fe9c17f2
all4senses <all4senses@gmail.com>
2012-04-06 06:01:25 +0300

    Submit a review

--- modules/_GV/gv_blocks/js/gv_requestquote.js
+++ modules/_GV/gv_blocks/js/gv_requestquote.js
@@ -135,7 +135,7 @@
           },
 				 	formOptions :{
 						//success: function(data){alert('Success!'); $("#status").fadeTo(50,1,function(){ $(this).html("You are now registered!").fadeTo(5000, 0); })},
-            success: function(data){$('#requestQuoteFormWrapper .sending').hide('clip'); $("#requestQuoteFormWrapper .success").show('clip');},
+            success: function(data){$('#requestQuoteFormWrapper .sending').hide('clip'); $("#requestQuoteFormWrapper .success").append(data.data); $("#requestQuoteFormWrapper .success").show('clip');},
 						beforeSubmit: function(data){$('#requestQuoteFormWrapper .multipartForm').hide('clip'); $("#requestQuoteFormWrapper .sending").append('Data is sendingt: ' + $.param(data)); $("#requestQuoteFormWrapper .sending").show('clip'); },//function(data){$("#data").html("data sent to the server: " + $.param(data));},
             dataType: 'json',
 						resetForm: true
