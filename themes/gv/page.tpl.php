a4c1e28e089d28c1c64d20e4b2773c3042a8f7da
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-23 16:53:30 +0300

    Theming main page

--- themes/gv/page.tpl.php
+++ themes/gv/page.tpl.php
@@ -19,34 +19,44 @@
     <?php ////if ($breadcrumb): print $breadcrumb; endif;?>
   </header> <!-- /#header -->
 
-  <section id="main" role="main" class="clearfix">
-    <?php print $messages; ?>
-    <a id="main-content"></a>
-    <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
-    <?php print render($page['help']); ?>
-    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-    <?php print render($page['content']); ?>
-  </section> <!-- /#main -->
+  <div id="all-content">
+    
+      <section id="main" role="main" class="clearfix">
+
+          <?php print $messages; ?>
+          <a id="main-content"></a>
+          <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <?php print render($page['content']); ?>
+
+      </section> <!-- /#main -->
+
+
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
+
+      <?php if ($page['sidebar_second']): ?>
+        <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+
+  </div> <!-- /#content -->
   
-  <?php if ($page['sidebar_first']): ?>
-    <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
-      <?php print render($page['sidebar_first']); ?>
-    </aside>  <!-- /#sidebar-first -->
-  <?php endif; ?>
-
-  <?php if ($page['sidebar_second']): ?>
-    <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
-      <?php print render($page['sidebar_second']); ?>
-    </aside>  <!-- /#sidebar-second -->
-  <?php endif; ?>
-
+  
   <footer id="footer" role="contentinfo" class="clearfix">
-    <?php print render($page['footer']) ?>
-    <?php print $feed_icons ?>
+    <div class="inside">
+      <?php print render($page['footer']) ?>
+      <?php print $feed_icons ?>
+    </div> <!-- /.inside -->
   </footer> <!-- /#footer -->
 
 </div> <!-- /#container -->
