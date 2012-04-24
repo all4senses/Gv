43c73b2a7d186bb89c96aa11c4af23d7ac93ad8e
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-24 16:27:05 +0300

    Theming main page

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -97,7 +97,7 @@ function gv_preprocess_block(&$variables, $hook) {
   if ($variables['block_html_id'] == 'block-system-main') {
     $variables['theme_hook_suggestions'][] = 'block__bare';
   }
-  dpm($variables);
+//dpm($variables);
   $variables['title_attributes_array']['class'][] = 'block-title';
 }
 
