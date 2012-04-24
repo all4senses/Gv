dd82f92534356eaf4f61bfee2a6033f515a45dbe
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-24 16:31:47 +0300

    Theming main page

--- themes/gv/block.tpl.php
+++ themes/gv/block.tpl.php
@@ -2,6 +2,9 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
+    <?php if ($block->region == 'sidebar_first' || $block->region == 'sidebar_second'): ?>
+      <div class="block-icon pngfix"></div>
+    <?php endif;?>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif;?>
   <?php print render($title_suffix); ?>
