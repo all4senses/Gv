51ba4b14ba3531a4928c2870df9c21ded7186d11
all4senses <all4senses@gmail.com>
2012-04-25 03:07:44 +0300

    Theming home page

--- themes/gv/page.tpl.php
+++ themes/gv/page.tpl.php
@@ -38,6 +38,7 @@
           <?php print render($title_prefix); ?>
           <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
           <?php print render($title_suffix); ?>
+          
           <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
           <?php print render($page['help']); ?>
           <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
