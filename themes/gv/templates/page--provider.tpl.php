126c4198132f8ce9f11008db65dc0423e360b167
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-25 18:27:47 +0300

    Theming main page

--- themes/gv/templates/page--provider.tpl.php
+++ themes/gv/templates/page--provider.tpl.php
@@ -1,4 +1,4 @@
-<div id="container" class="clearfix">
+<!-- <div id="container" class="clearfix"> -->
 
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
@@ -10,43 +10,65 @@
   <header id="header" role="banner" class="clearfix">
 
     <nav id="navigation" role="navigation" class="clearfix">
-      
-      <div id="header-menu-back"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo"></a></div>
-
+      <div id="header-menu-back"></div>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo"></a>
       <?php print render($page['header']); ?>
     </nav> <!-- /#navigation -->
 
     <?php ////if ($breadcrumb): print $breadcrumb; endif;?>
   </header> <!-- /#header -->
 
-  <section id="main" role="main" class="clearfix">
-    <?php print $messages; ?>
-    <a id="main-content"></a>
-    <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
-    <?php print render($page['help']); ?>
-    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-    <?php print render($page['content']); ?>
-  </section> <!-- /#main -->
   
-  <?php if ($page['sidebar_first']): ?>
-    <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
-      <?php print render($page['sidebar_first']); ?>
-    </aside>  <!-- /#sidebar-first -->
+  <?php if ($page['highlighted']): ?>
+    <section id="highlighted" class="clearfix">
+      <?php print render($page['highlighted']); ?>
+    </section>
   <?php endif; ?>
+  
+  
+  <div id="all-content" class="clearfix">
+      
+      
+    
+      <section id="main" role="main" class="clearfix">
 
-  <?php if (!$page['sidebar_second']): ?>
-    <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
-      <?php print render($page['sidebar_second']); ?>
-    </aside>  <!-- /#sidebar-second -->
-  <?php endif; ?>
+          <?php print $messages; ?>
+          <a id="main-content"></a>
+          
+          <?php if (!$is_front && $title): ?>
+            <?php print render($title_prefix); ?>
+            <h1 class="title" id="page-title"><?php print $title; ?></h1>
+            <?php print render($title_suffix); ?>
+          <?php endif; ?>
+          
+          <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <?php print render($page['content']); ?>
 
+      </section> <!-- /#main -->
+
+
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
+
+      <?php if (!$page['sidebar_second']): ?>
+        <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+
+  </div> <!-- /#all-content -->
+  
+  
   <footer id="footer" role="contentinfo" class="clearfix">
-    <?php print render($page['footer']) ?>
-    <?php print $feed_icons ?>
+    <div id="footer-inside">
+      <?php print render($page['footer']) ?>
+      <?php print $feed_icons ?>
+    </div> <!-- /.inside -->
   </footer> <!-- /#footer -->
 
-</div> <!-- /#container -->
+<!-- </div> --> <!-- /#container -->
