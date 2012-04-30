6ab5cb01dee6d11c9d4b651b4c0b6b816d71e4c5
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-30 20:12:10 +0300

    Submit review stars

--- modules/_GV/gv_pages/js/gv_review_stars.js
+++ modules/_GV/gv_pages/js/gv_review_stars.js
@@ -2,17 +2,33 @@
 
   Drupal.behaviors.gv_review_stars = {
     attach: function (context, settings) {
-       //$caption = $("<span/>");
-       $('.form-item-rating-features').stars({
+
+        $('.form-item-rating-features').stars({
           inputType: "select",
           captionEl: $('#edit-rating-features-choice'),
-          //captionEl: $caption,
-          //captionEl: 'edit-rating-features-choice',
           cancelShow: false
         });
-       //$caption.appendTo(".form-item-rating-features");
-       console.log('Review stars!');
-       
+        
+        $('.form-item-rating-sound').stars({
+          inputType: "select",
+          captionEl: $('#edit-rating-sound-choice'),
+          cancelShow: false
+        });
+        $('.form-item-rating-rely').stars({
+          inputType: "select",
+          captionEl: $('#edit-rating-rely-choice'),
+          cancelShow: false
+        });
+        $('.form-item-rating-money').stars({
+          inputType: "select",
+          captionEl: $('#edit-rating-money-choice'),
+          cancelShow: false
+        });
+        $('.form-item-rating-service').stars({
+          inputType: "select",
+          captionEl: $('#edit-rating-service-choice'),
+          cancelShow: false
+        });
     
     }
   };
