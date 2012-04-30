880b7d664c54ff18d8a3ba80ab77aca6562eb3c2
Alex Khrapovitskiy <all4senses@gmail.com>
2012-04-30 20:01:06 +0300

    Submit review stars

--- modules/_GV/gv_pages/js/gv_review_stars.js
+++ modules/_GV/gv_pages/js/gv_review_stars.js
@@ -2,15 +2,15 @@
 
   Drupal.behaviors.gv_review_stars = {
     attach: function (context, settings) {
-       $caption = $("<span/>");
+       //$caption = $("<span/>");
        $('.form-item-rating-features').stars({
           inputType: "select",
-          //captionEl: $('#edit_rating_features_choice'),
-          captionEl: $caption,
+          captionEl: $('#edit-rating-features-choice'),
+          //captionEl: $caption,
           //captionEl: 'edit-rating-features-choice',
           cancelShow: false
         });
-       $caption.appendTo(".form-item-rating-features");
+       //$caption.appendTo(".form-item-rating-features");
        console.log('Review stars!');
        
     
