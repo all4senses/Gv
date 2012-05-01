09de007b5efeadcc40e1b459bf328094fe4731ae
all4senses <all4senses@gmail.com>
2012-05-02 00:18:12 +0300

    Submit review page

--- modules/_GV/gv_pages/js/gv_review_fieldHints.js
+++ modules/_GV/gv_pages/js/gv_review_fieldHints.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.gv_review_fieldHints = {
     attach: function (context, settings) {
       
-      $('input[id="edit-pros"], input[id="edit-cons"]').hint();
+      $('input[id="edit-field-r-fname"], input[id="edit-field-r-lname"], input[id="edit-field-r-email"], input[id="edit-pros"], input[id="edit-cons"]').hint();
     
     }
   };
