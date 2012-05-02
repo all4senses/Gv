3a14abb9d8f6122483d0d61ce7f64377bca07569
all4senses <all4senses@gmail.com>
2012-05-02 19:31:36 +0300

    Submit a review

--- themes/gv/templates/page--provider.tpl.php
+++ themes/gv/templates/page--provider.tpl.php
@@ -55,7 +55,7 @@
         </aside>  <!-- /#sidebar-first -->
       <?php endif; ?>
 
-      <?php if (!$page['sidebar_second']): ?>
+      <?php if ($page['sidebar_second']): ?>
         <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
           <?php print render($page['sidebar_second']); ?>
         </aside>  <!-- /#sidebar-second -->
