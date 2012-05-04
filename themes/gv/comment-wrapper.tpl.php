b1815ca716ecc3469b740a92a0c71eabc9e3e4d5
Alex Khrapovitskiy <all4senses@gmail.com>
2012-05-04 11:17:28 +0300

    Article page

--- themes/gv/comment-wrapper.tpl.php
+++ themes/gv/comment-wrapper.tpl.php
@@ -5,14 +5,12 @@
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
-   <?php if ($content['comment_form']): ?>
+  <?php print render($content['comments']); ?>
+
+  <?php if ($content['comment_form']): ?>
     <section id="comment-form-wrapper">
       <h2 class="title"><?php print t('Add new comment'); ?></h2>
       <?php print render($content['comment_form']); ?>
     </section> <!-- /#comment-form-wrapper -->
   <?php endif; ?>
-    
-  <?php print render($content['comments']); ?>
-
-  
 </section> <!-- /#comments -->
