5cfe7441e4a4ebdf494ec7f3b9d30a310f736cc7
Alex Khrapovitskiy <all4senses@gmail.com>
2012-05-04 07:18:51 +0300

    Article page

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -85,7 +85,21 @@
           <?php $url = 'http://getvoip.com'. url('node/' . $node->nid); ?>
           
           <div class="others">
-            
+            <!-- ADDTHIS BUTTON BEGIN -->
+            <script type="text/javascript">
+            var addthis_config = {
+                pubid: "all4senses"
+            }
+            </script>
+
+            <a href="http://www.addthis.com/bookmark.php?v=250" 
+                    class="addthis_button"><img 
+                    src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" 
+                    width="125" height="16" border="0" alt="Share" /></a>
+
+            <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
+            <!-- ADDTHIS BUTTON END -->
+
           </div>
           
           <div class="main">
