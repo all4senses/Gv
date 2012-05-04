64a87847e1644f705aac81265d65d6e7494bc33e
Alex Khrapovitskiy <all4senses@gmail.com>
2012-05-04 03:52:13 +0300

    Article page

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -3,72 +3,81 @@
   <div class="inside">
 <?php endif; ?>
 
-  <?php if ($user_picture || $display_submitted || !$page): ?>
-    
-    <?php if (!$page): ?>
-      <header>
-    <?php endif; ?>
+ 
+  <div class="main-content">
+ 
+          
+      <?php if ($user_picture || $display_submitted || !$page): ?>
 
-      <?php print $user_picture; ?>
-  
-        
-        
-        
-      <?php print render($title_prefix); ?>
-      <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>>
-          <?php if (!isset($node->title_no_link)): ?>
-            <a href="<?php print $node_url; ?>">
-              <?php print $title; ?>
-            </a>
-          <?php else: ?>
-            <?php print $title; ?>
+        <?php if (!$page): ?>
+          <header>
+        <?php else: ?>
+            <div class="main-content">
+        <?php endif; ?>
+
+          <?php print $user_picture; ?>
+
+
+
+
+          <?php print render($title_prefix); ?>
+          <?php if (!$page): ?>
+            <h2<?php print $title_attributes; ?>>
+              <?php if (!isset($node->title_no_link)): ?>
+                <a href="<?php print $node_url; ?>">
+                  <?php print $title; ?>
+                </a>
+              <?php else: ?>
+                <?php print $title; ?>
+              <?php endif; ?>
+            </h2>
           <?php endif; ?>
-        </h2>
+          <?php print render($title_suffix); ?>
+
+
+
+
+          <?php if ($display_submitted): ?>
+
+            <span class="submitted">
+              <?php 
+                $created_str = '<span class="delim">|</span>' . date('F d, Y \a\t g:sa', $node->created); 
+                print preg_replace('/(<span.*>)(.*)(<a.*a>)(.*)(<\/span>)/', "$1By$3$created_str$5", $submitted);
+                dpm($node);
+                dpm($content);
+              ?>
+            </span>
+
+          <?php endif; ?>
+
+        <?php if (!$page): ?>
+          </header>
+        <?php endif; ?>
+
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-  
-        
-        
-        
-      <?php if ($display_submitted): ?>
-        
-        <span class="submitted">
-          <?php 
-            $created_str = '<span class="delim">|</span>' . date('F d, Y \a\t g:sa', $node->created); 
-            print preg_replace('/(<span.*>)(.*)(<a.*a>)(.*)(<\/span>)/', "$1By$3$created_str$5", $submitted);
-            dpm($node);
-            dpm($content);
-          ?>
-        </span>
-        
+
+      <div class="content"<?php print $content_attributes; ?>>
+        <?php
+          // Hide comments, tags, and links now so that we can render them later.
+          hide($content['comments']);
+          hide($content['links']);
+          hide($content['field_tags']);
+          print render($content);
+        ?>
+      </div>
+
+
+
+      <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
+        <footer>
+          <?php print render($content['field_tags']); ?>
+          <?php print render($content['links']); ?>
+        </footer>
       <?php endif; ?>
 
-    <?php if (!$page): ?>
-      </header>
-    <?php endif; ?>
-    
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // Hide comments, tags, and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      hide($content['field_tags']);
-      print render($content);
-    ?>
-  </div>
-    
+  </div> <!-- main-content -->
+ 
   
-       
-  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
-    <footer>
-      <?php print render($content['field_tags']); ?>
-      <?php print render($content['links']); ?>
-    </footer>
-  <?php endif; ?>
-
   <?php print render($content['comments']); ?>
 
 <?php if (!$page): ?>
