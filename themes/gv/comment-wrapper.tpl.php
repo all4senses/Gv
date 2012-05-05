4171ba47fa45fdc973df72aa75143e52eb581f03
all4senses <all4senses@gmail.com>
2012-05-05 04:16:20 +0300

    Article page

--- themes/gv/comment-wrapper.tpl.php
+++ themes/gv/comment-wrapper.tpl.php
@@ -1,15 +1,20 @@
 <section id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if ($content['comments'] && $node->type != 'forum'): ?>
-    <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Comments'); ?></h2>
-    <?php print render($title_suffix); ?>
-  <?php endif; ?>
 
+  <?php
+  dpm($content['comment_form']);
+  ?>
     <?php if ($content['comment_form']): ?>
     <section id="comment-form-wrapper">
       <h2 class="title"><?php print t('Add new comment'); ?></h2>
       <?php print render($content['comment_form']); ?>
     </section> <!-- /#comment-form-wrapper -->
+  <?php endif; ?>
+
+    
+  <?php if ($content['comments'] && $node->type != 'forum'): ?>
+    <?php print render($title_prefix); ?>
+    <h2 class="title"><?php print t('Comments'); ?></h2>
+    <?php print render($title_suffix); ?>
   <?php endif; ?>
     
   <?php print render($content['comments']); ?>
