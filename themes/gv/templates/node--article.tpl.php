458f1f0db8c3efdffdf41c8fbfa7f50f97eb57d2
all4senses <all4senses@gmail.com>
2012-05-05 04:37:00 +0300

    Article page

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -59,7 +59,7 @@
                 else {
                   print preg_replace('/(<span.*>)(.*)<span(.*)(about=")(.*)(".*)>(.*)<\/span>.*(<\/span>)/', "$1By<a href=" . '"$5"' . "$3$4$5$6>$7</a>$created_str$8", $submitted);
                 }
-                dpm($node);
+                //dpm($node);
                 //dpm($content);
                 //dpm($content['comments']);
               ?>
