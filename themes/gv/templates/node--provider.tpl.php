e2095324f3e289e12d353ae32de6bec66a2d4df5
Alex Khrapovitskiy <all4senses@gmail.com>
2012-05-06 05:11:10 +0300

    Provider page

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -32,41 +32,96 @@ dpm($content);
         <?php endif; ?>
     
 
+    
 
         <div class="content"<?php print $content_attributes; ?>>
+          
           <?php
             // Hide comments, tags, and links now so that we can render them later.
             hide($content['comments']);
             hide($content['links']);
             hide($content['field_tags']);
             hide($content['reviews_entity_view_1']);
+           ?>
+          
+           <?php if ($page): ?>
+             
+              <?php
+                if (isset($content['field_p_logo'][0]['#item']['uri'])) {
+                  echo theme('image_style', array( 'path' =>  $content['field_p_logo'][0]['#item']['uri'], 'style_name' => 'logo_provider_page')); 
+                }
+                else {
+                  echo render($title_prefix), '<h2', $title_attributes,'><a href="', $node_url, '>', $title, '</a></h2>', render($title_suffix);
+                }
+              ?>
+              <div class="basic-info">
+                <div class="title"><?php echo t('Company Contact Info:'); ?></div>
+                <div><?php echo $content['i_availability']['#markup']; ?></div>
+                <div><?php echo $content['i_web']['#markup']; ?></div>
+                <div><?php echo $content['i_founded']['#markup']; ?></div>
+              </div>
+          
+          
+              <div class="share main">
+                <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+                <script type="IN/Share" data-url="<?php echo $url?>" data-counter="right" data-showzero="true"></script>
+
+                <script type="text/javascript">
+                  (function() {
+                    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+                    po.src = 'https://apis.google.com/js/plusone.js';
+                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+                  })();
+                </script>
+                <g:plusone size="medium" href="<?php echo $url?>"></g:plusone>
+
+                <div id="fb-root"></div>
+                <script>(function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=138241656284512";
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
+                <div class="fb-like" data-href="<?php echo $url?>" data-send="false" data-layout="button_count" data-width="80" data-show-faces="false"></div>
+
+                <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url?>">Tweet</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+              </div> <!-- main share buttons -->
+                      
+                      
+                      
+                      
+          
+          <?php endif; ?>  
             
-            
-            echo theme('image_style', array( 'path' =>  $content['field_p_logo'][0]['#item']['uri'], 'style_name' => 'logo_provider_page')); 
+          <?php
             print render($content);
           ?>
-        </div>
+          
+        </div> <!-- content -->
 
+        
         
       <?php if ($page): ?>
     
-                  <footer>
+        <footer>
 
-                    <?php 
-                      if (isset($content['field_topics'])) {
-                        $tags = NULL;
-                        foreach (element_children($content['field_topics']) as $key) {
-                          $tags .= ($tags ? '<div class="delim">|</div>' : '') . l(t($content['field_topics'][$key]['#title']), 'articles/tags/' . str_replace(' ', '-', drupal_strtolower($content['field_topics'][$key]['#title'])));
-                        }
-                        if ($tags) {
-                          echo '<div class="topics"><div class="title">' . t('TAGS:') . '</div>' . $tags . '</div>';
-                        }
-                      }
-                      //print render($content['field_topics']); 
-                      //print render($content['links']);
-                      
-                    ?>
-                  </footer>
+          <?php 
+            if (isset($content['field_topics'])) {
+              $tags = NULL;
+              foreach (element_children($content['field_topics']) as $key) {
+                $tags .= ($tags ? '<div class="delim">|</div>' : '') . l(t($content['field_topics'][$key]['#title']), 'articles/tags/' . str_replace(' ', '-', drupal_strtolower($content['field_topics'][$key]['#title'])));
+              }
+              if ($tags) {
+                echo '<div class="topics"><div class="title">' . t('TAGS:') . '</div>' . $tags . '</div>';
+              }
+            }
+            //print render($content['field_topics']); 
+            //print render($content['links']);
+
+          ?>
+        </footer>
     
       <?php endif; ?>
 
