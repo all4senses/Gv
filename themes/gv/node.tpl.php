72bbe1dce987fed85d09d50e0ca14f4e2448648e
all4senses <all4senses@gmail.com>
2012-05-14 01:45:00 +0300

    Prefaces edit tabs

--- themes/gv/node.tpl.php
+++ themes/gv/node.tpl.php
@@ -3,7 +3,7 @@
     <div class="tabs-wrapper clearfix"><h2 class="element-invisible">Primary tabs</h2><ul class="tabs primary clearfix">
         <li class="active"><a class="active" href="/<?php echo $_GET['q']; ?>">View<span class="element-invisible">(active tab)</span></a></li>
         <li><a href="<?php echo url('node/' . $node->nid . '/edit', array('query' => array('destination' => $_GET['q']))); ?>">Edit</a></li>
-        <li><a href="<?php echo url('node/' . $node->nid . '/devel', array('query' => array('destination' => $_GET['q']))); ?>">Devel</a></li>
+        <!--<li><a href="<?php //echo url('node/' . $node->nid . '/devel', array('query' => array('destination' => $_GET['q']))); ?>">Devel</a></li>-->
       </ul></div>
   <?php endif; ?>
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
