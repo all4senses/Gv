d2d533f090c881181d807963da55e4a9fe303132
Alex Khrapovitskiy <all4senses@gmail.com>
2012-05-23 09:03:01 +0300

    Quote page

--- themes/gv/templates/node--review.tpl.php
+++ themes/gv/templates/node--review.tpl.php
@@ -55,19 +55,19 @@
                 $created_str = date('F d, Y \a\t g:sa', $node->created); 
                 if ($page) 
                   $created_str = '<span class="delim">|</span>' . $created_str;
-                  global $user;
-                  if (/*$user->uid && */$node->uid) {
-                    echo preg_replace('/(<span.*>)(.*)(<a.*a>)(.*)(<\/span>)/', "$1" . t('Reviewer') . ":$3$created_str$5", $submitted);
-                  }
-                  elseif (!$node->uid) {
+//                  global $user;
+//                  if (/*$user->uid && */$node->uid) {
+//                    echo preg_replace('/(<span.*>)(.*)(<a.*a>)(.*)(<\/span>)/', "$1" . t('Reviewer') . ":$3$created_str$5", $submitted);
+//                  }
+//                  elseif (!$node->uid) {
                     //echo preg_replace('/(<span.*>)(.*)(<span.*span>)(.*)(<\/span>)/', "$1" . t('Reviewer') . ": $3 $created_str$5", $submitted);
                     echo preg_replace('/(<span.*>)(.*)(<span.*span>)(.*)(<\/span>)/', "$1" . t('Reviewer') . ": " . $node->field_r_fname['und'][0]['safe_value'] . "$created_str$5", $submitted);
-                  }
-                  // Make a link for an authors profile from just a Name.
-                  else {
-                    //echo preg_replace('/(<span.*>)(.*)<span(.*)(about=")(.*)(".*)>(.*)<\/span>.*(<\/span>)/', "$1" . t('Reviewer') . ":<a href=" . '"$5"' . "$3$4$5$6>$7</a>$created_str$8", $submitted);
-                    echo preg_replace('/(<span.*>)(.*)<span(.*)(about=")(.*)(".*)>(.*)<\/span>.*(<\/span>)/', "$1" . t('Reviewer') . ":<a href=" . '"$5"' . "$3$4$5$6>$7</a>$created_str$8", $submitted);
-                  }
+//                  }
+//                  // Make a link for an authors profile from just a Name.
+//                  else {
+//                    //echo preg_replace('/(<span.*>)(.*)<span(.*)(about=")(.*)(".*)>(.*)<\/span>.*(<\/span>)/', "$1" . t('Reviewer') . ":<a href=" . '"$5"' . "$3$4$5$6>$7</a>$created_str$8", $submitted);
+//                    echo preg_replace('/(<span.*>)(.*)<span(.*)(about=")(.*)(".*)>(.*)<\/span>.*(<\/span>)/', "$1" . t('Reviewer') . ":<a href=" . '"$5"' . "$3$4$5$6>$7</a>$created_str$8", $submitted);
+//                  }
                 
                 //dpm($node);
                 //dpm($content);
