1939946107e1360c5bc6226718a837de26a1c102
all4senses <all4senses@gmail.com>
2012-05-30 00:03:47 +0300

    Quote page

--- themes/gv/templates/node--quote.tpl.php
+++ themes/gv/templates/node--quote.tpl.php
@@ -2,7 +2,7 @@
   //dpm($node);
   //dpm($node->q_data);
   //dpm($content);
-// Hide comments, tags, and links now so that we can render them later.
+  // Hide comments, tags, and links now so that we can render them later.
   hide($content['links']);
   hide($content['field_topics']);
 ?>
@@ -13,11 +13,15 @@
     <h3>Get Personalized Quotes & Compare Companies For Your VoIP Service Needs.</h3>
   </div>
 
+<?php 
+/*
   <!--
   <div id="experian">
     <div class="text">All Vendors are<br>Prescreened by Experian</div><div class="logo"></div>
   </div>
   -->
+*/
+?>
   <div class="partners"><div id="some-logo"></div></div>
   
   <div class="bottom-clear"></div> 
@@ -28,24 +32,7 @@
   <h2>Don't Trust Your Phone System With Just Anyone!</h2>
   <h1<?php print $title_attributes; ?>><?php print $title; ?></h1>
   <div class="left">
-  
-    <!--
-    <label>How many users do you need?</label>
-    <select>
-      <option value="1-3">1-3</option>    
-    </select>
-    <label>What is your immediate need?</label>
-    <select>
-      <option value="Moving to a new office">Moving to a new office</option>    
-    </select>
-    <label>What is your immediate need?</label>
-    <select>
-      <option value="Phones and Serivce">Phones and Serivce</option>    
-    </select>
-    <input type="button" value="Run the free report"/>
-    -->
     <?php echo gv_blocks_get_requestQuoteForPage(); ?>
-  
   </div>
   <div class="center">
     <?php echo render($content['field_q_image']); ?>
