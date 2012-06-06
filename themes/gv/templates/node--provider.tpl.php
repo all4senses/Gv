45752483d8c14bf33867a8bc198af6307f83f80e
all4senses <all4senses@gmail.com>
2012-06-06 17:39:06 +0300

    RDFa

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -103,7 +103,7 @@
               <div class="basic-info" rel="v:itemreviewed">
                 <div typeof="Organization">
                   <div class="caption"><?php echo t('Company !p Info:', array('!p' => '<span property="v:itemreviewed">' . $content['field_p_name'][0]['#markup'] . '</span>')); ?></div>
-                  <div><?php echo '<span class="title">' . t('Headquarters') . ':</span>' . $node->p_data['info']['i_heads']; ?></div>
+                  <div><?php echo '<span class="title">' . t('Headquarters') . ':</span><span property="v:address">' . $node->p_data['info']['i_heads'] . '</span>'; ?></div>
                   <div><?php echo '<span class="title">' . t('Founded In') . ':</span>' . $node->p_data['info']['i_founded']; ?></div>
                   <div><?php echo '<span class="title">' . t('Service Availability') . ':</span>' . $node->p_data['info']['i_availability']; ?></div>
                   <div><?php if (!$node->p_data['info']['i_web_hide']) echo '<span class="title">' . t('Website') . ':</span>' . l( (isset($node->p_data['info']['i_web_display']) && $node->p_data['info']['i_web_display']) ? $node->p_data['info']['i_web_display'] : str_replace(array('http://', 'https://'), '', $node->p_data['info']['i_web']), $node->p_data['info']['i_web']); ?></div>
