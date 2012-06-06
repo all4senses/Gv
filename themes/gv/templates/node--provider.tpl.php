c44cb46106bf96f798d2bae8871270bb17b65ecd
all4senses <all4senses@gmail.com>
2012-06-06 16:55:43 +0300

    RDFa

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -132,7 +132,7 @@
                       <div class="overall title"><?php $content['field_p_name'][0]['#markup'] . ' ' . t('Overall Rated:'); ?></div>
                     </div>
                     <div class="star-big">
-                      <?php echo render($content['gv_rating_overall']) . '<div class="descr">' . t('Out of 5 stars') . '</div>'; ?>
+                      <?php echo /*render($content['gv_rating_overall'])*/ '<div class="count" property="v:rating">' . $node->gv_rating_overall . '</div>' . '<div class="descr">' . t('Out of 5 stars') . '</div>'; ?>
                     </div>
                   </div>
               
