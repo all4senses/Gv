a7f563f59dfaf54fe0ac62af807bc4ba847fe34b
all4senses <all4senses@gmail.com>
2012-06-10 08:17:23 +0300

    Second Quote

--- themes/gv/templates/node--quote--v2.tpl.php
+++ themes/gv/templates/node--quote--v2.tpl.php
@@ -1,27 +1,8 @@
-<?php 
-  //dpm($node);
-  //dpm($node->q_data);
-  //dpm($content);
-  // Hide comments, tags, and links now so that we can render them later.
-  //hide($content['links']);
-  //hide($content['field_topics']);
-?>
-
-
 <div id="gv-logo">
   <div class="logo"></div>
   <h3>Get Personalized Quotes & Compare Companies For Your VoIP Service Needs.</h3>
 </div>
 
-<?php 
-/*
-  <!--
-  <div id="experian">
-    <div class="text">All Vendors are<br>Prescreened by Experian</div><div class="logo"></div>
-  </div>
-  -->
-*/
-?>
 <div class="partners">
   <div id="some-logo"></div>
   <a id="sitelock" href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=getvoip.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/getvoip.com"/></a> 
@@ -31,66 +12,55 @@
 
 
 <div class="content"<?php print $content_attributes; ?>>
-
-  <h2>Don't Trust Your Phone System With Just Anyone!</h2>
-  <h1<?php 
+<?php echo render($content['field_q_image']); ?>
   
   
+  <h1<?php print /*$title_attributes*/preg_replace('/datatype=".*"/', '', $title_attributes); 
+  ?>><?php //print $title; ?>Save Big</h1>
   
-  
-  
-  //http://www.lullabot.com/articles/how-does-rdf-work-drupal-7
-  
-  print /*$title_attributes*/preg_replace('/datatype=".*"/', '', $title_attributes); 
-  
-  
-  
-  
-  
-  
-  ?>><?php //print $title; ?>Compare Companies! Compare Rates.</h1>
-  <div class="left">
+  <div class="sub_header1">By Switching to a Business</div>
+  <div class="sub_header2"> VoIP Phone System That's Right For You </div>
+  <h2> Save Up To 65% Off a Traditional Phone System </h2>
+  <div class="left top">
     <?php 
       if ($_SERVER['REDIRECT_URL'] == '/voip-provider-quotes-final') {
         echo '<div class="quote-final">' . t('<p><strong>Thank you</strong> for taking your time to complete our form. A VoIP Expert will be contacting you shortly to provide you with a personalized VoIP Service quote.</p><p>In the meantime, you can gain a great deal of VoIP information right here at <a href="http://getvoip.com">GetVoIP.com</a></p>') . '</div>';
       }
-      else {
+      else { 
         echo gv_blocks_get_requestQuoteForPage_v2(); 
       }
     ?>
   </div>
   
-  <?php if ($_SERVER['REDIRECT_URL'] == '/voip-provider-quotes-final'): ?>
-    <div class="right-final">
-      <?php 
-        echo '<a href="/">' , render($content['field_q_image']) , '</a>'; 
-        echo '<h2>' , t('GET ACCESS TO THE LARGEST<br/> VOIP SERVICE GUIDE<br/> IN THE NATION!') , '</h2><div class="link">' , l(t('Learn more'), '<front>') , '</div>';
-      ?>
-    </div>
-  <?php else: ?>
+
   
-    <div class="center">
-      <?php echo render($content['field_q_image']); ?>
-      <div class="quote">"We found a VoIP company that offers us more features at a much better price"</div>
-      <div class="author">Rick Miller - Acme Inc</div>
+  <div class="right top">Let Us Pair You With A VoIP Service Provider<br/>That Best Fit Your Business Needs,<br/>At The Lowest Possible Price</div>
+
+  <div class="left bottom">
+    
+    <div>Complete our <strong>2-Step FAST, FREE & EASY</strong> form and a VoIP expert will begin creating a custom quote for your business phone system needs.</div>
+    
+    <div class="options">
+      <div class="option">Unlimited Calling</div>
+      <div class="option">Free Long Distance</div>
+      <div class="option">Leading Industry Reliability</div>
+      <div class="option">Quality VoIP Technology</div>
+      <div class="option">24/7 U.S. Customer Support</div>
+      <div class="option">and much more..</div>
     </div>
-    <div class="right">
-      <div class="options">
-        <div class="option">Top Rated Providers</div>
-        <div class="option">HD Quality Service</div>
-        <div class="option">No Contracts</div>
-        <div class="option">Robust VoIP Services</div>
-        <div class="option">Accredited BBB Companies</div>
-        <div class="option">Unlimited Calling</div>
-        <div class="option">Designed For Business</div>
-      </div>
-      <div id="right-bottom">
-        <div class="image"></div>
-        <div class="links">Get smarter. Read reviews. <br/>Get facts. Save money!</div>
-      </div>
-    </div>
+    
+    <div>Want to Save Money On Your Business VoIP Phone System?</div>
+    <input type="button" value="Request a Quote" />
+    <div>We'll Take Care Of The Rest!</div>
+    
+  </div>
   
-  <?php endif; ?>
+  <div id="right bottom">
+    <a href="#">What is a VoIP Phone System?</a>
+    <a class="bottom" href="#">Advantages of VoIP</a>
+  </div>
+  
+
   
   <?php
     //print render($content);
@@ -104,26 +74,6 @@
   <h2><?php echo t('All Major Manufacturers Supported:'); ?></h2>
   <div id="cisco"></div><div id="polycom"></div><div id="microsoft"></div><div id="att"></div><div id="verizon"></div><div id="comcast"></div><div id="panasonic"></div>
   <div class="bottom-clear"></div> 
-</div>
-
-<div id="bottom">
-  <div class="left">
-    <h3><?php echo $node->q_data['bottom_text']['left_title']; ?></h3>
-    <div class="text"><?php echo $node->q_data['bottom_text']['left_text']; ?></div>
-  </div>
-  <div class="center">
-    <h3><?php echo $node->q_data['bottom_text']['center_title']; ?></h3>
-    <div class="text"><?php echo $node->q_data['bottom_text']['center_text']; ?></div>
-  </div>
-  <div class="right">
-    <h3><?php echo $node->q_data['bottom_text']['right_title']; ?></h3>
-    <div class="text"><?php echo $node->q_data['bottom_text']['right_text']; ?></div>
-  </div>
-  <?php
-    
-  ?>
-  
-  <div class="bottom-clear"></div>
 </div>
 
 <footer>
