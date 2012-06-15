7881c3d48d5857631b7e4398f5add67a5dd395d4
all4senses <all4senses@gmail.com>
2012-06-15 23:50:05 +0300

    Orange Theme

--- themes/gv_orange/templates/block.tpl.php
+++ themes/gv_orange/templates/block.tpl.php
@@ -2,9 +2,6 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <?php if ($title != 'Request a Free Quote' && ($block->region == 'sidebar_first' || $block->region == 'sidebar_second') ): ?>
-      <div class="block-icon pngfix"><?php echo t('All'); ?></div>
-    <?php endif;?>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif;?>
   <?php print render($title_suffix); ?>
