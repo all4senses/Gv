ca84810288f3ec2f87d6186d40d8427538ca32e1
all4senses <all4senses@gmail.com>
2012-06-15 07:02:47 +0300

    Orange Theme

---
+++ themes/gv_orange/templates/block.tpl.php
@@ -0,0 +1,16 @@
+<section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <?php if ($block->region == 'sidebar_first' || $block->region == 'sidebar_second'): ?>
+      <div class="block-icon pngfix"></div>
+    <?php endif;?>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print $content ?>
+  </div>
+  
+</section> <!-- /.block -->
