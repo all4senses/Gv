e8e0582f1f96beed24834d50aa054a9feec191fd
all4senses <all4senses@gmail.com>
2012-06-16 00:06:12 +0300

    Orange Theme

--- themes/gv_orange/templates/block.tpl.php
+++ themes/gv_orange/templates/block.tpl.php
@@ -1,6 +1,6 @@
 
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-<?php dpm($block); ?>
+<?php dpm(get_defined_vars()); ?>
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
