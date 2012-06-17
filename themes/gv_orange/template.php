7721bc639a7885fdf7d63ef9919ecbb0fd843780
all4senses <all4senses@gmail.com>
2012-06-17 06:10:04 +0300

    Orange Theme

--- themes/gv_orange/template.php
+++ themes/gv_orange/template.php
@@ -60,10 +60,14 @@ function gv_orange_process_html(&$vars) {
 /**
  * Override or insert variables into the page templates.
  */
-/* -- Delete this line if you want to use these functions
+
 function gv_orange_preprocess_page(&$vars) {
+
+dpm($vars);
+
 }
 
+/* -- Delete this line if you want to use these functions
 function gv_orange_process_page(&$vars) {
 }
 // */
