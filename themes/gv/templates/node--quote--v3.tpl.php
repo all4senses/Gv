437d276882b29df812e10b5e2402c4c451bd242f
U-FLYSMALL\Alex <all4senses@gmail.com>
2012-07-04 09:58:00 +0300

    Third Quote

--- themes/gv/templates/node--quote--v3.tpl.php
+++ themes/gv/templates/node--quote--v3.tpl.php
@@ -55,9 +55,9 @@
       <div class="right">
         <div id="right-up">
           <h2>Compare VoIP Providers Tailored to Your Needs</h2>
-          <div class="explain"><span class="number">1</span>Tell us about your VoIP needs below</div>
-          <div class="explain"><span class="number">2</span>Your request will be reviewed by our expert team of VoIP researchers</div>
-          <div class="explain"><span class="number">3</span>We will connect you with three providers that best suit your needs</div>
+          <div class="explain"><div class="number">1</div><span class="text">Tell us about your VoIP needs below</span></div>
+          <div class="explain"><div class="number">2</div><span class="text">Your request will be reviewed by our expert team of VoIP researchers</span></div>
+          <div class="explain"><div class="number">3</div><span class="text">We will connect you with three providers that best suit your needs</span></div>
           <div class="bottom-clear"></div> 
         </div>
         <div id="right-up-image"></div>
