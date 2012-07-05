9b346981fdf7ac18cf479f72b35024912e980c17
U-FLYSMALL\Alex <all4senses@gmail.com>
2012-07-05 16:35:54 +0300

    Third Quote

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -201,7 +201,15 @@
                 
               </div>
               
-              <?php dpm($content['metatags']); ?>
+              <?php 
+              
+              echo render($content['metatags']['title']);
+              echo render($content['metatags']['description']);
+              echo render($content['metatags']['keywords']);
+              dpm($content['metatags']); 
+              
+              
+              ?>
               <?php echo render($content['metatags']); ?>
           
           
