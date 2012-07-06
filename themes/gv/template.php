705bf8372c625c50c7529668ccbaf395f41509e3
U-FLYSMALL\Alex <all4senses@gmail.com>
2012-07-06 12:37:40 +0300

    Third Quote

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -78,7 +78,7 @@ function gv_preprocess_node(&$variables) {
     elseif($variables['node']->type == 'quote' && ($variables['node']->title == 'Request a Quote page v2' || $variables['node']->title == 'Request a Quote page v2 Final') ) {
       $variables['theme_hook_suggestions'][] = 'node__quote__v2';
     }
-    elseif($variables['node']->type == 'quote' && ($variables['node']->title == 'Request a Quote page v3' || $variables['node']->title == 'Request a Quote page v2 Final') ) {
+    elseif($variables['node']->type == 'quote' && ($variables['node']->title == 'Request a Quote page v3' || $variables['node']->title == 'Request a Quote page v3 Final') ) {
       $variables['theme_hook_suggestions'][] = 'node__quote__v3';
     }
     
