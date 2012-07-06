543375b9716140de47f67ea937e2cece62a99318
U-FLYSMALL\Alex <all4senses@gmail.com>
2012-07-06 15:21:14 +0300

    Third Quote

--- themes/gv/templates/node--quote--v3.tpl.php
+++ themes/gv/templates/node--quote--v3.tpl.php
@@ -44,8 +44,9 @@
 
     <div class="left">
       <?php 
-        if ($_SERVER['REDIRECT_URL'] == '/request-voip-phone-system-quote-final') {
+        if ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final') {
           echo '<div class="quote-final">' . t('<p><strong>Thank you</strong> for taking your time to complete our form. A VoIP Expert will be contacting you shortly to provide you with a personalized VoIP Service quote.</p><p>In the meantime, you can gain a great deal of VoIP information right here at <a href="http://getvoip.com">GetVoIP.com</a></p>') . '</div>';
+          echo '<a href="/"><div class="field-name-field-q-image">', theme('image', array( 'path' => $initialQuotePage_node->field_q_image['und'][0]['uri'], 'alt' => $initialQuotePage_node->field_q_image['und'][0]['alt'], 'title' => $initialQuotePage_node->field_q_image['und'][0]['title'])), '</div></a>';
         }
         else {
           ?>
@@ -59,14 +60,13 @@
       ?>
     </div>
 
-    <?php if ($_SERVER['REDIRECT_URL'] == '/request-voip-phone-system-quote-final'): ?>
+    <?php if ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final'): ?>
       <div class="right-final">
         <?php 
-          //echo '<a href="/">' , render($content['field_q_image']) , '</a>'; 
-          //echo '<a href="/">', theme('image_style', array( 'path' => $initialQuotePage_node->field_q_image['und'][0]['uri'], 'alt' => $initialQuotePage_node->field_q_image['und'][0]['alt'], 'title' => $initialQuotePage_node->field_q_image['und'][0]['title'])), '</a>';
-          echo '<a href="/">', theme('image', array( 'path' => $initialQuotePage_node->field_q_image['und'][0]['uri'], 'alt' => $initialQuotePage_node->field_q_image['und'][0]['alt'], 'title' => $initialQuotePage_node->field_q_image['und'][0]['title'])), '</a>';
-          
-          echo '<h2>' , t('GET ACCESS TO THE LARGEST<br/> VOIP SERVICE GUIDE<br/> IN THE NATION!') , '</h2><div class="link">' , l(t('Learn more'), '<front>') , '</div>';
+          echo '<div class="right-final">',
+                  '<a class="image" href="/"><div class="field field-name-field-q-image-2 field-type-image field-label-hidden"><div class="field-items"><div class="field-item even"><img width="476" height="262" alt="" src="http://getvoip.com/sites/default/files/get-access.png" typeof="foaf:Image"></div></div></div></a>',
+                  '<h2>' , t('GET ACCESS TO THE LARGEST<br/> VOIP SERVICE GUIDE<br/> IN THE NATION!') , '</h2><div class="link">' , l(t('Learn more'), '<front>') , '</div>',
+               '</div>';        
         ?>
       </div>
     <?php else: ?>
