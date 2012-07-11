7b2aa5bc41a728df4b000ec8c5d1a6dea1dca2a9
U-FLYSMALL\Alex <all4senses@gmail.com>
2012-07-11 04:53:45 +0300

    Different fixes

--- themes/gv/templates/node--preface.tpl.php
+++ themes/gv/templates/node--preface.tpl.php
@@ -13,7 +13,7 @@
 <?php endif; ?> <!-- if (!$page) -->
     
 
-  <?php print $user_picture; ?>
+  <?php //print $user_picture; ?>
 
 
   <?php print render($title_prefix); ?>
@@ -37,4 +37,38 @@
       hide($content['field_tags']);
       print render($content);
     ?>
+    
+    <?php 
+    global $user;
+    if ($user->uid == 1): ?>
+    
+     <div class="share">
+        <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+        <script type="IN/Share" data-url="<?php echo $url?>" data-counter="right" data-showzero="true"></script>
+
+        <script type="text/javascript">
+          (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+        <g:plusone size="medium" href="<?php echo $url?>"></g:plusone>
+
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=138241656284512";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+        <div class="fb-like" data-href="<?php echo $url?>" data-send="false" data-layout="button_count" data-width="80" data-show-faces="false"></div>
+
+        <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url?>">Tweet</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+      </div> <!-- main share buttons -->
+    <?php endif; ?>
+      
   </div>
