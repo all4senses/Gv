ddc3b6e6c4ea37cfd9de58e9c89334f6e964bf8d
all4senses <all4senses@gmail.com>
2012-08-07 14:30:11 +0300

    Inject block in views rows

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -181,8 +181,9 @@ function gv_preprocess_views_view_row_rss(&$vars) {
 function gv_preprocess_views_view_unformatted(&$vars) {
   
   if( ($vars['view']->name == 'articles' /*|| $vars['view']->name == 'blog'*/ || $vars['view']->name == 'news') && $vars['view']->current_display == 'page') {
-    $vars['theme_hook_suggestions'][] = 'views-view-unformatted__articles';
+    $vars['theme_hook_suggestions'][] = 'views-view-unformatted__articles__page';
   }
+  //elseif($vars['view']->name == 'articles' && $vars['view']->current_display == 'page')
   //dpm($vars);
 }
 
