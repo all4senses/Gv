8e264f48b8e39760f041641bc6641543477673d4
all4senses <all4senses@gmail.com>
2012-08-07 16:35:36 +0300

    Inject block in views rows

--- themes/gv/templates/views-view-unformatted--blog--page.tpl.php
+++ themes/gv/templates/views-view-unformatted--blog--page.tpl.php
@@ -5,13 +5,23 @@
  *
  * @ingroup views_templates
  */
-dpm($rows);
+//dpm($rows);
 dpm('test---------------------->blog');
+global $user;
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
+  
+  <?php 
+  
+  if ($user->uid == 1) {
+    echo 'test';
+    echo gv_pages_getTopPickProvidersBlocksForAllReviewsPage();
+  }
+  ?>
+  
   <div class="<?php print $classes_array[$id]; ?>">
     <?php print $row; ?>
   </div>
