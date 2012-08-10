86512d2bb2fca62ef4c76753829ee45cbcfd7f1c
all4senses <all4senses@gmail.com>
2012-08-10 20:09:27 +0300

    Different improvements

--- themes/gv/templates/node--admin-page.tpl.php
+++ themes/gv/templates/node--admin-page.tpl.php
@@ -84,3 +84,23 @@
 <?php else: ?>
   </div> <!-- <div class="main-content"> -->
 <?php endif; ?>
+
+ <?php 
+ 
+ global $user;
+ if ($user->uid == 1):
+ ?>
+  
+  <!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style ">
+<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+<a class="addthis_button_tweet"></a>
+<a class="addthis_button_google_plusone"></a>
+<a class="addthis_button_linkedin"></a>
+</div>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-50253baf64925d35"></script>
+<!-- AddThis Button END -->
+
+ <?php endif; ?>
+
