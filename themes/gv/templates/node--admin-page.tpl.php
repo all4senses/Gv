bf797f90c07c03cb5a5731a05e1fbfb47957c376
all4senses <all4senses@gmail.com>
2012-08-10 20:27:23 +0300

    Different improvements

--- themes/gv/templates/node--admin-page.tpl.php
+++ themes/gv/templates/node--admin-page.tpl.php
@@ -93,12 +93,12 @@
   
   <!-- AddThis Button BEGIN -->
 <div class="addthis_toolbox addthis_default_style ">
+<a class="addthis_button_linkedin_counter"></a>
+<a class="addthis_button_google_plusone"></a>
 <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
 <a class="addthis_button_tweet"></a>
-<a class="addthis_button_google_plusone"></a>
-<a class="addthis_button_linkedin_counter"></a>
 </div>
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true, "ui_language":"en_us"};</script>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true, ui_language:"en"};</script>
 <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-50253baf64925d35"></script>
 <!-- AddThis Button END -->
 
