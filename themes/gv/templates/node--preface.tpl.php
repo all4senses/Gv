204b858c69b9d64c594fd5a551634250c87ed81c
all4senses <all4senses@gmail.com>
2012-08-10 22:45:39 +0300

    Different improvements

--- themes/gv/templates/node--preface.tpl.php
+++ themes/gv/templates/node--preface.tpl.php
@@ -44,6 +44,24 @@
     
      <div class="share">
        <div class="main">
+        
+        <?php 
+              // No significant improvements with addthis buttons set... It bacame even slower.
+              /*global $user; if($user->uid == 1): ?> 
+         
+
+              <!-- AddThis Button BEGIN -->
+            <div class="addthis_toolbox addthis_default_style ">
+            <a class="addthis_button_linkedin_counter"></a>
+            <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
+            <a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:locale="en_US"></a>
+            <a class="addthis_button_tweet"></a>
+            </div>
+            <script type="text/javascript">var addthis_config = {"data_track_addressbar":true, ui_language:"en", locale:"en_US"};</script>
+            <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-50253baf64925d35"></script>
+            <!-- AddThis Button END -->
+
+         <?php else: */?> 
          
         <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
         <script type="IN/Share" data-url="<?php echo $url?>" data-counter="right" data-showzero="true"></script>
@@ -69,6 +87,9 @@
 
         <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url?>">Tweet</a>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        
+        <?php //endif; ?> 
+        
        </div><!-- main -->
       </div> <!-- share buttons -->
     <?php //endif; ?>
