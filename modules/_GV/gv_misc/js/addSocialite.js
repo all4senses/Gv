beaab6a784cbf9a24343a8f2820df616b628fb4f
all4senses <all4senses@gmail.com>
2012-08-15 06:54:50 +0300

    Socialite

--- modules/_GV/gv_misc/js/addSocialite.js
+++ modules/_GV/gv_misc/js/addSocialite.js
@@ -3,13 +3,15 @@
   Drupal.behaviors.gv_addSocialite = {
     attach: function (context, settings) {
        
-       
+        console.log('point 1');
         var loadButtons = function() {
+          console.log('in func');
+          console.log($(this));
           Socialite.load('.social-buttons');
           $('#header').unbind('mouseenter', loadButtons);
           $('#all-content').unbind('mouseenter', loadButtons);
         };
-        
+        console.log('point 2');
         $('#header').bind('mouseenter', loadButtons);
         $('#all-content').bind('mouseenter', loadButtons);
        
