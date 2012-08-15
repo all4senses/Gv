0df948b1e308301be6740afe0e43ef998d9534e7
all4senses <all4senses@gmail.com>
2012-08-15 07:00:12 +0300

    Socialite

--- modules/_GV/gv_misc/js/addSocialite.js
+++ modules/_GV/gv_misc/js/addSocialite.js
@@ -3,26 +3,26 @@
   Drupal.behaviors.gv_addSocialite = {
     attach: function (context, settings) {
        
-        console.log('point 1');
-        var loadButtons = function() {
-          console.log('in func');
-          console.log($(this));
-          Socialite.load('.social-buttons');
-          $('#header').unbind('mouseenter', loadButtons);
-          $('#all-content').unbind('mouseenter', loadButtons);
-        };
-        console.log('point 2');
-        $('#header').bind('mouseenter', loadButtons);
-        $('#all-content').bind('mouseenter', loadButtons);
+//        console.log('point 1');
+//        var loadButtons = function() {
+//          console.log('in func');
+//          console.log($(this));
+//          Socialite.load('.social-buttons');
+//          $('#header').unbind('mouseenter', loadButtons);
+//          $('#all-content').unbind('mouseenter', loadButtons);
+//        };
+//        console.log('point 2');
+//        $('#header').bind('mouseenter', loadButtons);
+//        $('#all-content').bind('mouseenter', loadButtons);
        
-       /*
+       
        $('#header').one('mouseenter', function()
        //$('body').one('mouseenter', function()
        //$('#bshadow').one('mouseover', function()
         {
           Socialite.load('.social-buttons');
         });
-        */
+       
     }
   };
 
