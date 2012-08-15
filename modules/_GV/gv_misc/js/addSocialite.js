65a9138142d954c06d050b9c34c6e2765070d878
all4senses <all4senses@gmail.com>
2012-08-15 06:05:52 +0300

    Socialite

--- modules/_GV/gv_misc/js/addSocialite.js
+++ modules/_GV/gv_misc/js/addSocialite.js
@@ -3,13 +3,24 @@
   Drupal.behaviors.gv_addSocialite = {
     attach: function (context, settings) {
        
+       
+        var loadButtons = function() {
+          Socialite.load('.social-buttons');
+          $('#header').unbind('mouseenter', loadButtons);
+          $('#all-content').unbind('mouseenter', loadButtons);
+        };
+        
+        $('#header').bind('mouseenter', loadButtons);
+        $('#all-content').bind('mouseenter', loadButtons);
+       
+       /*
        $('#header').one('mouseenter', function()
        //$('body').one('mouseenter', function()
        //$('#bshadow').one('mouseover', function()
         {
           Socialite.load('.social-buttons');
         });
-    
+        */
     }
   };
 
