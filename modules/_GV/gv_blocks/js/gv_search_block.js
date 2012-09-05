4d9b70ca4375f2b2222b70ff0f617835bfaa0b70
all4senses <all4senses@gmail.com>
2012-09-06 00:10:17 +0300

    Search block default text

--- modules/_GV/gv_blocks/js/gv_search_block.js
+++ modules/_GV/gv_blocks/js/gv_search_block.js
@@ -10,11 +10,12 @@
         $('input[name="search_block_form"]').click(function(e){
           
           //console.log($('input[name="search_block_form"]').val());
-          
+          /*
           if ($('input[name="search_block_form"]').val() == $('input[name="search_block_form"]').attr('title')) {
             $('input[name="search_block_form"]').val('');
             //e.preventDefault();
           }
+          */
           
         });
         /*
