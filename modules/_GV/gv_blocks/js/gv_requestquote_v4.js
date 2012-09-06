49469a5082910e69c8b8109eef7efc8bc5d9ab04
all4senses <all4senses@gmail.com>
2012-09-07 01:08:51 +0300

    Prefaces with bottoms turned on

--- modules/_GV/gv_blocks/js/gv_requestquote_v4.js
+++ modules/_GV/gv_blocks/js/gv_requestquote_v4.js
@@ -24,14 +24,17 @@
             return;
             
             var l = $(this).val().length;
-            if( !(event.keyCode == 8                                // backspace
-                || event.keyCode == 9
-                || event.keyCode == 46                              // delete
-                || (event.keyCode >= 35 && event.keyCode <= 40)     // arrow keys/home/end
-              
-                || (event.keyCode >= 48 && event.keyCode <= 57)     // numbers on keyboard
-                || (event.keyCode >= 96 && event.keyCode <= 105))   // number on keypad
-                ) {
+            if( !(     event.keyCode == 8                                // backspace
+                    || event.keyCode == 9
+                    || event.keyCode == 46                              // delete
+                    || (event.keyCode >= 35 && event.keyCode <= 40)     // arrow keys/home/end
+
+                    || (event.keyCode >= 48 && event.keyCode <= 57)     // numbers on keyboard
+                    || (event.keyCode >= 96 && event.keyCode <= 105)    // number on keypad
+                  
+                    || (event.keyCode == 32 || event.keyCode == 189 || event.keyCode == 190)    // space, dash, dot
+                 )   
+              ) {
                     event.preventDefault();     // Prevent character input
             }
             else {
@@ -141,7 +144,6 @@
               phone: {
                 required: true,
                 //number: true,
-                phoneUS: true,
                 minlength: 9,
                 maxlength: 15,
                 notEqualsTo: $('input[id="phone"]').attr('title')
