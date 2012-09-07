dc58199a6e5aa21687979cd8930addfb19730edc
all4senses <all4senses@gmail.com>
2012-09-07 09:03:33 +0300

    Submit a review fix

--- modules/_GV/gv_pages/js/gv_review_fieldHints.js
+++ modules/_GV/gv_pages/js/gv_review_fieldHints.js
@@ -3,8 +3,8 @@
   Drupal.behaviors.gv_review_fieldHints = {
     attach: function (context, settings) {
       
-      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-pros"], input[id="edit-cons"]').hint();
-    
+      //$('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-pros"], input[id="edit-cons"]').hint();
+      $('input[id="edit-pros"], input[id="edit-cons"]').hint();
     }
   };
 
