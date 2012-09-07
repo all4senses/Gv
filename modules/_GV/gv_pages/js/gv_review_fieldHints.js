c27bacd783395a99df3f6bd9313f173f681245bf
all4senses <all4senses@gmail.com>
2012-09-07 09:16:38 +0300

    Submit a review fix

--- modules/_GV/gv_pages/js/gv_review_fieldHints.js
+++ modules/_GV/gv_pages/js/gv_review_fieldHints.js
@@ -4,7 +4,7 @@
     attach: function (context, settings) {
       
       //$('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-pros"], input[id="edit-cons"]').hint();
-      //$('input[id="edit-pros"], input[id="edit-cons"]').hint();
+      $('input[id="edit-pros"]').hint();
     }
   };
 
