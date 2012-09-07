e98f9b1c271787813a1e89ec10bb7edf1211c44b
all4senses <all4senses@gmail.com>
2012-09-07 03:42:41 +0300

    Front metatags

--- themes/gv/templates/node--preface.tpl.php
+++ themes/gv/templates/node--preface.tpl.php
@@ -49,14 +49,13 @@
         
         <?php global $user; if(/*$user->uid != */1): ?>
               
-              <?php 
-                if (isset($node->metatags['title']['value']) && $node->metatags['title']['value']) {
-                  $share_title = $node->metatags['title']['value'];
+              <?php
+                if ($is_front) {
+                  $share_title = gv_misc_metatag_getFrontTitle();
                 }
-                elseif ($is_front) {
-                  $share_title = 'VoIP Services - VoIP Providers - Phone System Solutions | Get VoIP';
-                  $mt = gv_misc_metatag_getFrontTitle();
-                  dpm($mt);
+                
+                if (!$share_title && isset($node->metatags['title']['value']) && $node->metatags['title']['value']) {
+                  $share_title = $node->metatags['title']['value'];
                 }
                 else {
                   $share_title = $title;
