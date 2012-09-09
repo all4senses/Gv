895dd5761ae5c1769093d0829cb11def217958f9
all4senses <all4senses@gmail.com>
2012-09-09 11:02:31 +0300

    Quote page v3 Guide links

--- themes/gv/templates/node--quote--v3.tpl.php
+++ themes/gv/templates/node--quote--v3.tpl.php
@@ -86,7 +86,7 @@
           <div class="bottom-clear"></div> 
         </div>
         <div id="right-up-image"></div>
-        <div class="guide"></div>
+        <div class="guide">Free VoIP Guide With Request</div>
         <div id="right-bottom">
           <?php echo gv_blocks_get_requestQuoteForPage_v3(); ?>
           <div class="bottom-clear"></div> 
