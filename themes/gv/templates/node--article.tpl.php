679e5e66d80734ffd03e122183648c3c69c0aca2
all4senses <all4senses@gmail.com>
2012-09-24 17:17:37 +0300

    New keywords

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -140,6 +140,7 @@
           
           if (isset($content['field_topics']) && (!isset($content['metatags']['keywords']['#attached']['drupal_add_html_head'][0][0]['#value']) || !$content['metatags']['keywords']['#attached']['drupal_add_html_head'][0][0]['#value']) ) {
             hide($content['metatags']['keywords']);
+            dpm($content['field_topics']);
             gv_misc_pushTagsToMetatags('keywords', $content['field_topics']);
           }
           print render($content);
