d5333d27e61ffc1f8a0c6339e2cde4b9e59f7814
all4senses <all4senses@gmail.com>
2012-09-24 17:48:16 +0300

    News keywords

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -140,8 +140,13 @@
           
           if (isset($content['field_topics']) && (!isset($content['metatags']['keywords']['#attached']['drupal_add_html_head'][0][0]['#value']) || !$content['metatags']['keywords']['#attached']['drupal_add_html_head'][0][0]['#value']) ) {
             hide($content['metatags']['keywords']);
+            dpm('tags');
             dpm($content['field_topics']);
             gv_misc_pushTagsToMetatags('keywords', $content['field_topics']);
+          }
+          else {
+            dpm('keywords');
+            dpm($content['metatags']['keywords']['#attached']['drupal_add_html_head'][0][0]['#value']);
           }
           print render($content);
         ?>
