6d22b34e6960f6adaaaa45c8922774bb3633da45
all4senses <all4senses@gmail.com>
2012-09-25 05:12:16 +0300

    Metatags order

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -80,7 +80,7 @@ function gv_process_html(&$variables) {
 /*
 * Implements hook_html_head_alter
 */
-function yourtheme_html_head_alter(&$head_elements) {
+function gv_html_head_alter(&$head_elements) {
   dpm('head_alter');
   dpm($head_elements);
 }
