9adcfbf89410155680a3f378c66805e131fb7300
all4senses <all4senses@gmail.com>
2012-09-25 03:49:19 +0300

    Metatags order

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -55,6 +55,7 @@ function gv_menu_local_tasks(&$variables) {
  * Override or insert variables into the page template.
  */
 function gv_process_page(&$variables) {
+  dpm($variables);
   if(isset($variables['node'])) {
     $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
   }
