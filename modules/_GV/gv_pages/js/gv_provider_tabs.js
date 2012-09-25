f16a854d7b05f9a2a77acf4e9b66778ac54a753f
all4senses <all4senses@gmail.com>
2012-09-25 06:51:39 +0300

    Metatags order

--- modules/_GV/gv_pages/js/gv_provider_tabs.js
+++ modules/_GV/gv_pages/js/gv_provider_tabs.js
@@ -3,8 +3,7 @@
   Drupal.behaviors.gv_provider_tabs = {
     attach: function (context, settings) {
 
-        
-        console.log('Tabs!');
+        //console.log('Tabs!');
         $( ".data.tabs" ).tabs();
         
     }
