9398a7fe9c6d8c180965a643fd786cc7d10ff72a
all4senses <all4senses@gmail.com>
2012-09-30 20:38:53 +0300

    Try to fix updatet listing error

--- modules/_GV/gv_misc/js/gv_preventEnterOnEditField.js
+++ modules/_GV/gv_misc/js/gv_preventEnterOnEditField.js
@@ -4,9 +4,34 @@
     attach: function (context, settings) {
 
         //console.log('Tabs!');
-        $( ".field-type-number-integer input").click(function(){
+        $(".field-type-number-integer input").click(function(){
           console.log('Test');
         });
+        
+        $('.field-type-number-integer input').keydown(function (event) { 
+            
+            console.log($(this).val());
+            console.log(event.keyCode);
+            
+            /*
+            if( !(     event.keyCode == 8                                // backspace
+                    || event.keyCode == 9
+                    || event.keyCode == 46                              // delete
+                    || (event.keyCode >= 35 && event.keyCode <= 40)     // arrow keys/home/end
+
+                    || (event.keyCode >= 48 && event.keyCode <= 57)     // numbers on keyboard
+                    || (event.keyCode >= 96 && event.keyCode <= 105)    // number on keypad
+                  
+                    || (event.keyCode == 32 || event.keyCode == 189 || event.keyCode == 190 || event.keyCode == 173)    // space, dash, dot
+                 )   
+              ) {
+                    event.preventDefault();     // Prevent character input
+            }
+            */
+           
+           
+
+        });   
 
     }
   };
