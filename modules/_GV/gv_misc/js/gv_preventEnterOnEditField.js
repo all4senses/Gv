dafd3116e7f4778cb10201f3c44d822cba256cfd
all4senses <all4senses@gmail.com>
2012-09-30 20:49:18 +0300

    Try to fix updatet listing error

--- modules/_GV/gv_misc/js/gv_preventEnterOnEditField.js
+++ modules/_GV/gv_misc/js/gv_preventEnterOnEditField.js
@@ -9,11 +9,11 @@
         });
         
         $('.field-type-number-integer input').keydown(function (event) { 
-            alert('z');
+
             console.log($(this).val());
             console.log(event.keyCode);
             
-            /*
+            
             if( !(     event.keyCode == 8                                // backspace
                     || event.keyCode == 9
                     || event.keyCode == 46                              // delete
@@ -21,13 +21,11 @@
 
                     || (event.keyCode >= 48 && event.keyCode <= 57)     // numbers on keyboard
                     || (event.keyCode >= 96 && event.keyCode <= 105)    // number on keypad
-                  
-                    || (event.keyCode == 32 || event.keyCode == 189 || event.keyCode == 190 || event.keyCode == 173)    // space, dash, dot
                  )   
               ) {
                     event.preventDefault();     // Prevent character input
             }
-            */
+            
            
            
 
