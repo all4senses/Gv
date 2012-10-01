7092fe0679a7c172ea885d8f56b3dbfe836cb8d2
all4senses <all4senses@gmail.com>
2012-10-02 01:37:02 +0300

    Search results pager issue

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -13,10 +13,10 @@ function gv_preprocess_search_results(&$variables) {
     dpm($_SESSION['gv_node_search_data']);
     //dpm($_SESSION['gv_limit_rows']);
     
-    //$page = pager_default_initialize($gv_num_rows, $gv_limit_rows);
     //$page = pager_default_initialize(43, 15);
-    $page = pager_default_initialize($_SESSION['gv_node_search_data']['gv_num_rows'], $_SESSION['gv_node_search_data']['gv_limit_rows']);
-    //unset($_SESSION['gv_node_search_data']);
+
+    $page = pager_default_initialize($gv_num_rows, $gv_limit_rows);
+    //$page = pager_default_initialize($_SESSION['gv_node_search_data']['gv_num_rows'], $_SESSION['gv_node_search_data']['gv_limit_rows']);
     
     $variables['pager'] = theme('pager', array('tags' => NULL));
     //dpm($variables['pager']);
