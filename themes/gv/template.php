e1ad877837c22e4ed16af606bdb62781a5e196cd
all4senses <all4senses@gmail.com>
2012-10-02 01:09:05 +0300

    Search results pager issue

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -8,9 +8,11 @@ function gv_preprocess_search_results(&$variables) {
   if ($user->uid == 1) {
     
     global $gv_num_rows, $gv_limit_rows;
-    dpm($gv_num_rows);
-    dpm($gv_limit_rows);
-    $page = pager_default_initialize($gv_num_rows, $gv_limit_rows);
+    dpm('$gv_num_rows =' . $gv_num_rows);
+    dpm('$gv_limit_rows = ' . $gv_limit_rows);
+    
+    //$page = pager_default_initialize($gv_num_rows, $gv_limit_rows);
+    $page = pager_default_initialize(43, 15);
     
     $variables['pager'] = 'tst ' . theme('pager', array('tags' => NULL));
     dpm($variables['pager']);
