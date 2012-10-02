096b96ec5293166545e6ca98d3ca7afcfc97baa8
all4senses <all4senses@gmail.com>
2012-10-03 01:07:56 +0300

    New social button links layout

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -123,7 +123,7 @@
       if ($user->uid) {
         
         ?>
-          <div class="lock-n-rss">
+          <div id="lock-n-rss">
             <div id="sitelock"><a id="sitelock" href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=getvoip.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/getvoip.com"/></a></div>
             <a id="valid-rss" href="http://appc.w3.org/check.cgi?url=http%3A//getvoip.com/rss.xml"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/valid-rss-rogers.png" alt="[Valid RSS]" title="Validate my RSS feed" /></a>
           </div>
