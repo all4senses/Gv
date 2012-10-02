eb5b6b74ba4d95a59bf1dc9d75d853dc412e4a42
all4senses <all4senses@gmail.com>
2012-10-03 01:00:42 +0300

    Chart fields widths fix

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -114,7 +114,29 @@
   <footer id="footer" role="contentinfo" class="clearfix">
     <?php echo render($page['footer']); ?>
     <div class="c">© 2012 GetVoIP.com | All Rights Reserved</div>
-    <div id="sitelock"><a id="sitelock" href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=getvoip.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/getvoip.com"/></a></div>
-    <a id="valid-rss" href="http://appc.w3.org/check.cgi?url=http%3A//getvoip.com/rss.xml"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/valid-rss-rogers.png" alt="[Valid RSS]" title="Validate my RSS feed" /></a>
+    
+
+    
+    <?php
+      global $user;
+      
+      if ($user->uid) {
+        
+        ?>
+          <div class="lock-n-rss">
+            <div id="sitelock"><a id="sitelock" href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=getvoip.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/getvoip.com"/></a></div>
+            <a id="valid-rss" href="http://appc.w3.org/check.cgi?url=http%3A//getvoip.com/rss.xml"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/valid-rss-rogers.png" alt="[Valid RSS]" title="Validate my RSS feed" /></a>
+          </div>
+        <?php
+          echo '<div id="block-gv-blocks-follow-links"><div class="follow-us">Follow Us</div>', gv_blocks_get_headerLinks(), '</div>';
+      }
+      else {
+        ?>
+          <div id="sitelock"><a id="sitelock" href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=getvoip.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/getvoip.com"/></a></div>
+          <a id="valid-rss" href="http://appc.w3.org/check.cgi?url=http%3A//getvoip.com/rss.xml"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/valid-rss-rogers.png" alt="[Valid RSS]" title="Validate my RSS feed" /></a>
+        <?php
+      }
+    ?>
+    
   </footer> <!-- /#footer -->
 
