7867cd690ccd6a9a978a7ab20f9de32a12b364ee
all4senses <all4senses@gmail.com>
2012-10-27 06:10:27 +0300

    w3c validation

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -141,7 +141,7 @@
         <?php
           
       }
-      else {
+      else { 
         ?>
           <div id="sitelock"><a href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=getvoip.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/getvoip.com"/></a></div>
           <a id="valid-rss" href="http://appc.w3.org/check.cgi?url=http%3A//getvoip.com/rss.xml"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/valid-rss-rogers.png" alt="[Valid RSS]" title="Validate my RSS feed" /></a>
