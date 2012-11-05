3dc39185a098812410b2b5d10389a6d366068c31
all4senses <all4senses@gmail.com>
2012-11-05 21:58:29 +0200

    Zoomable phone main

---
+++ modules/_GV/gv_misc/js/gv_makeImagesZoomable.js
@@ -0,0 +1,22 @@
+(function ($) {
+
+  Drupal.behaviors.gv_makeImagesZoomable = {
+    attach: function (context, settings) {
+       
+       //$(".full-content img").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+       //$(".full-content img").colorbox({rel:"gallery"});
+       $("a.zoom").css('cursor', 'pointer').css('cursor', '-webkit-zoom-in').css('cursor', '-moz-zoom-in').colorbox({
+         //href: this.src
+         width:"80%", 
+         height:"80%"
+//         ,href: function() {
+//           //console.log(this.src);
+//           return this.src;
+//         }
+       });
+       //console.log('full content!');
+       
+    }
+  };
+
+}(jQuery));
