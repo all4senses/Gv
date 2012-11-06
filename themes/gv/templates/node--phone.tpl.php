1f9be05a5d43fc4493f698fe19f42d24fbbd587b
all4senses <all4senses@gmail.com>
2012-11-06 05:57:01 +0200

    Phone page

--- themes/gv/templates/node--phone.tpl.php
+++ themes/gv/templates/node--phone.tpl.php
@@ -216,12 +216,11 @@
                             <div class="overall"> 
                               <div class="text">
                                 <?php echo '<div class="voters"><div class="title">' . t('Number of Reviews') . ':</div><div class="count" property="v:count">' . $node->gv_voters . '</div></div>'; ?>
-                                <?php //echo render($content['gv_recommend']); ?>
                                 <?php echo '<div class="recommend"><div class="title">' . t('Would recommend') . ': </div><div class="data">' . $node->gv_recommend . '% ' . t('of all voters') . '</div></div>'; ?>
-                                <div class="overall title"><?php $node->field_p_name['und'][0]['value'] /*$content['field_p_name'][0]['#markup']*/ . ' ' . t('Overall Rated:'); ?></div>
+                                <div class="overall title"><?php $node->field_p_name['und'][0]['value'] . ' ' . t('Overall Rated:'); ?></div>
                               </div>
                               <div class="star-big">
-                                <?php echo /*render($content['gv_rating_overall'])*/ '<div class="count" content="' . $node->gv_rating_overall . '" property="v:rating">' . $node->gv_rating_overall . '</div>' . '<div class="descr">' . t('Out of 5 stars') . '</div>'; ?>
+                                <?php echo '<div class="count" content="' . $node->gv_rating_overall . '" property="v:rating">' . $node->gv_rating_overall . '</div>' . '<div class="descr">' . t('Out of 5 stars') . '</div>'; ?>
                               </div>
                             </div>
 
@@ -230,11 +229,13 @@
                         <div class="bottom-clear"></div>
               
                       <?php 
-                        echo $node->userReviews . '<br/>' . l('Rate it', 'voip-phone-submit-user-review', array('attributes' => array('target' => '_blank'), 'query' => array('id' => $node->nid))); 
+                      
                         global $user;
                         if ($user->uid == 1) {
                           echo $node->addPhoneReviewForm;
                         }
+                        echo $node->userReviews; // . '<br/>' . l('Rate it', 'voip-phone-submit-user-review', array('attributes' => array('target' => '_blank'), 'query' => array('id' => $node->nid))); 
+                        
                       ?>
                     </div>
                 
