31678ed1b3ed12c1822288f46a96a04dda8fafda
all4senses <all4senses@gmail.com>
2012-11-07 02:47:50 +0200

    Phone page

--- themes/gv_orange/templates/block.tpl.php
+++ themes/gv_orange/templates/block.tpl.php
@@ -1,10 +1,10 @@
-<?php dpm($block->region); if (!strpos($classes, 'first')): ?>
+<?php if (!strpos($classes, 'first')): ?>
   <div class="top-line <?php print $block_html_id; ?>"></div>
 <?php endif; ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h3<?php print $title_attributes; ?>><?php print $title; ?></h3>
+    <?php echo ($block->region == 'content' ? '<h2 ' : '<h3 ') . $title_attributes; ?>><?php echo $title . ($block->region == 'content' ? '</h2>' : '</h3>'); ?>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
