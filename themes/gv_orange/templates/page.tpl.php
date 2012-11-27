e58655424be5ace40ff2dfd1eb49fcd6fb3e93b0
all4senses <all4senses@gmail.com>
2012-11-27 21:12:10 +0200

    OM menu caching

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -28,10 +28,10 @@
       </div>
       
       <?php 
-          global $user;
-          if ($user->uid == 1) {
-            echo gv_blocks_getBlockThemed(array('module' => 'om_maximenu', 'delta' => 'om-maximenu-1', 'no_subject' => TRUE, 'class' => 'block-om-maximenu', 'shadow' => FALSE), TRUE, '+31 day');
-          }
+          //global $user;
+
+          echo gv_blocks_getBlockThemed(array('module' => 'om_maximenu', 'delta' => 'om-maximenu-1', 'no_subject' => TRUE, 'class' => 'block-om-maximenu', 'shadow' => FALSE), TRUE, '+31 day');
+
 //        if ($user->uid == 1) {
           echo /*'<a id="itexpo" href="http://getvoip.com/blog/tags/itexpo-2012"></a>',*/ render($page['header']); 
           
