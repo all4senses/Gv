d683b25793ffa09e9133002e39996e68d69b1ac8
all4senses <all4senses@gmail.com>
2012-11-28 00:50:19 +0200

    Fix Countdown js whith ajax views pager

--- modules/_GV/gv_misc/js/gv_countdown.js
+++ modules/_GV/gv_misc/js/gv_countdown.js
@@ -3,17 +3,23 @@
   Drupal.behaviors.gv_countdown = {
     attach: function (context, settings) {
        
-        $(".kkcount-down#ipad_mini").kkcountdown({
-          dayText		: 'day ',
-          daysText 	: 'days ',
-          hoursText	: 'h ',
-          minutesText	: 'm ',
-          secondsText	: 's left',
-          displayZeroDays : false
-          //,callback	: test
-          //,addClass	: 'shadow'
-        });
-        
+       if (!$(".kkcount-down#ipad_mini").hasClass('processed')) {
+         
+          $(".kkcount-down#ipad_mini").kkcountdown({
+            dayText		: 'day ',
+            daysText 	: 'days ',
+            hoursText	: 'h ',
+            minutesText	: 'm ',
+            secondsText	: 's left',
+            displayZeroDays : false
+            //,callback	: test
+            //,addClass	: 'shadow'
+          });
+          
+          $(".kkcount-down#ipad_mini").addClass('processed');
+       }
+       
+       
 //        function test(){
 //          console.log('KONIEC');
 //        }
