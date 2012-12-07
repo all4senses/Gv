24088d634e34d5be9878d913f7bdccf5fbdf1148
all4senses <all4senses@gmail.com>
2012-12-07 05:50:27 +0200

    2013 in page template

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -18,11 +18,11 @@
       <div id="header-menu-back"></div>
       
       <div id="logo-block">
-        <a href="<?php print $front_page; ?>" title="<?php print t('GetVoIP Home'); ?>" id="logo">
+        <a href="<?php print $front_page; ?>" title="<?php print 'GetVoIP Home'; ?>" id="logo">
           <img src="<?php print 'http://getvoip.com/sites/all/themes/gv_orange/css/images/getvoip-logo2.png'; ?>" alt="VoIP Service Providers" title="<?php print t('GetVoIP Home'); ?>" />
         </a>
         <div class="descr">
-          <div class="title">2012 VOIP GUIDE</div>
+          <div class="title">2013 VOIP GUIDE</div>
           <div class="subtitle">SERVICE PROVIDER REVIEWS</div><div class="stars"><img src="/sites/all/themes/gv_orange/css/images/sprite-0.png" alt="VoIP Reviews" title="VoIP Reviews"/></div>
         </div>
       </div>
