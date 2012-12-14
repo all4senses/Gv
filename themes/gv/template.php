2b867eb493af57103ca91b3ed7896b0b1ac5aacd
all4senses <all4senses@gmail.com>
2012-12-14 06:36:30 +0200

    Paginated content metatags

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -367,6 +367,9 @@ function gv_html_head_alter(&$head_elements) {
     return;
   }
   
+  drupal_set_title('xxx');
+  return;
+  
   // Remove a canonical tag if there next or prev tags are present.
   if (!empty($head_elements['next']) || !empty($head_elements['prev'])) {
     unset($head_elements['metatag_canonical']);
