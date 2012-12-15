802695838b2d825a8b63495511c1fb18e35020e5
all4senses <all4senses@gmail.com>
2012-12-15 02:36:05 +0200

    404

--- themes/gv/templates/node--admin-page--page404.tpl.php
+++ themes/gv/templates/node--admin-page--page404.tpl.php
@@ -8,6 +8,18 @@
           hide($content['comments']);
           hide($content['links']);
           print render($content);
+          
+          // Get content of the Sitemap.
+          $key = 'view-sitemap-page';
+          $query = db_select('node', 'n');
+          $query->join('field_data_field_preface_key', 'pk', "pk.entity_id = n.nid"); 
+          $query->join('field_data_body', 'fb', "fb.entity_id = n.nid"); 
+          $query->fields('fb', array('body_value'))
+                ->condition('n.type', 'preface')
+                ->condition('pk.field_preface_key_value', $key);
+          $sitemap_body = $query->execute()->fetchField(); 
+          dpm($sitemap_body);
+    
         ?>
       </div>
    
