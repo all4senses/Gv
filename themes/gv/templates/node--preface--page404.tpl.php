9b0e2a3117a0e1067aaf78e25dee23c2eb46a08a
all4senses <all4senses@gmail.com>
2012-12-15 02:56:40 +0200

    404

--- themes/gv/templates/node--preface--page404.tpl.php
+++ themes/gv/templates/node--preface--page404.tpl.php
@@ -1,8 +1,8 @@
-  <div class="main-content page404"> 
+  <div class="page404"> 
 
           <!-- <h1><?php //print $title; ?></h1> -->
 
-      <div class="content page"<?php print $content_attributes; ?>>
+      <div class="content page preface"<?php print $content_attributes; ?>>
         <?php
           // Hide comments, tags, and links now so that we can render them later.
           hide($content['comments']);
