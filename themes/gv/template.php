531ef1b8ac3cf7621360e4df980e15229b8629cb
all4senses <all4senses@gmail.com>
2012-12-24 17:59:46 +0200

    Meta pager fix

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -51,7 +51,7 @@ function gv_pager_next($variables) {
     //dpm($out);
     $out_altered = str_replace('&amp;', '&', $out);
     //if(preg_match('|.*href="(.*)" .*|', $out, $matches) && !empty($matches[1])) {
-    if(preg_match('|.*href="([^"]*)" .*title="([^"]*)"|', $out_altered, $matches) && !empty($matches[1])) {
+    if(preg_match('|.*href="([^"]*)" .*title="([^"]*)" .*|', $out_altered, $matches) && !empty($matches[1])) {
       
       //dpm('out in next = ' . $out_altered);
       //dpm($matches);
@@ -84,13 +84,15 @@ function gv_pager_previous($variables) {
   $out = theme_pager_previous($variables);
   if ($out) {
     $out_altered = str_replace('&amp;', '&', $out);
-    if(preg_match('|.*href="([^"]*)" .*|', $out_altered, $matches) && !empty($matches[1])) {
+    if(preg_match('|.*href="([^"]*)" .*title="([^"]*)" .*|', $out_altered, $matches) && !empty($matches[1])) {
       //dpm($matches[1]);
       
       dpm('out in prev = ' . $out_altered);
       dpm($matches);
       //gv_misc_addMetatag('prev', NULL, $href = 'http://getvoip.com' . str_replace('&amp;', '&', $matches[1]));
-      gv_misc_addMetatag('prev', NULL, $href = 'http://getvoip.com' . $matches[1]);
+      if ($matches[2] == 'Go to previous page') {
+        gv_misc_addMetatag('prev', NULL, $href = 'http://getvoip.com' . $matches[1]);
+      }
     }
   }
   return $out;
