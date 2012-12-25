7744b0ad6d274546d2750557050c54fdcc1c5763
all4senses <all4senses@gmail.com>
2012-12-25 10:22:05 +0200

    Nofollow

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -19,7 +19,7 @@ function gv_preprocess_user_profile(&$profile) {
 function gv_link($variables) {
   //global $user;
   //if ($user->uid == 1 && $variables['path'] == 'http://click.websitegear.com/track/1883449' && strpos($variables['path'], 'ttp://') && !strpos($variables['path'], 'ttp://getvoip.com')) {
-  if (strpos($variables['path'], 'ttp://') && !strpos($variables['path'], 'ttp://getvoip.com')) {
+  if (strpos($variables['path'], '://') && !strpos($variables['path'], 'ttp://getvoip.com')) {
     //dpm($variables['path']);
     //dpm($variables);
     if (empty($variables['options']['attributes']['rel'])) {
