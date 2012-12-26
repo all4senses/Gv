59da976c6bfe8bd5b8d6a7decaf99a837ad911d2
all4senses <all4senses@gmail.com>
2012-12-26 18:50:37 +0200

    404 page remove a preface from a Sitemap body

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -164,6 +164,7 @@
             gv_misc_pushTagsToMetatags($keyword_metatag_name, $content['field_topics']);
           }
           
+          dpm($content);
           echo render($content);
         ?>
       </div>
