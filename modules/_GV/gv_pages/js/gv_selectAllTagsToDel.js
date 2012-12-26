43cfb144da7554742189e765dff5888078b74409
all4senses <all4senses@gmail.com>
2012-12-26 15:59:21 +0200

    Main tags admin

--- modules/_GV/gv_pages/js/gv_selectAllTagsToDel.js
+++ modules/_GV/gv_pages/js/gv_selectAllTagsToDel.js
@@ -3,9 +3,8 @@
   Drupal.behaviors.gv_selectAllTagsToDel = {
     attach: function (context, settings) {
 
-        //console.log('select-all!');
         $( ".select-all" ).click(function(){
-          
+          console.log('select-all!');
         });
         
     }
