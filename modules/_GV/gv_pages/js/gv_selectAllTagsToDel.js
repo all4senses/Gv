5109cb57817a1dbc40e0d505cb415a865dbc5d36
all4senses <all4senses@gmail.com>
2012-12-26 16:16:48 +0200

    Main tags admin

--- modules/_GV/gv_pages/js/gv_selectAllTagsToDel.js
+++ modules/_GV/gv_pages/js/gv_selectAllTagsToDel.js
@@ -2,9 +2,22 @@
 
   Drupal.behaviors.gv_selectAllTagsToDel = {
     attach: function (context, settings) {
-
+        
+        var checked = false;
+        
         $( ".select-all" ).click(function(){
-          console.log('select-all!');
+          
+          //console.log('select-all!');
+          
+          if (!checked) {
+            $("#edit-tags input").attr('checked', true);
+            checked = true;
+          }
+          else {
+            $("#edit-tags input").attr('checked', false);
+            checked = false;
+          }
+          
         });
         
     }
