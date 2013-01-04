d6d53da12ce5ef67eb7a15e5f7880cb33b26f9fc
all4senses <all4senses@gmail.com>
2013-01-04 04:58:25 +0200

    Provider page review button

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -117,7 +117,7 @@
                   <div class="gv_votes"><?php echo '<div class="caption">' . t('Overall Consumer Ratings') . '</div>' . render($content['gv_ratings']); ?></div>
                   <div class="overall"> 
                     <div class="text">
-                      <?php echo '<a id="write-review" href="/voip-provider-submit-user-review"><img src="/sites/default/files/writeareview.png" /></a><div class="voters"><div class="title">' . 'Number of Reviews' . ':</div><div class="count" property="v:count">' . $node->gv_voters . '</div></div>'; ?>
+                      <?php echo '<a id="write-review" href="/voip-provider-submit-user-review?id=' . $node->nid . '"><img src="/sites/default/files/writeareview.png" /></a><div class="voters"><div class="title">' . 'Number of Reviews' . ':</div><div class="count" property="v:count">' . $node->gv_voters . '</div></div>'; ?>
                       <?php //echo render($content['gv_recommend']); ?>
                       <?php echo '<div class="recommend"><div class="title">' . t('Would recommend') . ': </div><div class="data">' . $node->gv_recommend . '% of Users' . '</div></div>'; ?>
                       <div class="overall title"><?php $node->field_p_name['und'][0]['value'] /*$content['field_p_name'][0]['#markup']*/ . ' ' . t('Overall Rated:'); ?></div>
