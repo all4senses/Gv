8845ac0366a3206cc34de3d1ad3788a71a17dddf
all4senses <all4senses@gmail.com>
2013-02-08 20:56:31 +0200

    Remove all reviews snippet data

--- themes/gv/templates/node--review.tpl.php
+++ themes/gv/templates/node--review.tpl.php
@@ -2,7 +2,7 @@
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 <?php endif; ?>
    
-  <div class="main-content"  xmlns:v="http://rdf.data-vocabulary.org/#" <?php if (@$_SERVER['REDIRECT_URL'] == '/providers/reviews') { echo 'typeof="v:Review"?>'; } ?>>
+  <div class="main-content"  xmlns:v="http://rdf.data-vocabulary.org/#" <?php if (@$_SERVER['REDIRECT_URL'] != '/providers/reviews') { echo 'typeof="v:Review"'; } ?>>
     
             <?php if (!$page): ?>
               <header>
