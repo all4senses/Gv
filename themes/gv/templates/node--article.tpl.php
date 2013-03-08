76557a6adce57463cdda5848da0007841f16f6a2
all4senses <all4senses@gmail.com>
2013-03-08 17:53:00 +0200

    New home style

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -20,7 +20,7 @@ if($view_mode == 'home_teaser') {
   }
             
   
-  echo $teaser_data['main_image_html'] . '<h3>'. l($node->title, 'node/' . $node->nid) . '</h3><div class="submitted"><span class="author">By ' . $author_name . '</span> / ' . date('F d, Y', $node->created) . '</div>' 
+  echo $teaser_data['main_image_html'] . '<h3>'. l($node->title, 'node/' . $node->nid) . '</h3><div class="submitted">By <span class="author">' . $author_name . '</span> / ' . date('F d, Y', $node->created) . '</div>' 
           . '<div class="teaser">' . $teaser_data['teaser_only'] . '</div>';
   
   return;
