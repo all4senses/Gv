313a458303f38476bf13a13873114fa733aa3dac
all4senses <all4senses@gmail.com>
2013-03-22 21:00:36 +0200

    AdRoll js add via template.php

--- modules/_GV/gv_misc/js/gv_add_adroll.js
+++ modules/_GV/gv_misc/js/gv_add_adroll.js
@@ -5,28 +5,28 @@
       
       console.log('test 1');
       
-      alert("hello!");
-      
-      adroll_adv_id = "YJ2QITGB3FFUPCENFTQBW7";
-      adroll_pix_id = "X2ZQHGK7VZDKNJRBCO6IOF";
-      (function () {
-      var oldonload = window.onload;
-      window.onload = function(){
-        __adroll_loaded=true;
-        var scr = document.createElement("script");
-        var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-        scr.setAttribute('async', 'true');
-        scr.type = "text/javascript";
-        scr.src = host + "/j/roundtrip.js";
-        
-        console.log('test 2');
-        
-        ((document.getElementsByTagName('head') || [null])[0] ||
-          document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-        if(oldonload){oldonload()}};
-      }());
-    
-    
+//      alert("hello!");
+//      
+//      adroll_adv_id = "YJ2QITGB3FFUPCENFTQBW7";
+//      adroll_pix_id = "X2ZQHGK7VZDKNJRBCO6IOF";
+//      (function () {
+//      var oldonload = window.onload;
+//      window.onload = function(){
+//        __adroll_loaded=true;
+//        var scr = document.createElement("script");
+//        var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+//        scr.setAttribute('async', 'true');
+//        scr.type = "text/javascript";
+//        scr.src = host + "/j/roundtrip.js";
+//        
+//        console.log('test 2');
+//        
+//        ((document.getElementsByTagName('head') || [null])[0] ||
+//          document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+//        if(oldonload){oldonload()}};
+//      }());
+//    
+//    
     }
   };
 
