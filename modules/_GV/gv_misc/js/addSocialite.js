6aefcb50a76fa6081bd180e3fd325a71a40c79ee
all4senses <all4senses@gmail.com>
2013-03-22 21:25:43 +0200

    AdRoll js add via template.php

--- modules/_GV/gv_misc/js/addSocialite.js
+++ modules/_GV/gv_misc/js/addSocialite.js
@@ -3,22 +3,18 @@
   Drupal.behaviors.gv_addSocialite = {
     attach: function (context, settings) {
        
-       console.log('teeeest');
-       
        var buttonLoaded = false;
 
        $('#header').one('mouseenter', function() {
           if (!buttonLoaded) {
             buttonLoaded = true;            
             Socialite.load('.social-buttons');
-            console.log('teeeest 1');
           }
         });
        $('.content.page').one('mouseenter', function() {
           if (!buttonLoaded) {
             buttonLoaded = true;            
             Socialite.load('.social-buttons');
-            console.log('teeeest 2');
           }
         });
         
@@ -26,7 +22,6 @@
           if (!buttonLoaded) {
             buttonLoaded = true;            
             Socialite.load('.social-buttons');
-            console.log('teeeest 3');
           }
         });
 
--- modules/_GV/gv_misc/js/gv_add_adroll.js
+++ modules/_GV/gv_misc/js/gv_add_adroll.js
@@ -1,3 +1,23 @@
+adroll_adv_id = "YJ2QITGB3FFUPCENFTQBW7";
+adroll_pix_id = "X2ZQHGK7VZDKNJRBCO6IOF";
+(function () {
+var oldonload = window.onload;
+window.onload = function(){
+  __adroll_loaded=true;
+  var scr = document.createElement("script");
+  var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+  scr.setAttribute('async', 'true');
+  scr.type = "text/javascript";
+  scr.src = host + "/j/roundtrip.js";
+
+  console.log('test 2');
+
+  ((document.getElementsByTagName('head') || [null])[0] ||
+    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+  if(oldonload){oldonload()}};
+}());
+
+/*
 (function ($) {
 
   Drupal.behaviors.gv_add_adroll = {
@@ -31,3 +51,4 @@
   };
 
 }(jQuery));
+*/
