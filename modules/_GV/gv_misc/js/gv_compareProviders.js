4bf5d1ca5d58592136533528b86f2648731d4006
all4senses <all4senses@gmail.com>
2013-04-19 19:27:59 +0300

    Provider options new set

--- modules/_GV/gv_misc/js/gv_compareProviders.js
+++ modules/_GV/gv_misc/js/gv_compareProviders.js
@@ -5,17 +5,19 @@
               
        $(".p-compare").click(function(e){
          console.log(e);
-         console.log(this);
+         //console.log(this);
          
-         console.log($(this).val());
+         //console.log($(this).val());
          
          console.log($(this));
          
          
          
-         console.log('xxx');
-         console.log($(this)[0]);
+
+         //console.log($(this)[0]);
          console.log($(this)[0].checked);
+         console.log($(this)[0].id);
+         console.log($(this)[0].name);
        });
 
 
