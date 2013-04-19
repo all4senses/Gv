9bece33fedd0a75442fb7b09f49bdb18588f21a3
all4senses <all4senses@gmail.com>
2013-04-19 19:34:41 +0300

    Provider options new set

--- modules/_GV/gv_misc/js/gv_compareProviders.js
+++ modules/_GV/gv_misc/js/gv_compareProviders.js
@@ -18,7 +18,7 @@
          console.log($(this)[0].checked);
          console.log($(this)[0].id);
          console.log($(this)[0].name);
-         return FALSE;
+         return false;
        });
 
 
