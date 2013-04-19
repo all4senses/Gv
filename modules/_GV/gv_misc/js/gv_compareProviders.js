d3c684d5cde917b050fb41cc49c8f7bd7ccbded8
all4senses <all4senses@gmail.com>
2013-04-19 21:21:17 +0300

    Provider options new set

--- modules/_GV/gv_misc/js/gv_compareProviders.js
+++ modules/_GV/gv_misc/js/gv_compareProviders.js
@@ -6,39 +6,33 @@
        
        $('#block-views-providers-block_top_business_cmp').append('<div class="compare-button" id="b1" style="display: none; height: 100px; width: 35px; background-color: red; position: absolute; right: -10px;top: 220px;"></div>');
            
+       $("#block-views-providers-block_top_business_cmp .compare-button#b1").click(function(e){
+         console.log('click');
+       });    
+           
        $(".p-compare").click(function(e){
-         console.log(e);
-         //console.log(this);
-         
-         //console.log($(this).val());
-         
-         console.log($(this));
-         
-         
-         
-
-         //console.log($(this)[0]);
-         console.log($(this)[0].checked);
-         console.log($(this)[0].id);
-         console.log($(this)[0].name);
-         //return false;
+         //console.log(e);
+         //console.log($(this));
+         //console.log($(this)[0].checked);
+         //console.log($(this)[0].id);
+         //console.log($(this)[0].name);
          
          var checked_count = 0;
          var checkboxes = $(".p-compare");
-         console.log(checkboxes);
+         //console.log(checkboxes);
          
          for (i = 0; i < checkboxes.length; ++i) {
            if (checkboxes[i].checked) {
-             console.log(i + ' is checked');
+             //console.log(i + ' is checked');
              ++checked_count;
            }
          }
          
          if (checked_count >= 3) {
-           console.log('more than 3');
+           //console.log('more than 3');
            for (i = 0; i < checkboxes.length; ++i) {
             if (!checkboxes[i].checked) {
-              console.log(i + ' disabled');
+              //console.log(i + ' disabled');
               checkboxes[i].disabled = true;
               checkboxes[i].title = 'You can select not more than 3 providers';
             }
@@ -53,11 +47,11 @@
          }
          
          if (!checked_count) {
-           console.log('not selected at all');
+           //console.log('not selected at all');
            $('.compare-button#b1').hide();
          }
          else {
-           console.log('selected something');
+           //console.log('selected something');
            $('.compare-button#b1').show();
          }
          
