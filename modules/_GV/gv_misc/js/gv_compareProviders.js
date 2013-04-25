9b766abdfc8f0468788ed524958ddd2bae0e46ef
all4senses <all4senses@gmail.com>
2013-04-25 21:08:08 +0300

    Compare page styling

--- modules/_GV/gv_misc/js/gv_compareProviders.js
+++ modules/_GV/gv_misc/js/gv_compareProviders.js
@@ -9,7 +9,8 @@
        //$('.bu-providers').append('<div class="compare-button" id="b1" style="display: none; height: 100px; width: 35px; position: absolute; right: -24px;top: 216px; z-index: 20;"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
        $('.bu-providers').append('<div class="compare-button" id="b1"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
          
-         
+       var block_height = $(".bu-providers tbody").height();
+       console.log('height = ' + block_height);
          
        $(".compare-button#b1").click(function(e){
          
