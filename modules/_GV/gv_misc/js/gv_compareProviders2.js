4905a597dca8f1a2172dccef1eadb1deaa0752d4
all4senses <all4senses@gmail.com>
2013-04-26 19:36:53 +0300

    Compare provider new functionality

---
+++ modules/_GV/gv_misc/js/gv_compareProviders2.js
@@ -0,0 +1,83 @@
+(function ($) {
+
+  Drupal.behaviors.gv_compareProviders = {
+    attach: function (context, settings) {
+           
+       var block_height = ($(".bu-providers tbody").height() / 2) + 15;
+       //console.log('height = ' + block_height);
+       
+       //$('#block-views-providers-block_top_business_cmp').append('<div class="compare-button" id="b1" style="display: none; height: 100px; width: 35px; position: absolute; right: -24px;top: 216px; z-index: 20;"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
+       //$('.bu-providers').append('<div class="compare-button" id="b1" style="display: none; height: 100px; width: 35px; position: absolute; right: -24px;top: 216px; z-index: 20;"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
+       $('.bu-providers').append('<div class="compare-button" id="b1" style="top: ' + block_height + 'px;"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
+         
+       
+         
+       $(".compare-button#b1").click(function(e){
+         
+         var checkboxes = $(".p-compare");
+         
+         var params = '';
+         for (i = 0; i < checkboxes.length; ++i) {
+           if (checkboxes[i].checked) {
+             //console.log(checkboxes[i].name);
+             if (params) {
+               params += ';' + checkboxes[i].name;
+             }
+             else {
+               params = checkboxes[i].name;
+             }
+           }
+         }
+         var url = 'http://getvoip.com/compare-providers?p=' + encodeURIComponent(params);
+         //console.log(url);
+         
+         top.location.href = url;
+         
+       });    
+           
+       $(".p-compare").click(function(e){
+         
+         var current_tr = $(this).parent().parent();
+         
+         console.log(current_tr);
+         console.log(e);
+         
+         var checked_count = 0;
+         var checkboxes = $(".p-compare");
+         
+         for (i = 0; i < checkboxes.length; ++i) {
+           if (checkboxes[i].checked) {
+             ++checked_count;
+           }
+         }
+         
+         if (checked_count >= 3) {
+           for (i = 0; i < checkboxes.length; ++i) {
+            if (!checkboxes[i].checked) {
+              checkboxes[i].disabled = true;
+              checkboxes[i].title = 'You can select not more than 3 providers';
+            }
+          }
+         }
+         else {
+           for (i = 0; i < checkboxes.length; ++i) {
+              checkboxes[i].disabled = false;
+              checkboxes[i].title = '';
+            }
+         }
+         
+         if (!checked_count) {
+           $('.compare-button#b1').hide();
+         }
+         else {
+           $('.compare-button#b1').show();
+         }
+         
+       });
+
+
+       
+    }
+  };
+
+}(jQuery));
