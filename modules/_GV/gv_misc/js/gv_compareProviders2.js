aa6eb5d7d291d264172cc31f5b4a09757f846578
all4senses <all4senses@gmail.com>
2013-04-26 21:03:10 +0300

    Compare provider new functionality

--- modules/_GV/gv_misc/js/gv_compareProviders2.js
+++ modules/_GV/gv_misc/js/gv_compareProviders2.js
@@ -47,7 +47,7 @@
          console.log(e);
          
          $('#b2').remove();
-         $(this).parent().parent().append('<div class="compare-button" id="b2" style="display: block !important; top: 0px;"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
+         $(this).parent().parent().append('<div class="compare-button" id="b2" style="display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-button2.png" style="height: 150px; cursor: pointer;"/></div>');
        
          
          var checked_count = 0;
