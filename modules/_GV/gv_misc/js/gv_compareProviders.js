30f03983fb6e18205e16d6b857e4800327e38436
all4senses <all4senses@gmail.com>
2013-05-01 23:52:08 +0300

    Providers logos sprites

--- modules/_GV/gv_misc/js/gv_compareProviders.js
+++ modules/_GV/gv_misc/js/gv_compareProviders.js
@@ -94,7 +94,7 @@
             if ($(this)[0].checked) {
               
               //var b_top = ($(this).parent().height()/2 - 35);
-              var b_top = ($(this).parent().parent().height()/2 - 35);
+              var b_top = ($(this).parent().parent().height()/2 - 35) - $(this).parent().parent().height()/2;
             
               //$(this).parent().parent().append('<div class="compare-button" id="b2" style="display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
               //$(this).parent().append('<div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
@@ -104,7 +104,7 @@
              //console.log(checkboxes_checked[0]);
              
               //var b_top = $(checkboxes_checked[checked_count - 1]).parent().height()/2 - 35;
-              var b_top = $(checkboxes_checked[checked_count - 1]).parent().parent().height()/2 - 35;
+              var b_top = $(checkboxes_checked[checked_count - 1]).parent().parent().height()/2 - 35 - $(this).parent().parent().height()/2;
               
              //$(checkboxes_checked[checked_count - 1]).parent().parent().append('<div class="compare-button" id="b2" style="display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
              //$(checkboxes_checked[checked_count - 1]).parent().append('<div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
