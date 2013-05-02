c0d2042beb564e56ff3d4ee92fa07c6ed7116fc2
all4senses <all4senses@gmail.com>
2013-05-02 17:58:24 +0300

    Providers logos sprites

--- modules/_GV/gv_misc/js/gv_compareProviders.js
+++ modules/_GV/gv_misc/js/gv_compareProviders.js
@@ -78,7 +78,7 @@
          
          if (!checked_count) {
            //$('.compare-button#b1').hide();
-           $('#b2').remove();
+           $('#b2').parent().remove();
          }
          else {
            
@@ -90,7 +90,7 @@
             //console.log(e);
 
             
-            $('#b2').remove();
+            $('#b2').parent().remove();
             if ($(this)[0].checked) {
               
               //var b_top = ($(this).parent().height()/2 - 35);
@@ -98,7 +98,7 @@
             
               //$(this).parent().parent().append('<div class="compare-button" id="b2" style="display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
               //$(this).parent().append('<div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
-              $(this).parent().append('<div style="padding: 0px; top: -30px; position: relative; width: 73px; margin: 0px; height: 1px;"><div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div></div>');
+              $(this).parent().append('<div style="padding: 0px; top: -30px; position: relative; width: 70px; margin: 0px; height: 1px;"><div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div></div>');
             }
             else {
              //console.log(checkboxes_checked[0]);
@@ -108,7 +108,7 @@
               
              //$(checkboxes_checked[checked_count - 1]).parent().parent().append('<div class="compare-button" id="b2" style="display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
              //$(checkboxes_checked[checked_count - 1]).parent().append('<div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div>');
-             $(checkboxes_checked[checked_count - 1]).parent().append('<div style="padding: 0px; top: -30px; position: relative; width: 73px; margin: 0px; height: 1px;"><div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div></div>');
+             $(checkboxes_checked[checked_count - 1]).parent().append('<div style="padding: 0px; top: -30px; position: relative; width: 70px; margin: 0px; height: 1px;"><div class="compare-button" id="b2" style="top: ' + b_top + 'px; display: block !important;"><img src="/sites/all/themes/gv_orange/css/images/compare-btn-next4.png"/></div></div>');
             }
             
             $(this).parent().css('position', 'relative');
