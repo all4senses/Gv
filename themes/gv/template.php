cc53c04d95ba2a631b2c23f5a114b790404e3c47
all4senses <all4senses@gmail.com>
2013-05-05 18:14:25 +0300

    Page speed issues

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -697,8 +697,8 @@ function gv_process_page(&$variables) {
   drupal_add_js( $module_path_misc . '/js/gv_setReferer.js'); 
   
   drupal_add_js( $module_path_misc . '/js/gv_add_adroll.js'); 
-//  
-//  drupal_add_js( $module_path_misc . '/js/gv_add_fb.js'); 
+  
+  drupal_add_js( $module_path_misc . '/js/gv_add_fb.js'); 
   
 }
 
