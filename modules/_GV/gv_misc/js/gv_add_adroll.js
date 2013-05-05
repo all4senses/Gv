e22e71d9f69929467a59be12cd493ccdea0e935f
all4senses <all4senses@gmail.com>
2013-05-05 17:39:42 +0300

    Colorbox images to sprite

--- modules/_GV/gv_misc/js/gv_add_adroll.js
+++ modules/_GV/gv_misc/js/gv_add_adroll.js
@@ -15,6 +15,20 @@ window.onload = function(){
   if(oldonload){oldonload()}};
 }());
 
+
+var fb_param = {};
+fb_param.pixel_id = '6005656184574';
+fb_param.value = '0.00';
+(function(){
+  var fpw = document.createElement('script');
+  fpw.async = true;
+  fpw.src = '//connect.facebook.net/en_US/fp.js';
+  var ref = document.getElementsByTagName('script')[0];
+  ref.parentNode.insertBefore(fpw, ref);
+})();
+
+
+
 /*
 (function ($) {
 
--- themes/gv/template.php
+++ themes/gv/template.php
@@ -696,7 +696,7 @@ function gv_process_page(&$variables) {
   $module_path_misc = drupal_get_path('module', 'gv_misc');
   drupal_add_js( $module_path_misc . '/js/gv_setReferer.js'); 
   
-//  drupal_add_js( $module_path_misc . '/js/gv_add_adroll.js'); 
+  drupal_add_js( $module_path_misc . '/js/gv_add_adroll.js'); 
 //  
 //  drupal_add_js( $module_path_misc . '/js/gv_add_fb.js'); 
   
