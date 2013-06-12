276054f3db3f9abcbf68120ccb5547d47d412ff6
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-06-12 17:43:29 +0300

    Floating share

--- modules/_GV/gv_misc/js/gv_floatingSocials.js
+++ modules/_GV/gv_misc/js/gv_floatingSocials.js
@@ -8,14 +8,17 @@
        
        console.log('length = ' + $(".float.share").length);
        
-       $(".float.share").stickyfloat({ 
-         duration: 200 
-         //stickToBottom: true
-         //,
-         //,startOffset: 300
-         //,offsetY: 450
-       });
-
+       if ($(".float.share").length) {
+         
+            $(".float.share").stickyfloat({ 
+              duration: 200 
+              //stickToBottom: true
+              //,
+              //,startOffset: 300
+              //,offsetY: 450
+            });
+        
+       }
        
     }
   };
