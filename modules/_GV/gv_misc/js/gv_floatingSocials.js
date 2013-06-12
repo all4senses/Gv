62a9258bb7ea1b680bb68391d9df8541029c7aeb
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-06-12 17:49:10 +0300

    Floating share

--- modules/_GV/gv_misc/js/gv_floatingSocials.js
+++ modules/_GV/gv_misc/js/gv_floatingSocials.js
@@ -2,12 +2,7 @@
 
   Drupal.behaviors.gv_floatingSocials = {
     attach: function (context, settings) {
-       
-       console.log('xxx');
-       console.log($(".float.share"));
-       
-       console.log('length = ' + $(".float.share").length);
-       
+      
        if ($(".float.share").length) {
          
             $(".float.share").stickyfloat({ 
