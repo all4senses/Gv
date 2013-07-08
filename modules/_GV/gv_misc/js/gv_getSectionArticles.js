fc47a645890ad66e2509199d6059c86ef08922b0
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-07-08 18:11:16 +0300

    Library Sections

---
+++ modules/_GV/gv_misc/js/gv_getSectionArticles.js
@@ -0,0 +1,40 @@
+(function ($) {
+
+  Drupal.behaviors.gv_getSectionArticles = {
+    attach: function (context, settings) {
+             
+          articles = '';
+          
+          $('#block-gv-blocks-library-sections .more-link a').click(function(){
+            console.log('clickkk');
+            console.log(this);
+            console.log($(this));
+          });
+          /*
+          (jQuery).ajax({
+            
+                url: '/get_sectionarticles', 
+                data: {
+                        op: 'get'
+                      }, 
+                    type: 'POST', 
+                    dataType: 'json'
+                    , 
+                    success: function(data) 
+                            { 
+                                if(!data.error) {
+                                    articles = data.articles;
+                                    console.log('Articles arrived: ' + articles);
+                                }
+                                return false;
+                            } 
+                    
+            }); // end of (jQuery).ajax
+
+            */
+
+       
+    }
+  };
+
+}(jQuery));
