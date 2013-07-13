ecff329ff90710361e8da0bdbcc310444c012177
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-07-13 18:30:27 +0300

    Library page sections

--- modules/_GV/gv_misc/js/gv_getSectionArticles.js
+++ modules/_GV/gv_misc/js/gv_getSectionArticles.js
@@ -13,52 +13,62 @@
             this_button = this;
             
             console.log($(this_button));
-          
-            this_button.opened = false;
             
-//            console.log('clickkk');
-            //console.log(this);
-            //console.log($(this));
             
-            //console.log($(this)[0].className);
-          
+            if (!this_button.opened) {
+              
+                  console.log('NOT opened!');
+                  this_button.opened = false;
+
+      //            console.log('clickkk');
+                  //console.log(this);
+                  //console.log($(this));
+
+                  //console.log($(this)[0].className);
+
+                  console.log($(this_button));
+
+                  pr1 = $(this).parent().parent();
+                  //console.log(pr1);
+
+                  //console.log($(this).prev().prev());
+
+                  (jQuery).ajax({
+
+                      url: '/get-sectionarticles', 
+                      data: {
+                              op: 'get',
+                              section: $(this)[0].className
+                            }, 
+                          type: 'POST', 
+                          dataType: 'json'
+                          , 
+                          success: function(data) 
+                                  {   
+                                      if(!data.error && data.articles) {
+                                          //pr1 = $(this).parent().parent();
+                                          //console.log(pr1);
+                                          pr1.find('.articles').html(data.articles);
+                                          //console.log('Articles arrived: ' + data.articles);
+                                          this_button.opened = true;
+                                          console.log($(this_button));
+                                      }
+                                      return false;
+                                  } 
+
+                  }); // end of (jQuery).ajax
+                  
+          }
+          else {
+            console.log('opened!');
             console.log($(this_button));
+          }
             
-            pr1 = $(this).parent().parent();
-            //console.log(pr1);
+          return false;
+        });
 
-            //console.log($(this).prev().prev());
-            
-            (jQuery).ajax({
-            
-                url: '/get-sectionarticles', 
-                data: {
-                        op: 'get',
-                        section: $(this)[0].className
-                      }, 
-                    type: 'POST', 
-                    dataType: 'json'
-                    , 
-                    success: function(data) 
-                            {   
-                                if(!data.error && data.articles) {
-                                    //pr1 = $(this).parent().parent();
-                                    //console.log(pr1);
-                                    pr1.find('.articles').html(data.articles);
-                                    //console.log('Articles arrived: ' + data.articles);
-                                    //this_button.opened = true;
-                                    console.log($(this_button));
-                                }
-                                return false;
-                            } 
-                    
-            }); // end of (jQuery).ajax
-
-            return false;
-          });
           
           
-            
 
        
     }
