7e046236106fe46723c1f584b957ff3b93b643d3
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-06 22:08:04 +0700

    Related articles

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -374,8 +374,12 @@
                       
                       // Related articles.
                       if (!empty($node->related_articles)) {
-                        //echo '<div class="related_articles">' . $node->related_articles[0] . '</div>';
                         dpm($node->related_articles);
+                        echo '<div class="related_articles">';
+                          foreach ($node->related_articles as $nid => $article) {
+                            echo '<div><img src="' . $article->field_main_image_value['src_themed'] . '" />', l($article->title, 'node/' . $nid) . '</div>';
+                          }
+                        echo '</div>';
                       }
                       
                       
