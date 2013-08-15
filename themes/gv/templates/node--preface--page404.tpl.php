7348a7abc374d8922a4a519c760c45fb1b0c1c93
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-16 03:15:19 +0700

    ttp issue work around

--- themes/gv/templates/node--preface--page404.tpl.php
+++ themes/gv/templates/node--preface--page404.tpl.php
@@ -10,6 +10,7 @@
           // Fix ttp:// urls, i don't understand where they are coming from, but seems that via msie6.0
           if (strpos($_GET['destination'], 'ttp://') === 0) {
             watchdog('My test 404', print_r($_GET, TRUE) . ' '. print_r($_SERVER, TRUE) . ' ' . print_r($user, TRUE), NULL, WATCHDOG_WARNING);
+            drupal_goto(str_replace('ttp://getvoip', '', $_GET['destination']));
           }
           // Hide comments, tags, and links now so that we can render them later.
           hide($content['comments']);
