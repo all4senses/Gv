845f0e610a8f111f5ab8a8040c103747849717ca
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-18 18:35:07 +0700

    Related articles block

--- modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
+++ modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
@@ -3,16 +3,16 @@
   Drupal.behaviors.gv_relatedArticlesRotator = {
     attach: function (context, settings) {
        
-      //$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true); // for a Version of UI > 1.9
-      $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 30000, true);
+      //$("#rotator > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true); // for a Version of UI > 1.9
+      $("#rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 30000, true);
        
       // Pause on hover.
-      $("#featured").hover(  
+      $("#rotator").hover(  
           function() {  
-            $("#featured").tabs("rotate",0,true);  
+            $("#rotator").tabs("rotate",0,true);  
           },  
           function() {  
-            $("#featured").tabs("rotate",30000,true);  
+            $("#rotator").tabs("rotate",30000,true);  
           }  
       );  
        
