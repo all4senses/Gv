c1569a67c086dce83a09813fd21360ed242484ac
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-20 01:21:10 +0700

    Related articles block

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -46,6 +46,11 @@
               if ($_GET['q'] == 'node/1293') {
                 echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_nocut_wg.png" alt="GetVoIP" title="GetVoIP" />';
               }
+              else 
+              {
+                //echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/getvoip-logo3.png" alt="GetVoIP" title="GetVoIP" />';
+                echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_cut_wgs.png" alt="GetVoIP" title="GetVoIP" />';
+              }
             }
             else 
             {
