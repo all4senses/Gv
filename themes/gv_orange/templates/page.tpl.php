ba1142eba8a701bef6c27540e4c402eabd9c16ea
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-22 04:06:26 +0700

    New logo test

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -47,7 +47,7 @@
                 echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_nocut_wg.png" alt="GetVoIP" title="GetVoIP" />';
               }
               elseif ($_GET['q'] == 'node/1568') {
-                echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_nocut_wgs2.png" alt="GetVoIP" title="GetVoIP" />';
+                echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_cut_wgs2.png" alt="GetVoIP" title="GetVoIP" />';
               }
               else 
               {
