b5800acf89c7d0baf854e11771e4da9437a430ef
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-23 06:54:35 +0700

    Phone page share block

--- modules/_GV/gv_misc/js/gv_popup_share_links.js
+++ modules/_GV/gv_misc/js/gv_popup_share_links.js
@@ -1,13 +1,12 @@
 (function ($) {
 
-  Drupal.behaviors.gv_colorbox_share_link = {
+  Drupal.behaviors.gv_popup_share_link = {
     attach: function (context, settings) {
        
-       $(".share-static a").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+       // Colorbox doesn't work for some share providers.
+       ////$(".share-static a").colorbox({iframe:true, innerWidth:425, innerHeight:344});
        
        
-       
-       // GV track click video link.
        
        $(".share-static a").click(function(){
          
@@ -16,7 +15,8 @@
          //console.log('href = ' + $(this).attr('href'));
          //console.log('title = ' + $(this).attr('title'));
 
-          
+          // Open links in a new small poopup windows.
+          // 
           //window.open("'" + $(this).attr('href')+ "'", 'ShareThis','toolbar=0,status=0,width=626,height=436'); 
           window.open($(this).attr('href'), 'ShareThis','toolbar=0,status=0,width=626,height=436,top=100,left=100'); 
           return false;
