0d3c32f51e3807bb9aaabc2dffcba53dfd50f5b0
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-24 07:05:26 +0700

    Filter submission from some spammers phoe number

--- themes/gv_orange/templates/page--compare_providers.tpl.php
+++ themes/gv_orange/templates/page--compare_providers.tpl.php
@@ -78,12 +78,7 @@
             
           ?>
           <a id="main-content"></a>
-          
-          <?php /* if (!$is_front && $title): ?>
-            <?php print render($title_prefix); ?>
-            <h1 class="title" id="page-title"><?php print $title; ?></h1>
-            <?php print render($title_suffix); ?>
-          <?php endif; */ ?>
+
           
           <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
           <?php print render($page['help']); ?>
