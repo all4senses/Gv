259c072cfb78e4ef37dd7a9551157b7049a9769a
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-09-08 02:04:01 +0700

    Similar posts block

--- modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
+++ modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
@@ -17,31 +17,44 @@
             $("#rotator").tabs("rotate",30000,true);  
             //$("#rotator").tabs("rotate",3000,true);  
           }  
-      );  
+      );
         
-      $('.sidebar .block .content #rotator a.title').mouseenter(function(){
-        console.log($(this).css('height'));
-        console.log($(this));
-        console.log($(this)[0].parentElement.id);
         
-        $(this)[0].original_title_height = $(this).css('height');
-        console.log($(this));
         
-        //original_title_height[$(this)[0].parentElement.id] = $(this).css('height');
         
-        //console.log($(this)[0].parentElement.id);
-        //console.log(original_title_height[$(this)[0].parentElement.id]);
-
-        //jQuery('.sidebar .block .content #rotator a.title').css('height');
-        //console.log(original_height);
-        //console.log(original_title_height[this.parentElement.id]);
+      $(".sidebar .block .content #rotator a.title").hover(  
+          function() {  
+            $(this)[0].original_title_height = $(this).css('height');
+            $(this).css('height', 'inherit');
+          },  
+          function() {  
+            $(this).css('height') = $(this)[0].original_title_height;
+          }  
+      );
         
-        //console.log(original_title_height);
-
-        //jQuery('.sidebar .block .content #rotator a.title').css('height', 'inherit');
-        ///this.css('height', 'inherit');
-      });
-      
+//      $('.sidebar .block .content #rotator a.title').mouseenter(function(){
+//        console.log($(this).css('height'));
+//        console.log($(this));
+//        console.log($(this)[0].parentElement.id);
+//        
+//        $(this)[0].original_title_height = $(this).css('height');
+//        console.log($(this));
+//        
+//        //original_title_height[$(this)[0].parentElement.id] = $(this).css('height');
+//        
+//        //console.log($(this)[0].parentElement.id);
+//        //console.log(original_title_height[$(this)[0].parentElement.id]);
+//
+//        //jQuery('.sidebar .block .content #rotator a.title').css('height');
+//        //console.log(original_height);
+//        //console.log(original_title_height[this.parentElement.id]);
+//        
+//        //console.log(original_title_height);
+//
+//        //jQuery('.sidebar .block .content #rotator a.title').css('height', 'inherit');
+//        ///this.css('height', 'inherit');
+//      });
+//      
       
 
 
