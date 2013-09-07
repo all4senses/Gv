92412501cb985cfb3b8f0b75c3623e450f2a4a48
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-09-08 02:18:53 +0700

    Similar posts block

--- modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
+++ modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
@@ -21,49 +21,20 @@
         
         
         
-        
+      // Show full title on hover if it was more than 3 lines and has been cutted to two lines.
       $(".sidebar .block .content #rotator a.title").hover(  
           function() {  
             $(this)[0].original_title_height = $(this).css('height');
             $(this).css('height', 'inherit');
-            console.log( $(this).css('height'));
             
             $('#block-gv-blocks-sidebar-related-articles.block .content')[0].original_title_height = $('#block-gv-blocks-sidebar-related-articles.block .content').css('height');
             $('#block-gv-blocks-sidebar-related-articles.block .content').css('height', 'inherit');
-            
-            
           },  
           function() {  
             $(this).css('height', $(this)[0].original_title_height);
-            console.log( $(this).css('height'));
-            
             $('#block-gv-blocks-sidebar-related-articles.block .content').css('height', $('#block-gv-blocks-sidebar-related-articles.block .content')[0].original_title_height);
           }  
       );
-        
-//      $('.sidebar .block .content #rotator a.title').mouseenter(function(){
-//        console.log($(this).css('height'));
-//        console.log($(this));
-//        console.log($(this)[0].parentElement.id);
-//        
-//        $(this)[0].original_title_height = $(this).css('height');
-//        console.log($(this));
-//        
-//        //original_title_height[$(this)[0].parentElement.id] = $(this).css('height');
-//        
-//        //console.log($(this)[0].parentElement.id);
-//        //console.log(original_title_height[$(this)[0].parentElement.id]);
-//
-//        //jQuery('.sidebar .block .content #rotator a.title').css('height');
-//        //console.log(original_height);
-//        //console.log(original_title_height[this.parentElement.id]);
-//        
-//        //console.log(original_title_height);
-//
-//        //jQuery('.sidebar .block .content #rotator a.title').css('height', 'inherit');
-//        ///this.css('height', 'inherit');
-//      });
-//      
       
 
 
