2a9bef58c41f44d6b3ac7afe23cb6dec4654ce8c
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-09-08 01:21:42 +0700

    Similar posts block

--- modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
+++ modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
@@ -18,6 +18,21 @@
             //$("#rotator").tabs("rotate",3000,true);  
           }  
       );  
+        
+      $('.sidebar .block .content #rotator a.title').click(function(){
+        original_title_height[this.parentElement.id] = this.css('height');
+        //jQuery('.sidebar .block .content #rotator a.title').css('height');
+        //console.log(original_height);
+        //console.log(original_title_height[this.parentElement.id]);
+        console.log(original_title_height);
+
+        //jQuery('.sidebar .block .content #rotator a.title').css('height', 'inherit');
+        ///this.css('height', 'inherit');
+      });
+      
+      
+
+
        
     }
   };
