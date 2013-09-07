5847e49a57b6a00bf11a55d6fa832c5b73472a94
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-09-08 02:34:23 +0700

    Similar posts block

--- modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
+++ modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
@@ -5,8 +5,8 @@
        
       //$("#rotator > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true); // for a Version of UI > 1.9
       
-      $("#rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 20000, true);
-      //$("#rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 3000, true);
+      //$("#rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 20000, true);
+      $("#rotator").tabs({fxSlide: true }).tabs("rotate", 20000, true);
       
       // Pause on hover.
       $("#rotator").hover(  
