844345876e25549db4b0e32c585a554ae24ba242
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-09-08 16:57:11 +0700

    Similar posts block

--- modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
+++ modules/_GV/gv_misc/js/gv_relatedArticlesRotator.js
@@ -27,7 +27,7 @@
       
       // Works.
        $("#rotator").tabs({fx: [
-                                {opacity: "toggle", left: ["250", 'easeInOutBounce'], duration: 50}, // will be used for hide
+                                {opacity: "toggle", left: ["250", 'easeInOutBounce'], duration: 100}, // will be used for hide
                                 {opacity: "toggle", left: ["0", 'easeInOutBounce'], duration: 1500} // will be used for show
                               ]
                     }).tabs("rotate", 7000, true);
@@ -41,7 +41,7 @@
             //$("#rotator").tabs("rotate",0,true);  
             
             $("#rotator").tabs({fx: [
-                                {opacity: "toggle", left: ["250", 'easeInOutBounce'], duration: 50}, // will be used for hide
+                                {opacity: "toggle", left: ["250", 'easeInOutBounce'], duration: 100, complete: function() {alert('!!!');}  }, // will be used for hide
                                 {opacity: "toggle", left: ["0", 'easeInOutBounce'], duration: 1500} // will be used for show
                               ]
                     }).tabs("rotate", 0, true);
@@ -51,7 +51,7 @@
             //$("#rotator").tabs("rotate",77000,true);  
             
             $("#rotator").tabs({fx: [
-                                {opacity: "toggle", left: ["250", 'easeInOutBounce'], duration: 50}, // will be used for hide
+                                {opacity: "toggle", left: ["250", 'easeInOutBounce'], duration: 100}, // will be used for hide
                                 {opacity: "toggle", left: ["0", 'easeInOutBounce'], duration: 1500} // will be used for show
                               ]
                     }).tabs("rotate", 7000, true);
