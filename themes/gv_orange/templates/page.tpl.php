5239d05dbed00f1f92b3d7e1843954e1430327d7
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-09-15 22:40:50 +0700

    Block As featured in

--- themes/gv_orange/templates/page.tpl.php
+++ themes/gv_orange/templates/page.tpl.php
@@ -42,7 +42,8 @@
             else 
             */
           
-            if ($user->uid) {
+//            if ($user->uid) 
+              {
               /*
               if ($_GET['q'] == 'node/1293') {
                 echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_nocut_wg.png" alt="GetVoIP" title="GetVoIP" />';
@@ -58,11 +59,11 @@
               */
               echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/getvoip-logo4.png" alt="GetVoIP" title="GetVoIP" />';
             }
-            else 
-            {
-              //echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/getvoip-logo3.png" alt="GetVoIP" title="GetVoIP" />';
-              echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_cut_wgs.png" alt="GetVoIP" title="GetVoIP" />';
-            }
+//            else 
+//            {
+//              //echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/getvoip-logo3.png" alt="GetVoIP" title="GetVoIP" />';
+//              echo '<img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv_logo_cut_wgs.png" alt="GetVoIP" title="GetVoIP" />';
+//            }
           ?>
         </a>
         <div class="descr">
