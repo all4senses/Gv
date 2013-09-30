c796640225438d2c54cb9f08d0ae66a914188acf
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-10-01 01:52:20 +0700

    Block latest startnow articles in the body - to two columns

--- themes/gv/templates/views-view-unformatted--articles--blockbody-latestart.tpl.php
+++ themes/gv/templates/views-view-unformatted--articles--blockbody-latestart.tpl.php
@@ -14,8 +14,15 @@ dpm('$half_count = ' . $half_count);
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
+<div class="col one">  
 <?php foreach ($rows as $id => $row): ?>
+  <?php 
+    if ($id == $half_count) {
+      echo '</div><div class="col two">';
+    }
+  ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
+</div>
