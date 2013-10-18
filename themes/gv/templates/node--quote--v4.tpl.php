be75d66dea835362642564944fbfa2604eddcd5b
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-10-18 13:15:29 +0700

    Quote LP v4

--- themes/gv/templates/node--quote--v4.tpl.php
+++ themes/gv/templates/node--quote--v4.tpl.php
@@ -1,7 +1,3 @@
-<div id="v4">
-  <div id="quote-content">
-
-    
   <?php
     if ($node->title == 'Request a Quote page v4 Final') {
       $path_to_custom_js = drupal_get_path('module', 'gv_blocks') . '/js/';
@@ -16,131 +12,177 @@
       $initialQuotePage_node = $node;
     }
   ?>
+
+
+
+
+<div id="v4">
+  <div id="quote-content">
+
     
-  <?php echo '<div class="field-name-field-q-image">', theme('image', array( 'path' => $initialQuotePage_node->field_q_image['und'][0]['uri'], 'alt' => $initialQuotePage_node->field_q_image['und'][0]['alt'], 'title' => $initialQuotePage_node->field_q_image['und'][0]['title'])), '</div>'; ?>
-  <div id="gv-logo">
-    <?php 
-      echo '<div class="logo"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv-logo-lp4.jpg" alt="GetVoIP" title="GetVoIP" /></div>'; 
-    ?>
-    <h3>The Authority on VoIP Service Selection</h3>
-  </div>
+      <div class="full">
 
-  <div class="bottom-clear"></div> 
+          <div id="gv-logo">
+            <?php 
+              echo '<div class="logo"><img src="http://getvoip.com/sites/all/themes/gv_orange/css/images/gv-logo-lp4.jpg" alt="GetVoIP" title="GetVoIP" /></div>'; 
+            ?>
+            <h3>The Authority on VoIP Service Selection</h3>
+          </div>
 
-  
- 
-  
-  
-  <div class="content<?php echo ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final' ? ' final' : '')?>"<?php print $content_attributes; ?>>
-
-
-    <div class="left">
-      <?php 
-          ?> 
-            <h2>Save Up To 80% On Your<br/> Business Phone System!</h2>
-            <div class="caption">Benefits & Features:</div>
-            <div class="text bullet v1"><strong>Lowest Rates & Exclusive Discounts</strong></div>
-            <div class="text bullet v2"><strong>Personalized Quotes From Top Providers</strong></div>
-            <div class="text bullet v3"><strong>No Contracts Or Set-Up Fees</strong></div>
-            <div class="text bullet v4"><strong>Intuitive Feature-Loaded HD VoIP Phones</strong></div>
-            
-            <div class="ps"><span>6000+ Submissions -</span> Average saving of $180 per month</div>
-
-    </div>
-
-  </div>
-  
-    <?php if ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final'): ?>
-        
-        <div class="right final">
-           <?php  
-           echo '<a class="guide" href="/pdfs/SMBVOIPAdvantagesWhitePaper.pdf" target="_blank">Download Your Free VoIP Buyer\'s Guide<img src="/sites/all/themes/gv_orange/css/images/click-here-button.png" /></a><div class="quote-final">' . t('<p><strong>Thank you</strong> for taking your time to complete our form. A VoIP Expert will be contacting you shortly to provide you with a personalized VoIP Service quote.</p><p>In the meantime, you can gain a great deal of VoIP information right here at <a href="http://getvoip.com">GetVoIP.com</a></p>') . '</div>';
-           ?>
-        </div>
-    <?php else: ?>
-
-      <div class="right">
-        
-        <div id="right-bottom">
-          <?php echo gv_blocks_get_requestQuoteForPage_v4(); ?>
-          <div class="bottom-clear"></div> 
-        </div>
       </div>
-    
-    <form action="http://ww3.vocalocity.com/l/7772/2012-09-28/36c856" method="post"></form>
+  
+
+<div class="bottom-clear"></div> 
+  
+  
+
+
+      <div class="full">
+
+        <div class="content<?php echo ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final' ? ' final' : '')?>"<?php print $content_attributes; ?>>
+
+              <?php echo '<div class="field-name-field-q-image">', theme('image', array( 'path' => $initialQuotePage_node->field_q_image['und'][0]['uri'], 'alt' => $initialQuotePage_node->field_q_image['und'][0]['alt'], 'title' => $initialQuotePage_node->field_q_image['und'][0]['title'])), '</div>'; ?>
+
+              <div class="left">
+                      <h2>Save Up To 80% On Your<br/> Business Phone System!</h2>
+                      <div class="caption">Benefits & Features:</div>
+                      <div class="text bullet v1"><strong>Lowest Rates & Exclusive Discounts</strong></div>
+                      <div class="text bullet v2"><strong>Personalized Quotes From Top Providers</strong></div>
+                      <div class="text bullet v3"><strong>No Contracts Or Set-Up Fees</strong></div>
+                      <div class="text bullet v4"><strong>Intuitive Feature-Loaded HD VoIP Phones</strong></div>
+
+                      <div class="ps"><span>6000+ Submissions -</span> Average saving of $180 per month</div>
+              </div>
+
+        </div>
+
+        <?php if ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final'): ?>
+
+            <div class="right final">
+              <?php  
+              echo '<a class="guide" href="/pdfs/SMBVOIPAdvantagesWhitePaper.pdf" target="_blank">Download Your Free VoIP Buyer\'s Guide<img src="/sites/all/themes/gv_orange/css/images/click-here-button.png" /></a><div class="quote-final">' . t('<p><strong>Thank you</strong> for taking your time to complete our form. A VoIP Expert will be contacting you shortly to provide you with a personalized VoIP Service quote.</p><p>In the meantime, you can gain a great deal of VoIP information right here at <a href="http://getvoip.com">GetVoIP.com</a></p>') . '</div>';
+              ?>
+            </div>
+        <?php else: ?>
+
+            <div class="right">
+
+              <div id="right-bottom">
+                <?php echo gv_blocks_get_requestQuoteForPage_v4(); ?>
+                <div class="bottom-clear"></div> 
+              </div>
+            </div>
+
+          <form action="http://ww3.vocalocity.com/l/7772/2012-09-28/36c856" method="post"></form>
+
+
+          <?php endif; ?>
+
+          <?php
+            echo render($content['metatags']);
+          ?>
+          <div class="bottom-clear"></div> 
+
+      </div> <!-- of Full -->
+
+
+
+      <div class="full">
+        
+          <div id="upper-block">
+            <h1>We identify and rank the best business VoIP providers and phone systems.</h1>
+
+            <div class="quotes one">
+              <div class="text">"GetVoIP.com stands out in the online VoIP implementation space by providing expert analysis and unparalleled information along with hands-on reviews and feature explanations, which ultimately translates into quality prospects and high conversion rates."</div>
+              <div class="source">- Chris Rabbu, VP of Marketing at Vocalocity, Inc.</div>
+            </div>
+            <div class="bottom-clear"></div> 
+          </div>
+
+      </div> <!-- of Full -->
+
+      
       
 
-    <?php endif; ?>
+      
+      <div class="full">
 
-    <?php
-      echo render($content['metatags']);
-    ?>
-    <div class="bottom-clear"></div> 
-  
-  
+          <div id="brands">
+            <h2><?php echo 'Supported by all major VoIP Companies in the country:'; ?></h2>
+            <?php
+                if (isset($initialQuotePage_node->field_q_image2['und'][0])) {
+                  echo '<ul id="mycarousel" class="jcarousel-skin-tango">';
+                  foreach ($initialQuotePage_node->field_q_image2['und'] as $brand) {
+                    echo '<li>', '<table><tbody><tr><td>', 
+                            theme('image_style', array( 'path' =>  $brand['uri'], 'style_name' => 'thumbnail', 'alt' => $brand['alt'], 'title' => $brand['title'])),
+                        '</td></tr></tbody></table>', '</li>'; 
+                  }
+                  echo '</ul>';
+                }
 
-  <div id="upper-block">
-    <h1>We identify and rank the best business VoIP providers and phone systems.</h1>
-    
-    <div class="quotes one">
-      <div class="text">"GetVoIP.com stands out in the online VoIP implementation space by providing expert analysis and unparalleled information along with hands-on reviews and feature explanations, which ultimately translates into quality prospects and high conversion rates."</div>
-      <div class="source">- Chris Rabbu, VP of Marketing at Vocalocity, Inc.</div>
-    </div>
-    <div class="bottom-clear"></div> 
-  </div>
+            ?>
 
-  <div id="brands">
-    <h2><?php echo 'Supported by all major VoIP Companies in the country:'; ?></h2>
-    <?php
-        if (isset($initialQuotePage_node->field_q_image2['und'][0])) {
-          echo '<ul id="mycarousel" class="jcarousel-skin-tango">';
-          foreach ($initialQuotePage_node->field_q_image2['und'] as $brand) {
-            echo '<li>', '<table><tbody><tr><td>', 
-                    theme('image_style', array( 'path' =>  $brand['uri'], 'style_name' => 'thumbnail', 'alt' => $brand['alt'], 'title' => $brand['title'])),
-                 '</td></tr></tbody></table>', '</li>'; 
-          }
-          echo '</ul>';
-        }
+            <div class="bottom-clear"></div> 
+          </div>
         
-     ?>
-    
-    <div class="bottom-clear"></div> 
-  </div>
+      </div> <!-- of Full -->        
 
-
-<?php ?>
   
   
-  <?php ?>
-  <div id="bottom">
-    <div class="text">
-      <h3><?php echo $initialQuotePage_node->q_data['bottom_text']['left_title']; ?></h3>
-      <div><?php echo $initialQuotePage_node->q_data['bottom_text']['left_text']; ?></div>
-    </div>
-    <div class="text">
-      <h3><?php echo $initialQuotePage_node->q_data['bottom_text']['center_title']; ?></h3>
-      <div><?php echo $initialQuotePage_node->q_data['bottom_text']['center_text']; ?></div>
-    </div>
-    <div class="text last">
-      <h3><?php echo $initialQuotePage_node->q_data['bottom_text']['right_title']; ?></h3>
-      <div><?php echo $initialQuotePage_node->q_data['bottom_text']['right_text']; ?></div>
-    </div>
+
+      
+      <div class="full">
+
+          <div id="bottom">
+            <div class="text">
+              <h3><?php echo $initialQuotePage_node->q_data['bottom_text']['left_title']; ?></h3>
+              <div><?php echo $initialQuotePage_node->q_data['bottom_text']['left_text']; ?></div>
+            </div>
+            <div class="text">
+              <h3><?php echo $initialQuotePage_node->q_data['bottom_text']['center_title']; ?></h3>
+              <div><?php echo $initialQuotePage_node->q_data['bottom_text']['center_text']; ?></div>
+            </div>
+            <div class="text last">
+              <h3><?php echo $initialQuotePage_node->q_data['bottom_text']['right_title']; ?></h3>
+              <div><?php echo $initialQuotePage_node->q_data['bottom_text']['right_text']; ?></div>
+            </div>
 
 
-    <div class="bottom-clear"></div>
-  </div>
+            <div class="bottom-clear"></div>
+          </div>
 
-<?php ?>
+      </div> <!-- of Full --> 
+      
 
-  <?php ?>
   
-  <footer>
-    <div class="copy">© 2010-2013 GetVoIP.com All Rights Reserved <br/> BizMedia Central LLC | New York, NY</div>
-  </footer>
-          
-<?php ?>
-  </div>
-</div>
+      <footer id="footer" role="contentinfo" class="clearfix<?php echo $hide_follow_us_links ? ' no-follow-us-links' : '' ?>">
+        <div id="footer-inside">
+
+          <?php
+              if (!$hide_follow_us_links) {
+                echo '<div id="block-gv-blocks-follow-links"><div class="follow-us">Follow Us</div>', gv_blocks_get_headerLinks(), '</div>';
+              }
+              echo render($page['footer']);
+          ?>
+
+
+          <div class="c">GetVoIP.com is an independent provider comparison and shoppers guide, offering unbiased consumer reviews. We monetize from advertisers and affiliates. This does not influence the rankings and reviews on our website. 
+            <div>© 2012-2013 GetVoIP.com | All Rights Reserved</div>
+          </div>
+
+        </div>
+      </footer> <!-- /#footer -->
+  
+  
+  
+  
+  
+
+  </div> <!-- End of <div id="quote-content"> -->
+  
+</div> <!-- End of <div id="v4"> -->
+
+
 
 <?php if ($_SERVER['REDIRECT_URL'] == '/compare-voip-solutions-final'): /* If we are on the final page, after the submission */ ?> 
 
