9c240fbf5c2a5b4a0719f840787c34798b85f49c
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-10-20 16:39:25 +0700

    Quote LP v4

--- themes/gv/templates/node--quote--v4.tpl.php
+++ themes/gv/templates/node--quote--v4.tpl.php
@@ -96,7 +96,7 @@
             <h1>We identify and rank the best business VoIP providers and phone systems.</h1>
 
             <div class="quotes one">
-              <div class="text">"GetVoIP.com stands out in the online VoIP implementation space by providing expert analysis and unparalleled information along with hands-on reviews and feature explanations, which ultimately translates into quality prospects and high conversion rates."</div>
+              <div class="text">"GetVoIP stands out in the business VoIP implementation space by providing expert analysis and unparalleled information along with hands-on reviews and feature explanations, which ultimately translates into an easy to setup and manage phone system."</div>
               <div class="source">Chris Rabbu, VP of Marketing at Vocalocity, Inc.</div>
             </div>
             <div class="bottom-clear"></div> 
