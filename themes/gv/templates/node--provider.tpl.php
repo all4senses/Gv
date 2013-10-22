d80dbc033c3611ee8840f544800800a66a6c48ad
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-10-23 00:31:44 +0700

    Google reviews snippet fix

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -160,7 +160,7 @@
                         //$stars_overall = theme('gv_misc_fivestar_static', array('rating' => $node->gv_rating_overall * 20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css')));
                         //echo '<span class="title">' . $provider_name . ' Rated: </span>' . $stars_overall . '<span class="count" content="' . $node->gv_rating_overall . '" property="v:rating">' . $node->gv_rating_overall . '</span> out of 5'; 
                         
-                        if ($node->gv_voters === 1) {
+                        if ($node->gv_voters == 1) {
                           echo '<span class="title">' . $provider_name . ' Rated: </span>
                           <span class="count" rel="v:rating">
                             <span typeof="v:Rating">
