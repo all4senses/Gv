971cb1159e4680b7db3622e68e175cf06dedc504
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-10-25 01:38:35 +0700

    Quote LP v4 - Exit Intent

---
+++ modules/_GV/gv_misc/js/gv_exitIntent_lpV4.js
@@ -0,0 +1,37 @@
+(function ($) {
+
+
+  // http://stackoverflow.com/questions/1617757/detect-when-mouse-leaves-via-top-of-page-with-jquery
+
+  Drupal.behaviors.gv_exitIntent_lpV4 = {
+    attach: function (context, settings) {
+       
+        var done = null;
+       
+        $(document).bind("mouseleave", function(e)
+        {
+            console.log(e.pageY);
+            
+            //if (!done && e.pageY <= 1)
+            if (!done && e.pageY - $(window).scrollTop() <= 1)
+            {    
+                done = true;
+                //now = new Date();           
+                //for (i=0; i < times.length; i++)
+                {
+                    //if (now.getTime() > times[i][0] && now.getTime() < times[i][1])
+                    {
+                        //$.fn.colorbox({iframe:true, width:650, height:600, href: "get-popup-on-exit-intent", open: true});  
+                        $.fn.colorbox({iframe:true, width:650, height:600, href: 'get/iframe/exitIntent_lpV4', open: true});  
+                        
+                        //alert('test');
+                    }    
+                }
+            }
+        });
+       
+       
+    }
+  };
+
+}(jQuery));
