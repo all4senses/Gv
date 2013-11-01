1310f6a23221ed126f625abbad432ea9bf3d51b1
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-02 01:08:18 +0700

    Sip page wide chart

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -959,8 +959,9 @@ function gv_preprocess_views_view(&$vars) {
 function gv_preprocess_html(&$variables) {
   
   if (!empty($variables['page']['sidebar_second_below_chart']) && $key = array_search('no-sidebars', $variables['classes_array']) !== FALSE) {
-    $variables['classes_array'][] = 'one-sidebar sidebar-second';
+    dpm($key);
     unset($variables['classes_array'][$key]);
+    $variables['classes_array'][] = 'one-sidebar sidebar-second';
   }
   
   dpm($variables['classes_array']);
