f6e9d7168e221fd7a0c4162439d257b58f633be9
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-02 00:25:58 +0700

    Sip page wide chart

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -955,24 +955,27 @@ function gv_preprocess_views_view(&$vars) {
 */
 
 
-/*
+
 function gv_preprocess_html(&$variables) {
+  
+  dpm($variables['classes_array']);
+  
   //dpm($variables);
   //global $user;
   //if ($user->uid == 1) {
-    foreach ($variables['page']['content']['system_main']['nodes'] as $key => $html) {
-      //dpr($html['body']['#object']->rdf_mapping);
-      if (isset($variables['page']['content']['system_main']['nodes'][$key]['body']['#object']->rdf_mapping['comment_count']['datatype'])) {
-        unset($variables['page']['content']['system_main']['nodes'][$key]['body']['#object']->rdf_mapping['comment_count']['datatype']);
-      }
-      //dpr($variables['page']['content']['system_main']['nodes'][$key]['body']['#object']->rdf_mapping);
-    }
+//    foreach ($variables['page']['content']['system_main']['nodes'] as $key => $html) {
+//      //dpr($html['body']['#object']->rdf_mapping);
+//      if (isset($variables['page']['content']['system_main']['nodes'][$key]['body']['#object']->rdf_mapping['comment_count']['datatype'])) {
+//        unset($variables['page']['content']['system_main']['nodes'][$key]['body']['#object']->rdf_mapping['comment_count']['datatype']);
+//      }
+//      //dpr($variables['page']['content']['system_main']['nodes'][$key]['body']['#object']->rdf_mapping);
+//    }
     //die;
   //}
 
 }
  
-*/
+
 
 
 /**
