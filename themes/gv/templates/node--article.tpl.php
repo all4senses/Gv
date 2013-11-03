cacbd1b69d0b994d5ecde815e13851a1ca238ef8
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-03 07:16:45 +0700

    Publish but not visible button on edit article

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -160,6 +160,8 @@
 
                 <?php print ' ' . /*$title_attributes*/ /*preg_replace('/datatype=".*"/', '', $title_attributes);*/ ''/*preg_replace('/datatype=""/', '', $title_attributes)*/; 
                 if (!$node->status) {echo ' class="not-published"';} 
+                
+                
                 ?>><?php if (!isset($node->title_no_link) && !$page): ?><a href="<?php print $node_url; ?>"><?php print $title; ?></a>
                 <?php else: ?><?php print $title; ?><?php endif; ?><?php if ($page): ?></h1><?php else: ?></h2><?php endif; ?> 
 
