d84d99822297ed4a5dcfa9966adb22b9675cfd14
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-09 00:11:14 +0700

    Change quote lp

--- modules/_GV/gv_misc/js/gv_exitIntent_lpV4.js
+++ modules/_GV/gv_misc/js/gv_exitIntent_lpV4.js
@@ -25,7 +25,7 @@
                         
                         ////$.fn.colorbox({iframe:true, width:650, height:400, href: 'get/iframe/exitIntent_lpV4', open: true});  
                         
-                        $.fn.colorbox({inline:true, href:"#exitIntent", width:780, height:430});  
+                        $.fn.colorbox({inline:true, href:"#exitIntent", width:780, height:440});  
                         
                         //$.fn.colorbox.close();
                         //alert('test');
