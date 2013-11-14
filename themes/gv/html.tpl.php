67cdb1d5b362d983383f8ba338c0fb2647786a16
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-15 01:04:19 +0700

    Move styles to bottom

--- themes/gv/html.tpl.php
+++ themes/gv/html.tpl.php
@@ -15,7 +15,12 @@
   <?php print $head; ?>
   <!-- Set the viewport width to device width for mobile -->
   <meta name="viewport" content="width=device-width" />
-  <?php print $styles; ?>
+  <?php 
+    global $user;
+    if ($user->uid != 1) {
+      print $styles; 
+    }
+  ?>
   <?php $args = arg(); 
         if ($args[0] == 'node' && (@$args[1] == 'add' || isset($args[2]) && $args[2] == 'edit')) { $top = TRUE; print $scripts; } 
   ?>
@@ -31,7 +36,12 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
   
-  <?php if (!isset($top)) { print $scripts; } ?>
+  <?php 
+    if ($user->uid == 1) {
+      print $styles; 
+    }
+    if (!isset($top)) { print $scripts; } 
+  ?>
   <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6005656184574&amp;value=0" /></noscript>
   
 </body>
