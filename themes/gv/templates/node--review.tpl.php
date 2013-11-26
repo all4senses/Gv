25b68ee46ecf72ccc81e89a91d9bca7872860fe0
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-26 08:32:40 +0700

    Bottom with latest articles changes

--- themes/gv/templates/node--review.tpl.php
+++ themes/gv/templates/node--review.tpl.php
@@ -130,11 +130,11 @@ elseif($view_mode == 'teaser_onPrefaceBottomLatest') {
   
   $stars = theme('gv_misc_fivestar_static', array('rating' => $node->field_r_rating_overall['und'][0]['value'] * 20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css')));
       
-  echo '<h3>'. $all_data_quick[$provider_nid]['name'] . ' - ' . $node->title . '</h3><div class="review">' . $teaser . '</div>';
-  echo '<div class="rating">' . $stars . '<span class="count">' . $node->field_r_rating_overall['und'][0]['value'] . ' out of 5</span></div>';
-  
-  echo '<div class="submitted"><span class="author">- by ' . $node->field_r_fname['und'][0]['value'] . ' ' . strtoupper($node->field_r_lname['und'][0]['value'][0]) . '.</span> / ' . date('F d, Y', $node->created) . '</div>';
-  
+  echo '<h3>'. $all_data_quick[$provider_nid]['name'] . ' - ' . $node->title . '</h3>
+    <div class="rating">' . $stars . '<span class="count">' . $node->field_r_rating_overall['und'][0]['value'] . ' out of 5</span></div>
+    <div class="submitted"><span class="author">- by ' . $node->field_r_fname['und'][0]['value'] . ' ' . strtoupper($node->field_r_lname['und'][0]['value'][0]) . '.</span> / ' . date('F d, Y', $node->created) . '</div>
+    <div class="review">' . $teaser . '</div>';
+    
   return;
 }
 ?>
