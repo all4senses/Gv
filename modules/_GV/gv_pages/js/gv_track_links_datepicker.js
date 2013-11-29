e2dc924f9d0ad24c1afc8024513ac7430b424b7f
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-29 08:01:30 +0700

    Track click report improvements

--- modules/_GV/gv_pages/js/gv_track_links_datepicker.js
+++ modules/_GV/gv_pages/js/gv_track_links_datepicker.js
@@ -6,6 +6,24 @@
         
         console.log('Datepicker!');
         
+        $( "#edit-date-from" ).datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3,
+          onClose: function( selectedDate ) {
+            $( "#to" ).datepicker( "option", "minDate", selectedDate );
+          }
+        });
+        $( "#edit-date-to" ).datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3,
+          onClose: function( selectedDate ) {
+            $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+          }
+        });
+    
+    
     }
   };
 
