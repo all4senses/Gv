7a94938d7cf107e8523d67c56dc9372410e445e5
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-11-29 07:44:01 +0700

    Track click report improvements

---
+++ modules/_GV/gv_pages/js/gv_track_links_datepicker.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  Drupal.behaviors.gv_track_links_datepicker = {
+    attach: function (context, settings) {
+
+        
+        console.log('Datepicker!');
+        
+    }
+  };
+
+}(jQuery));
