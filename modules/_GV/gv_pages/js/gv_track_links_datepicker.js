6e0f1ad58623140f804dae897bc591e3e60037aa
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-12-01 16:56:36 +0700

    Track click report improvements

--- modules/_GV/gv_pages/js/gv_track_links_datepicker.js
+++ modules/_GV/gv_pages/js/gv_track_links_datepicker.js
@@ -16,10 +16,11 @@
           firstDay: 1,
           showAnim: 'slideDown',
           //showButtonPanel: true,
-          formatDate: 'yy-mm-dd',
-          onClose: function( selectedDate ) {
-            $( "#edit-date-to" ).datepicker( "option", "minDate", selectedDate );
-          }
+          formatDate: 'yy-mm-dd'
+//          ,
+//          onClose: function( selectedDate ) {
+//            $( "#edit-date-to" ).datepicker( "option", "minDate", selectedDate );
+//          }
         });
         $( "#edit-date-to" ).datepicker({
           defaultDate: "+1w",
@@ -28,14 +29,20 @@
           minDate: new Date(2013, 1, 1),
           maxDate: 'today',
           showWeek: true,
-          firstDay: 1,
-          onClose: function( selectedDate ) {
-            $( "#edit-date-from" ).datepicker( "option", "maxDate", selectedDate );
-          }
+          firstDay: 1
+//          ,
+//          onClose: function( selectedDate ) {
+//            $( "#edit-date-from" ).datepicker( "option", "maxDate", selectedDate );
+//          }
         });
     
 //        $( "#edit-time-from" ).timepicker({ 'timeFormat': 'H:i:s' });
 //        $( "#edit-time-to" ).timepicker({ 'timeFormat': 'H:i:s' });
+        
+        $( "#edit-set-timeframe" ).change(function(e) {
+          console.log(e);
+          console.log($(this));
+        });
         
         console.log(Drupal.settings['gv_pages']);
         
