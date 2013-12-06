b5d75635a7cb9f297ed7f926d4af4343bf8a5b03
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-12-06 18:27:58 +0700

    Track click report improvements

---
+++ modules/_GV/gv_misc/js/gv_track_direct_link.js
@@ -0,0 +1,49 @@
+(function ($) {
+
+  Drupal.behaviors.gv_track_direct_link = {
+    attach: function (context, settings) {
+       
+       
+       // GV track click video link.
+       
+       $(".direct-link").click(function(){
+         
+         //console.log('click');
+         //console.log('href = ' + $(this).attr('href'));
+         //console.log('title = ' + $(this).attr('title'));
+
+         (jQuery).ajax({
+            
+                url: '/click', 
+                data: {
+                        type: 'direct_link_click',
+                        oid: $(this).attr('id'),
+                        click_page: window.location.href,
+                        url: $(this).attr('href'),
+                        title: $(this).attr('title')
+                        //,referer: document.referrer
+                       
+                      }, 
+                    type: 'POST', 
+                    dataType: 'json'
+                    /*
+                    , 
+                    success: function(data) 
+                            { 
+                                if(!data.error) {
+                                    console.log('The header is arrived!');
+                                }
+                                return false;
+                            } 
+                     */
+            }); // end of (jQuery).ajax
+        
+
+
+       });
+
+       
+    }
+  };
+
+}(jQuery));
