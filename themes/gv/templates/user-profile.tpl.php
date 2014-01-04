33e0f73b426a47c0b09bba260631d58ad473347c
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-01-05 06:56:39 +0700

    HybridAuth

--- themes/gv/templates/user-profile.tpl.php
+++ themes/gv/templates/user-profile.tpl.php
@@ -36,24 +36,28 @@
   <?php //print render($user_profile); ?>
   
   <?php 
-    dpm($user_profile); 
+    //dpm($user_profile); 
     //dpm($variables); 
-    dpm($_GET); dpm(arg()); 
-    dpm($_SERVER);
+    //dpm($_GET); dpm(arg()); 
+    //dpm($_SERVER);
   ?>
   
   <?php 
  
+    $is_reviewer = strpos($_SERVER['REQUEST_URI'], 'reviewer/');
+  
     print render($user_profile['metatags']);
     
     $user_name = @$user_profile['field_u_fname'][0]['#markup'] . ' ' . @$user_profile['field_u_lname'][0]['#markup'];
     
-    echo '<h1 id="user-caption">Meet: ' , $user_name, '</h1>', render($user_profile['user_picture']), render($user_profile['field_u_bio']),  '<div class="bottom-clear"></div>';
+    echo '<h1 id="user-caption">Meet' . ($is_reviewer ? ' our reviewer' : '') . ': ' , $user_name, '</h1>', render($user_profile['user_picture']), render($user_profile['field_u_bio']),  '<div class="bottom-clear"></div>';
     
-    $articles = $user_profile['field_u_fname']['#object']->name == 'guest' ? views_embed_view('articles','block_all_by_guest') : views_embed_view('articles','block_all_by_author');
-    
-    if ($articles) {
-      echo '<div id="articles-caption">', t('Read some of !author\'s latest articles below:', array('!author' => @$user_profile['field_u_fname'][0]['#markup'])), '</div>', $articles;
+    if (!$is_reviewer) {
+      $articles = $user_profile['field_u_fname']['#object']->name == 'guest' ? views_embed_view('articles','block_all_by_guest') : views_embed_view('articles','block_all_by_author');
+
+      if ($articles) {
+        echo '<div id="articles-caption">', t('Read some of !author\'s latest articles below:', array('!author' => @$user_profile['field_u_fname'][0]['#markup'])), '</div>', $articles;
+      }
     }
     /*
     $articles = views_embed_view('articles','block_by_author');
