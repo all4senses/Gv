765725893f23f2eae89d180bdcab11333ef9b4ea
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-01-09 21:28:20 +0700

    Fix provider page reviews block

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -19,7 +19,7 @@ function gv_preprocess_user_profile(&$profile) {
 function gv_link($variables) {
   //global $user;
   dpm('-------> ' . $variables['path']);
-  if (!empty($variables['options']['attributes']['page'])) {
+  if ($variables['path'] == 'node/63') {
     dpm($variables['options']);
   }
   //if ($user->uid == 1 && $variables['path'] == 'http://click.websitegear.com/track/1883449' && strpos($variables['path'], 'ttp://') && !strpos($variables['path'], 'ttp://getvoip.com')) {
