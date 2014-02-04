a3209eb0f289fb719a4d162c4704e41ec3e287ad
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-02-05 05:33:56 +0700

    Add livechatinc script

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -659,6 +659,7 @@ function gv_process_page(&$variables) {
   //drupal_add_js( $module_path_misc . '/js/gv_add_adroll.js'); 
   //drupal_add_js( $module_path_misc . '/js/gv_add_fb.js'); 
   
+  drupal_add_js( $module_path_misc . '/js/gv_add_livechatinc.js'); 
   
   // JS for comparing providers functionality on pages ehere appropriate views are cached and therefore have not loaded js itselves within views.
   $pages_with_compare_provider_functionality = array('/providers/reviews');
