c8115eb28df3e817f3f5787f10c8aa212c3dd5fd
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-02-05 05:37:07 +0700

    Add livechatinc script

--- modules/_GV/gv_misc/js/gv_add_livechatinc.js
+++ modules/_GV/gv_misc/js/gv_add_livechatinc.js
@@ -5,5 +5,4 @@ __lc.license = 3881401;
     var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
     lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-    console.log('liveee');
 })();
