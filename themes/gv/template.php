8a4d9f164bf91fb4e0e9a14fbc289ab00ccb3c88
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-02-07 23:46:48 +0700

    Local and Font, font-face issues

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -685,7 +685,8 @@ function gv_process_page(&$variables) {
   // Remote Google fonts.
   // gv_misc_addMetatag('stylesheet', NULL, 'http://fonts.googleapis.com/css?family=Open+Sans:normal,bold|Ubuntu:500', 'NA', 'text/css');
   // Local fonts, taken from the remote side.  
-  drupal_add_css('sites/all/libraries/fonts/google/open-sans-n-ubuntu/fonts.css', array('group' => /*CSS_DEFAULT*/CSS_THEME, 'every_page' => TRUE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
+  // Commented out here and moved to style.css
+  ////drupal_add_css('sites/all/libraries/fonts/google/open-sans-n-ubuntu/fonts.css', array('group' => /*CSS_DEFAULT*/CSS_THEME, 'every_page' => TRUE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
 
   drupal_add_css('sites/all/libraries/fonts/other/fonts_other.css', array('group' => /*CSS_DEFAULT*/CSS_THEME, 'every_page' => TRUE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
   //
