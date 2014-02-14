1416ba88675a2bd5d76e9d81014f23a3fc00e5da
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-02-14 22:00:54 +0700

    New lp v7

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -760,6 +760,11 @@ function gv_preprocess_node(&$variables) {
         $body_classes_add['quote_page'] = 'quote-page v6';
         $variables['theme_hook_suggestions'][] = 'node__quote__v6';
     }
+    elseif($variables['node']->type == 'quote' && ($variables['node']->title == 'Request a Quote page v7' || $variables['node']->title == 'Request a Quote page v7 Final') ) {
+        global $body_classes_add;
+        $body_classes_add['quote_page'] = 'quote-page v7';
+        $variables['theme_hook_suggestions'][] = 'node__quote__v7';
+    }
     // Speed test page have its own template
     elseif($variables['node']->type == 'preface' && @$variables['node']->field_preface_key['und'][0]['value'] == 'voip-speed-test') {
       $variables['theme_hook_suggestions'][] = 'node__preface__voip_speed_test';
