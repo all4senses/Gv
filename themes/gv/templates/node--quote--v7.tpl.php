cc2bea1c18829962b4546d9e2580f71bb5c0b027
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-02-19 03:55:33 +0700

    New lp7

--- themes/gv/templates/node--quote--v7.tpl.php
+++ themes/gv/templates/node--quote--v7.tpl.php
@@ -44,7 +44,7 @@
         <img src="/images/theme/lp7-back.jpg" style="position: absolute;">
         <div id="main-content">
 
-              <h2>Save Up To 80% On a Better<br/> Business Phone System!</h2>
+              <h2>Save Up To 65% On a Better<br/> Business Phone System!</h2>
               <img id="q-image" src="/images/theme/lp7-phone.png" />
               
               <div class="content<?php echo ($_SERVER['REDIRECT_URL'] == '/lp7-final' ? ' final' : '')?>"<?php print $content_attributes; ?>>
