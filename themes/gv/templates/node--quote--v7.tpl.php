4b43b8cc59e786e2f26630ed503509df6a4543d8
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-03-05 22:51:14 +0700

    Quote page text change

--- themes/gv/templates/node--quote--v7.tpl.php
+++ themes/gv/templates/node--quote--v7.tpl.php
@@ -44,7 +44,7 @@
         <img src="/images/theme/lp7-back.jpg" style="position: absolute;">
         <div id="main-content">
 
-              <h2>Save Up To 65% On a Better<br/> Business Phone Service!</h2>
+              <h2>Save Up To 65% On a Better<br/> Business Phone Solution!</h2>
               <img id="q-image" src="/images/theme/lp7-phone.png" />
               
               <div class="content<?php echo ($_SERVER['REDIRECT_URL'] == '/service-quotes-final' ? ' final' : '')?>"<?php print $content_attributes; ?>>
