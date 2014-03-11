9d23ebd775e30f5fe248009865d294bd7ff8f3e5
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-03-11 23:28:36 +0700

    New provider type

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -356,8 +356,9 @@
 //            gv_misc_pushTagsToMetatags($keyword_metatag_name, $content['field_topics']);
 //          }
           
-          global $user;
-          if ($user->uid == 1) {
+//          global $user;
+//          if ($user->uid == 1) 
+            {
             hide($content['body']);
             //dpm($node->body['und'][0]['value']);
             echo $node->body['und'][0]['value'];
