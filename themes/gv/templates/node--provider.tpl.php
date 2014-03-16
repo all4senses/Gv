8573b2b33c802ce1cf33516052996de5eede98a1
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-03-16 21:37:18 +0700

    Mobile

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -134,8 +134,8 @@
                       <?php 
                       
                          echo '<div class="voters"><div class="title">Number of Reviews:</div><div class="count" property="v:count"><a href="#reviews">', $node->gv_voters, '</a></div></div>',
-                              '<div id="positive">', 'Positive reviews: ', $node->gv_recommends['positive'], '</div><div id="negative">Negative reviews: ', $node->gv_recommends['negative'], '</div>',
-                              '<div class="recommend"><div class="title">Would recommend: </div><div class="data">', $node->gv_recommend, '% of Users</div></div>',
+                              '<div id="positive">', 'Positive<span class="narrow"> reviews</span>: ', $node->gv_recommends['positive'], '</div><div id="negative">Negative<span class="narrow"> reviews</span>: ', $node->gv_recommends['negative'], '</div>',
+                              '<div class="recommend narrow"><div class="title">Would recommend: </div><div class="data">', $node->gv_recommend, '% of Users</div></div>',
                               '<a id="write-review" href="/voip-provider-submit-user-review?id=', $node->nid, '"><img src="/sites/all/themes/gv_orange/css/images/writeareview2.png" alt="Write a Review" /></a>';
                       ?>
                       
