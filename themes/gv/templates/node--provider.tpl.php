4453058fef714a8532b1dc97e9ac129a71893f88
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-03-17 08:18:46 +0700

    Mobile

--- themes/gv/templates/node--provider.tpl.php
+++ themes/gv/templates/node--provider.tpl.php
@@ -135,8 +135,9 @@
                   <div class="overall"> 
                     <div class="text">
                       <?php 
-                      
-                         echo '<div class="voters"><div class="title"><span class="not-narrow">Number of </span>Reviews:</div><div class="count" property="v:count"><a href="#reviews">', $node->gv_voters, '</a></div></div>',
+                       dpm($node->p_data);
+                         echo '<div class="price narrow">Price', '</div>',
+                              '<div class="voters"><div class="title"><span class="not-narrow">Number of </span>Reviews:</div><div class="count" property="v:count"><a href="#reviews">', $node->gv_voters, '</a></div></div>',
                               '<div id="positive">', 'Positive<span class="not-narrow"> reviews</span>: ', $node->gv_recommends['positive'], '</div><div id="negative">Negative<span class="not-narrow"> reviews</span>: ', $node->gv_recommends['negative'], '</div>',
                               '<div class="recommend not-narrow"><div class="title">Would recommend: </div><div class="data">', $node->gv_recommend, '% of Users</div></div>',
                               '<a id="write-review" href="/voip-provider-submit-user-review?id=', $node->nid, '"><img src="/sites/all/themes/gv_orange/css/images/writeareview2.png" alt="Write a Review" /></a>';
