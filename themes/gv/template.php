3d401b1b2f46b4b03fb4164aefaa320c35e4f90d
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-04-15 14:08:56 +0700

    Add review ling under the logo in chart

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -659,7 +659,7 @@ function gv_process_page(&$variables) {
   //drupal_add_js( $module_path_misc . '/js/gv_add_adroll.js'); 
   //drupal_add_js( $module_path_misc . '/js/gv_add_fb.js'); 
   
-  //dpm($_SERVER);
+  dpm($_SERVER);
   $pages_with_livechatinc = array(/*'/', */'/business', '/hosted-pbx', '/business-systems');
   if (in_array(@$_SERVER['REQUEST_URI'], $pages_with_livechatinc)) {
     drupal_add_js( $module_path_misc . '/js/gv_add_livechatinc.js'); 
