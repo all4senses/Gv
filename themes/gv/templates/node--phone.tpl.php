ec60515a9a73cc723ad6bc05f1648f1cd5a3cc85
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-04-21 04:07:44 +0700

    Phone teaser on Blog page

--- themes/gv/templates/node--phone.tpl.php
+++ themes/gv/templates/node--phone.tpl.php
@@ -257,7 +257,7 @@
                 $teaser = substr_replace ($teaser, '... ' . l('Read More', 'node/' . $node->nid, array('attributes' => array('class' => array('more'), 'rel' => 'nofollow'))), $last_pos);
                 dpm($teaser);
               ?>
-                <div class="content teaser">
+                <div class="content-teaser">
                      <?php echo $teaser;?>
                 </div>
                 
