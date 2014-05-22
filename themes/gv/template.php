39c2b4f75c6eacf0f5849a96f8c56373d49a2901
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-05-22 17:05:04 +0700

    New p type  -Web conferencing

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -1115,7 +1115,7 @@ function gv_preprocess_views_view(&$vars) {
 //    //dpm($vars['view']->display['block_top_business_cmp']);
 //  }
   
-  if (!empty($vars['view']->display[$vars['view']->current_display]->display_options['use_more'])) {
+  if (!empty($vars['view']->display[$vars['view']->current_display]->display_options['use_more']) && !empty($vars['view']->display[$vars['view']->current_display]->display_options['link_url']) ) {
     //$vars['more'] = '<div class="more-link"><a rel="nofollow" href="'. $more_url . '">' . $link_text .  '</a></div>';
     $vars['more'] = '<div class="more-link"><a rel="nofollow" href="/'. $vars['view']->display[$vars['view']->current_display]->display_options['link_url'] . '">' . $vars['view']->display[$vars['view']->current_display]->display_options['use_more_text'] .  '</a></div>';
   }
