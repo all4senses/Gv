9f5039a964082bace7c46164b6c2dc03a59fc3d2
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-05-25 06:39:40 +0700

    New lpv8-uk

--- themes/gv/template.php
+++ themes/gv/template.php
@@ -907,6 +907,11 @@ function gv_preprocess_node(&$variables) {
         $body_classes_add['quote_page'] = 'quote-page v8';
         $variables['theme_hook_suggestions'][] = 'node__quote__v8';
     }
+    elseif($variables['node']->type == 'quote' && ($variables['node']->title == 'Request a Quote page v8_uk' || $variables['node']->title == 'Request a Quote page v8_uk Final') ) {
+        global $body_classes_add;
+        $body_classes_add['quote_page'] = 'quote-page v8 uk';
+        $variables['theme_hook_suggestions'][] = 'node__quote__v8_uk';
+    }
     // Speed test page have its own template
     elseif($variables['node']->type == 'preface' && @$variables['node']->field_preface_key['und'][0]['value'] == 'voip-speed-test') {
       $variables['theme_hook_suggestions'][] = 'node__preface__voip_speed_test';
