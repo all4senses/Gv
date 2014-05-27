679ecee7fda8523decb4a5fb0f8e31c3250b78eb
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-05-27 14:07:15 +0700

    New design Article

--- themes/gv_blue/templates/block.tpl.php
+++ themes/gv_blue/templates/block.tpl.php
@@ -4,6 +4,9 @@
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
+    <?php if ($block->region == 'sidebar_second'): ?>
+      <div class="block-icon pngfix"></div>
+    <?php endif;?>
     <?php $h_tag = in_array($block->region, array('content', 'content_wide', 'above_footer')) ? 'h2' : 'h3'; ?>
     <<?php echo $h_tag . ' ' . $title_attributes; ?>>
     <?php echo $title . '</' . $h_tag . '>'; ?>
