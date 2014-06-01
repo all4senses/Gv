59e6cd689a2492a0f3d000ffb783ea188fe8727f
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-06-02 05:37:23 +0700

    Small p table

--- themes/gv_blue/templates/block--highlighted.tpl.php
+++ themes/gv_blue/templates/block--highlighted.tpl.php
@@ -2,9 +2,23 @@
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; print ' xxx'; ?>"<?php print $attributes; ?>>
   <div class="inside">
     <?php print render($title_prefix); ?>
+    
     <?php if ($title): ?>
-      <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+      
+      
+      <?php if (in_array($block->delta, array('blog_top_rotated', 'home_in_hlighted_4'))): ?>
+        <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+      
+      <?php else: ?>
+        
+        
+        <<?php echo 'h3' . $title_attributes; ?>>
+        <?php echo '<span class="block-icon">' . $title . '</span></h3>'; ?>
+      
+      <?php endif;?>
+        
     <?php endif;?>
+      
     <?php print render($title_suffix); ?>
 
     <div class="content"<?php print $content_attributes; ?>>
