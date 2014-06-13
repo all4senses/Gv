8a126a33432988b3ebf49de4729ac789ab7db746
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-06-14 03:10:08 +0700

    New design - Submit review page

---
+++ modules/_GV/gv_pages/js/gv_review_fieldHints2.js
@@ -0,0 +1,38 @@
+(function ($) {
+
+  Drupal.behaviors.gv_review_fieldHints = {
+    attach: function (context, settings) {
+      
+      //$('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').hint();
+      
+      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').each(function(){
+        if ($(this).val() == '') {
+          $(this).val($(this).attr('title'));
+          $(this).addClass('blur');
+        }
+      });
+      
+      //$('input[id="edit-pros"]').val($('input[id="edit-pros"]').attr('title'));
+      
+      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').focus(function(){
+        
+        if ($(this).val() == $(this).attr('title')) {
+          $(this).val('');
+          $(this).removeClass('blur');
+        }
+        
+      });
+      
+      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').blur(function(){
+        
+        if ($(this).val() == '') {
+          $(this).val($(this).attr('title'));
+          $(this).addClass('blur');
+        }
+        
+      });
+      
+    }
+  };
+
+}(jQuery));
