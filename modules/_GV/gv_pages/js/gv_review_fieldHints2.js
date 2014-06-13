a1e386b1bba17e6eacf8d777e9b079db621d3e9d
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-06-14 06:24:32 +0700

    New design - Submit review page

--- modules/_GV/gv_pages/js/gv_review_fieldHints2.js
+++ modules/_GV/gv_pages/js/gv_review_fieldHints2.js
@@ -3,9 +3,9 @@
   Drupal.behaviors.gv_review_fieldHints = {
     attach: function (context, settings) {
       
-      //$('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').hint();
+      //$('#edit-body-und-0-value, input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').hint();
       
-      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').each(function(){
+      $('#edit-body-und-0-value, input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').each(function(){
         if ($(this).val() == '') {
           $(this).val($(this).attr('title'));
           $(this).addClass('blur');
@@ -14,7 +14,7 @@
       
       //$('input[id="edit-pros"]').val($('input[id="edit-pros"]').attr('title'));
       
-      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').focus(function(){
+      $('#edit-body-und-0-value, input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').focus(function(){
         
         if ($(this).val() == $(this).attr('title')) {
           $(this).val('');
@@ -23,7 +23,7 @@
         
       });
       
-      $('input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').blur(function(){
+      $('#edit-body-und-0-value, input[id="edit-field-r-fname-temp"], input[id="edit-field-r-lname-temp"], input[id="edit-field-r-email-temp"], input[id="edit-field-r-oprovider-und-0-value"], input[id="edit-pros"], input[id="edit-cons"]').blur(function(){
         
         if ($(this).val() == '') {
           $(this).val($(this).attr('title'));
