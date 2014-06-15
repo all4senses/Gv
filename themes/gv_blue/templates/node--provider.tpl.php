50be064090568371a2c86e4680e5f8e6c7153bf3
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-06-16 06:04:25 +0700

    New design - Provider page - request popups

--- themes/gv_blue/templates/node--provider.tpl.php
+++ themes/gv_blue/templates/node--provider.tpl.php
@@ -130,7 +130,7 @@
                               <div id="block-gv-blocks-request-quote-v8" class="popup-request quote"> 
                                 <?php echo $logo_block, '<div class="header"><div class="title">Request a Quote</div><div class="subtitle">From ', $provider_name, '</div><div class="descr">In order to prepare a Quote, we need some information about you</div></div>'; ?>
                                 <div class="bottom-clear"></div>
-                                <?php echo gv_blocks_get_requestQuote_block_v8_popup(); ?>
+                                <?php echo gv_blocks_get_requestQuote_block_v8_popup_provider(); ?>
                                 
                                 <!-- <a href="/business" id="yes" target="_top">YES</a>
                                 <div id="no">NO - I like overpaying for my old phone service.</div> -->
