a6eccd3caebeb24562ff512d2d121db5290cd288
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-06-19 20:55:24 +0700

    New design GV- popup requests

--- modules/_GV/gv_pages/js/gv_provider_popup_requestLinks.js
+++ modules/_GV/gv_pages/js/gv_provider_popup_requestLinks.js
@@ -17,7 +17,7 @@
             $("body").css('overflow', 'hidden');
             
             
-            $.fn.colorbox({inline:true, href:".popup-request.quote", width:730, height:550});  
+            $.fn.colorbox({inline:true, href:".popup-request.quote", width:730, height:530});  
             
             
             turned_off = false;
@@ -57,7 +57,7 @@
             $("body").css('overflow', 'hidden');
             
           
-            $.fn.colorbox({inline:true, href:".popup-request.demo", width:730, height:550});  
+            $.fn.colorbox({inline:true, href:".popup-request.demo", width:730, height:530});  
             turned_off = false;
             
             if ($(".popup-request.demo form").css('display') == 'block') {
