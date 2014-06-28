3ab16ea219e79fc619a3de324075ee215ae55855
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-06-28 23:26:23 +0700

    New design GV - Different tunings

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -57,9 +57,13 @@
     //echo $extra_data['home_teaser_image_beautify'] . '<h3>'. l($node->title, 'node/' . $node->nid) . '</h3><div class="submitted">By <span class="author">' . $author_name . '</span> / ' . date('F d, Y', $node->created) . '</div>' 
             //. '<div class="teaser">' . $teaser_data['teaser_only_home'] . '</div>'
     
-    echo $extra_data['teaser_main_image_beautify'] . /*$extra_data['home_teaser_image_beautify'] .*/ '<h3>'. l($node->title, 'node/' . $node->nid) . '</h3><div class="submitted">' . date('F j\t\h \<\s\p\a\n\>Y\<\/\s\p\a\n\>', $node->created) . '</div>' 
-            ;
-
+    if (!empty($extra_data['home_teaser_image_beautify'])) {
+      echo $extra_data['home_teaser_image_beautify'] . /*$extra_data['home_teaser_image_beautify'] .*/ '<h3>'. l($node->title, 'node/' . $node->nid) . '</h3><div class="submitted">' . date('F j\t\h \<\s\p\a\n\>Y\<\/\s\p\a\n\>', $node->created) . '</div>';
+    }
+    else {
+      echo $extra_data['teaser_main_image_beautify'] . /*$extra_data['home_teaser_image_beautify'] .*/ '<h3>'. l($node->title, 'node/' . $node->nid) . '</h3><div class="submitted">' . date('F j\t\h \<\s\p\a\n\>Y\<\/\s\p\a\n\>', $node->created) . '</div>';
+    }
+    
     //echo '--333--';
     return;
   }
