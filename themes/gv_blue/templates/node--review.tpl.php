d05d497f257977a2b44818e905fa81480785674a
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-07-05 03:52:39 +0700

    New design GV - Service type pages bottom main reviews teasers

--- themes/gv_blue/templates/node--review.tpl.php
+++ themes/gv_blue/templates/node--review.tpl.php
@@ -106,9 +106,7 @@ elseif($view_mode == 'servicePage_bottomMainReviewTeaser') {
       //echo '<a class="logo" href="' . $logo_link . '">' . $image . '</a>';
       
 
-      $stars = theme('gv_misc_fivestar_static', array('rating' => $node->field_r_rating_overall['und'][0]['value'] * 20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css')));
-      echo '<div class="rating">' . $stars . '<span class="count">' . $node->field_r_rating_overall['und'][0]['value'] . ' out of 5</span></div>';
-
+      
   echo '</div>';
   
   
@@ -126,7 +124,12 @@ elseif($view_mode == 'servicePage_bottomMainReviewTeaser') {
   //$teaser = substr_replace ($teaser, '... ' . l(t('Read More'), 'node/' . $nid, array('attributes' => array('class' => array('more')))), $last_pos);
   $teaser = substr_replace ($teaser, '... ' . l(t('Read More'), 'node/' . $provider_nid, array('attributes' => array('class' => array('more'), 'rel' => 'nofollow'))), $last_pos);
   
-  echo '<a href="' . url('node/' . $provider_nid) . '"><h3>'. $node->title . '</h3></a><div class="review">' . $teaser . '</div>';
+  
+  $stars = theme('gv_misc_fivestar_static', array('rating' => $node->field_r_rating_overall['und'][0]['value'] * 20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css')));
+  $stars = '<div class="rating">' . $stars . '<span class="count">' . $node->field_r_rating_overall['und'][0]['value'] . ' out of 5</span></div>';
+
+      
+  echo '<a href="' . url('node/' . $provider_nid) . '"><h3>'. $node->title . '</h3></a>' . $stars . '<div class="review">' . $teaser . '</div>';
   
   echo '<div class="submitted"><span class="author">- by ' . $node->field_r_fname['und'][0]['value'] . ' ' . strtoupper($node->field_r_lname['und'][0]['value'][0]) . '.</span> / ' . date('F d, Y', $node->created) . '</div>';
   
