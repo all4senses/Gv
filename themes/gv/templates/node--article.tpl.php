19e668b9c3ad3b9d6b1f9de06cb2a32a044ff1b0
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-07-07 01:50:57 +0700

    Workarounf for images linked remotely by a writer

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -2,7 +2,8 @@
 
   $class_thumb_presented = NULL;
   $return = FALSE;
-
+  
+  
   $extra_data = $teaser_data = gv_misc_updateArticleExtraData($node, TRUE, 1397495323);
   $extra_data['guest_author'] = $author_name = !empty($extra_data['guest_author']) ? $extra_data['guest_author'] : NULL;
               
