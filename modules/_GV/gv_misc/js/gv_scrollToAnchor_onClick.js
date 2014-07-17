3c72577f7862da5b301498c584dd09deefd0569e
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-07-18 03:43:44 +0700

    New design GV - Different tunings

--- modules/_GV/gv_misc/js/gv_scrollToAnchor_onClick.js
+++ modules/_GV/gv_misc/js/gv_scrollToAnchor_onClick.js
@@ -8,22 +8,29 @@
         // http://stackoverflow.com/questions/4198041/jquery-smooth-scroll-to-an-anchor
         var hashTagActive = "";
         $(".scroll-to-link").click(function (event) {
-            console.log(this.hash);
-            console.log($(this.hash));
-            if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            //console.log(this.hash);
+            //console.log($(this.hash));
+            
+            hash = this.hash;
+            // Make a beautiful link - show on a link #find-solution but go to #block-gv-blocks-home-service-types
+            if (hash == '#find-solution') {
+              hash = '#block-gv-blocks-home-service-types';
+            }
+            
+            if(hashTagActive != hash) { //this will prevent if the user click several times the same link to freeze the scroll.
                 event.preventDefault();
                 //calculate destination place
                 var dest = 0;
-                if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                if ($(hash).offset().top > $(document).height() - $(window).height()) {
                     dest = $(document).height() - $(window).height();
                 } else {
-                    dest = $(this.hash).offset().top;
+                    dest = $(hash).offset().top;
                 }
                 //go to destination
                 $('html,body').animate({
                     scrollTop: dest
                 }, 800, 'swing');
-                //////hashTagActive = this.hash;
+                //////hashTagActive = hash;
             }
         });
         
