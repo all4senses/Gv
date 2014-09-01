b4a117269f33eac65830d6a6ffbe86a519858290
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-09-02 01:05:49 +0700

    Fix imagestyle create image

--- themes/gv/templates/node--article.tpl.php
+++ themes/gv/templates/node--article.tpl.php
@@ -524,12 +524,12 @@
   </div>  <!-- main-content -->
   
       <?php 
-      global $user;
-      if ($user->uid == 1) 
-      {  
-        //dpm($node->otherArticles);
-        echo $node->otherArticles;
-      }
+//      global $user;
+//      if ($user->uid == 1) 
+//      {  
+//        //dpm($node->otherArticles);
+//        echo $node->otherArticles;
+//      }
       ?>
   
 <?php endif; ?>
