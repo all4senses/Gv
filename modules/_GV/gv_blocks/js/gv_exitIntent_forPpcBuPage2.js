d718d8065efa2f2a4a41a461d7ca86cb6f78b63b
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-09-07 21:17:18 +0700

    Exit intent ppc bu

--- modules/_GV/gv_blocks/js/gv_exitIntent_forPpcBuPage2.js
+++ modules/_GV/gv_blocks/js/gv_exitIntent_forPpcBuPage2.js
@@ -73,6 +73,7 @@
         $('input[name="referrer"]').val(document.referrer);
         $('input[name="url"]').val(document.URL);
        
+       /*
         //$('select').selectmenu();
         $('select').selectmenu({
           //style:'popup', 
@@ -137,7 +138,7 @@
         $(".label_after").click(function(){
           $(this).prev().click();
         });
-        
+        */
         $("#requestQuoteFormWrapper .multipartForm").formwizard({ 
 				 	formPluginEnabled: true,
 				 	validationEnabled: true,
