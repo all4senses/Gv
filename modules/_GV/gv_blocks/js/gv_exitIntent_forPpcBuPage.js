6e7213dc7208f851d663c36fa91f237ec0a199fa
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-09-07 21:53:46 +0700

    Exit intent ppc bu

--- modules/_GV/gv_blocks/js/gv_exitIntent_forPpcBuPage.js
+++ modules/_GV/gv_blocks/js/gv_exitIntent_forPpcBuPage.js
@@ -7,7 +7,7 @@
        
        
        // Exit intent functionality.
-       console.log('456');
+       //console.log('xxx');
        
        //var turned_off = null;
         var turned_off = true;
@@ -70,12 +70,11 @@
        
        
        
-        $('input[name="referrer"]').val(document.referrer);
-        $('input[name="url"]').val(document.URL);
+        $('#requestQuoteFormWrapper-ppc input[name="referrer"]').val(document.referrer);
+        $('#requestQuoteFormWrapper-ppc input[name="url"]').val(document.URL);
        
-       /*
         //$('select').selectmenu();
-        $('select').selectmenu({
+        $('#requestQuoteFormWrapper-ppc select').selectmenu({
           //style:'popup', 
           maxHeight: 300
   			});
@@ -106,7 +105,7 @@
 
         });        
         
-        $('input[id="firstname"], input[id="lastname"]').keydown(function (event) { 
+        $('#requestQuoteFormWrapper-ppc input[id="firstname"], #requestQuoteFormWrapper-ppc input[id="lastname"]').keydown(function (event) { 
             
             //console.log($(this).val());
             //console.log(event.keyCode);
@@ -135,16 +134,16 @@
         });
 
         // Click on a label will click on a radio button.
-        $(".label_after").click(function(){
+        $("#requestQuoteFormWrapper-ppc .label_after").click(function(){
           $(this).prev().click();
         });
-        */
-        $("#requestQuoteFormWrapper .multipartForm").formwizard({ 
+        
+        $("#requestQuoteFormWrapper-ppc .multipartForm").formwizard({ 
 				 	formPluginEnabled: true,
 				 	validationEnabled: true,
 				 	//focusFirstInput: true,
-          //textSubmit : 'Send me my Free Quote',
-          //textNext: "Next",
+          textSubmit : 'Send me my Free Quote',
+          textNext: "Next",
           
           //inAnimation : {height: 'show'},
           //outAnimation: {height: 'hide'},
@@ -157,123 +156,123 @@
           //easing: 'slide',
           //easing: 'easeOutExpo',
           
-          /*
-////          validationOptions: {
-////            groups: {
-////              username: "firstname lastname email phone"
-////              ,first_step: "phones_amt q_for q_type buying_time"
-////            },
-////            errorPlacement: function(error, element) {
-////              if (element.attr("name") == "phones_amt" || element.attr("name") == "q_for" || element.attr("name") == "buying_time" || element.attr("name") == "q_type" )
-////                //error.insertAfter( $(".last_radio", element.parent()) );
-////                ////error.insertAfter("#buying_time");
-////                ////error.insertAfter("#on_error");
-////                error.insertAfter(".step");
-////              
-////                //alert(error.html() +  ': ' + $(".question", element.parent()).html() );
-////              else if(element.attr("name") == "firstname" || element.attr("name") == "lastname"  || element.attr("name") == "company" || element.attr("name") == "email" || element.attr("name") == "phone")
-////                //alert(error.html() +  ': ' + element.prev().html());
-////                error.insertAfter("#phone");
-////                //alert(Drupal.t('All fields with * are required'));
-////              else
-////                error.insertAfter(element);
-////            },
-//////            showErrors: function(errorMap, errorList) {
-//////              alert("Your form contains " + this.numberOfInvalids());
-//////              console.log(this);
-//////              console.log(this.invalidElements());
-//////              
-//////              this.defaultShowErrors();
-//////            },
-////            rules: {
-////              
-//////              http://docs.jquery.com/Plugins/Validation#List_of_built-in_Validation_methods    
-//////                        
-////              // Rules from jquery.validate.js              
-////              
-////              classRuleSettings: {
-////                required: {required: true},
-////                email: {email: true},
-////                url: {url: true},
-////                date: {date: true},
-////                dateISO: {dateISO: true},
-////                dateDE: {dateDE: true},
-////                number: {number: true},
-////                numberDE: {numberDE: true},
-////                digits: {digits: true},
-////                creditcard: {creditcard: true}
-////              },
-//              
-//              
-//              
-//              phones_amt: "required",
-//              q_for: "required",
-//              q_type: "required",
-//              buying_time: "required",
-//              connection: "required",
-//             
-//             firstname: {
-//                required: true,
-//                minlength: 2,
-//                notEqualsTo: $('input[id="firstname"]').attr('title')
-//							},
-//              lastname: {
-//                required: true,
-//                minlength: 2,
-//                notEqualsTo: $('input[id="lastname"]').attr('title')
-//							},
-//              phone: {
-//                required: true,
-//                //number: true,
-//                minlength: 10,
-//                maxlength: 15,
-//                notEqualsTo: $('input[id="phone"]').attr('title')
-//							}
-//              // works
-////              
-////							myemail: {
-////								required: true,
-////								email: false,
-////                number: true
-////							}
-//              
-//						},
-//						messages: {
-//              
-//              // Rules from jquery.validate.js
-////              
-////              messages: {
-////                required: "This field is required.",
-////                remote: "Please fix this field.",
-////                email: "Please enter a valid email address.",
-////                url: "Please enter a valid URL.",
-////                date: "Please enter a valid date.",
-////                dateISO: "Please enter a valid date (ISO).",
-////                number: "Please enter a valid number.",
-////                digits: "Please enter only digits.",
-////                creditcard: "Please enter a valid credit card number.",
-////                equalTo: "Please enter the same value again.",
-////                accept: "Please enter a value with a valid extension.",
-////                maxlength: $.validator.format("Please enter no more than {0} characters."),
-////                minlength: $.validator.format("Please enter at least {0} characters."),
-////                rangelength: $.validator.format("Please enter a value between {0} and {1} characters long."),
-////                range: $.validator.format("Please enter a value between {0} and {1}."),
-////                max: $.validator.format("Please enter a value less than or equal to {0}."),
-////                min: $.validator.format("Please enter a value greater than or equal to {0}.")
-//              },   
-//              
-//             
-//              // Doesn't work... Used snipped above for it.
-//              //required: Drupal.t('All fields with * are required'),
-//              
-//              
-//              // Works!
+          
+          validationOptions: {
+            groups: {
+              username: "firstname lastname email phone"
+              ,first_step: "phones_amt q_for q_type buying_time"
+            },
+            errorPlacement: function(error, element) {
+              if (element.attr("name") == "phones_amt" || element.attr("name") == "q_for" || element.attr("name") == "buying_time" || element.attr("name") == "q_type" )
+                //error.insertAfter( $(".last_radio", element.parent()) );
+                ////error.insertAfter("#buying_time");
+                ////error.insertAfter("#on_error");
+                error.insertAfter("#requestQuoteFormWrapper-ppc .step");
               
-//              phones_amt: Drupal.t('Make your choice!'),
-//              q_for: Drupal.t('Make your choice!'),
-//              buying_time: Drupal.t('Make your choice!'),
-//              connection: Drupal.t('Make your choice!'),
+                //alert(error.html() +  ': ' + $(".question", element.parent()).html() );
+              else if(element.attr("name") == "firstname" || element.attr("name") == "lastname"  || element.attr("name") == "company" || element.attr("name") == "email" || element.attr("name") == "phone")
+                //alert(error.html() +  ': ' + element.prev().html());
+                error.insertAfter("#requestQuoteFormWrapper-ppc #phone");
+                //alert(Drupal.t('All fields with * are required'));
+              else
+                error.insertAfter(element);
+            },
+//            showErrors: function(errorMap, errorList) {
+//              alert("Your form contains " + this.numberOfInvalids());
+//              console.log(this);
+//              console.log(this.invalidElements());
+//              
+//              this.defaultShowErrors();
+//            },
+            rules: {
               
+//              http://docs.jquery.com/Plugins/Validation#List_of_built-in_Validation_methods    
+//                        
+              // Rules from jquery.validate.js              
+              /*
+              classRuleSettings: {
+                required: {required: true},
+                email: {email: true},
+                url: {url: true},
+                date: {date: true},
+                dateISO: {dateISO: true},
+                dateDE: {dateDE: true},
+                number: {number: true},
+                numberDE: {numberDE: true},
+                digits: {digits: true},
+                creditcard: {creditcard: true}
+              },
+              */
+              
+              
+              phones_amt: "required",
+              q_for: "required",
+              q_type: "required",
+              buying_time: "required",
+              connection: "required",
+             
+             firstname: {
+                required: true,
+                minlength: 2,
+                notEqualsTo: $('input[id="firstname"]').attr('title')
+							},
+              lastname: {
+                required: true,
+                minlength: 2,
+                notEqualsTo: $('input[id="lastname"]').attr('title')
+							},
+              phone: {
+                required: true,
+                //number: true,
+                minlength: 10,
+                maxlength: 15,
+                notEqualsTo: $('input[id="phone"]').attr('title')
+							}
+              // works
+              /* 
+							myemail: {
+								required: true,
+								email: false,
+                number: true
+							}
+              */
+						},
+						messages: {
+              
+              // Rules from jquery.validate.js
+              /*
+              messages: {
+                required: "This field is required.",
+                remote: "Please fix this field.",
+                email: "Please enter a valid email address.",
+                url: "Please enter a valid URL.",
+                date: "Please enter a valid date.",
+                dateISO: "Please enter a valid date (ISO).",
+                number: "Please enter a valid number.",
+                digits: "Please enter only digits.",
+                creditcard: "Please enter a valid credit card number.",
+                equalTo: "Please enter the same value again.",
+                accept: "Please enter a value with a valid extension.",
+                maxlength: $.validator.format("Please enter no more than {0} characters."),
+                minlength: $.validator.format("Please enter at least {0} characters."),
+                rangelength: $.validator.format("Please enter a value between {0} and {1} characters long."),
+                range: $.validator.format("Please enter a value between {0} and {1}."),
+                max: $.validator.format("Please enter a value less than or equal to {0}."),
+                min: $.validator.format("Please enter a value greater than or equal to {0}.")
+              },   
+              */
+             
+              // Doesn't work... Used snipped above for it.
+              //required: Drupal.t('All fields with * are required'),
+              
+              
+              // Works!
+              /*
+              phones_amt: Drupal.t('Make your choice!'),
+              q_for: Drupal.t('Make your choice!'),
+              buying_time: Drupal.t('Make your choice!'),
+              connection: Drupal.t('Make your choice!'),
+              */
              
              
              
@@ -285,11 +284,11 @@
 
 
               // Works!
-              
-//              firstname: {
-//                notEqualsTo: Drupal.t("xxxx")
-//              },
-              
+              /*
+              firstname: {
+                notEqualsTo: Drupal.t("xxxx")
+              },
+              */
              
               email: {
                 //required: Drupal.t("We need your email address to contact you"),
@@ -297,17 +296,15 @@
               }
             }
           },
-				 	
-          */
-          formOptions :{
+				 	formOptions :{
 						//success: function(data){alert('Success!'); $("#status").fadeTo(50,1,function(){ $(this).html("You are now registered!").fadeTo(5000, 0); })},
-            //success: function(data){$('#requestQuoteFormWrapper .sending').hide('clip'); $("#requestQuoteFormWrapper .success").append(data.data); $("#requestQuoteFormWrapper .success").show('clip');},
+            //success: function(data){$('#requestQuoteFormWrapper-ppc .sending').hide('clip'); $("#requestQuoteFormWrapper-ppc .success").append(data.data); $("#requestQuoteFormWrapper-ppc .success").show('clip');},
             
-            success: function(data){$('#requestQuoteFormWrapper .sending').hide(); $("#requestQuoteFormWrapper .success").append(data.data); $("#requestQuoteFormWrapper .success").show();},
+            success: function(data){$('#requestQuoteFormWrapper-ppc .sending').hide(); $("#requestQuoteFormWrapper-ppc .success").append(data.data); $("#requestQuoteFormWrapper-ppc .success").show();},
 						
-            //beforeSubmit: function(data){$('#requestQuoteFormWrapper .multipartForm').hide('clip'); $("#requestQuoteFormWrapper .sending").append('Data is sendingt: ' + $.param(data)); $("#requestQuoteFormWrapper .sending").show('clip'); },//function(data){$("#data").html("data sent to the server: " + $.param(data));},
-            //beforeSubmit: function(data){$('#requestQuoteFormWrapper .multipartForm').hide('clip'); $("#requestQuoteFormWrapper .sending").append('<p>Please wait a moment while processing your request.</p>'); $("#requestQuoteFormWrapper .sending").show('clip'); },
-            beforeSubmit: function(data){$('#requestQuoteFormWrapper .multipartForm').hide(); $("#requestQuoteFormWrapper .sending").append('<div class="wait"><p><strong>Please wait</strong> a moment while processing your request...</p></div>'); $("#requestQuoteFormWrapper .sending").show(); },
+            //beforeSubmit: function(data){$('#requestQuoteFormWrapper-ppc .multipartForm').hide('clip'); $("#requestQuoteFormWrapper-ppc .sending").append('Data is sendingt: ' + $.param(data)); $("#requestQuoteFormWrapper-ppc .sending").show('clip'); },//function(data){$("#data").html("data sent to the server: " + $.param(data));},
+            //beforeSubmit: function(data){$('#requestQuoteFormWrapper-ppc .multipartForm').hide('clip'); $("#requestQuoteFormWrapper-ppc .sending").append('<p>Please wait a moment while processing your request.</p>'); $("#requestQuoteFormWrapper-ppc .sending").show('clip'); },
+            beforeSubmit: function(data){$('#requestQuoteFormWrapper-ppc .multipartForm').hide(); $("#requestQuoteFormWrapper-ppc .sending").append('<div class="wait"><p><strong>Please wait</strong> a moment while processing your request...</p></div>'); $("#requestQuoteFormWrapper-ppc .sending").show(); },
             
             
             dataType: 'json',
@@ -316,11 +313,11 @@
 				 }
 				);
     
-        //console.log($("#requestQuoteFormWrapper .multipartForm").formwizard("state"));
+        //console.log($("#requestQuoteFormWrapper-ppc .multipartForm").formwizard("state"));
     
     
     
-      $("#requestQuoteFormWrapper").removeClass('hidden'); 
+      $("#requestQuoteFormWrapper-ppc").removeClass('hidden'); 
     
 
     
