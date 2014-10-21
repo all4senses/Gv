9c5beec17b840b5c8eb134c5ba20aa7e84a42e03
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-10-21 18:13:27 +0700

    Conferences page

--- themes/gv_blue/templates/page--conferences_finder.tpl.php
+++ themes/gv_blue/templates/page--conferences_finder.tpl.php
@@ -16,14 +16,21 @@
           <img src="/sites/all/themes/gv_blue/css/images/tc_back.jpg" style="position: absolute; z-index: -10;">
                     
           <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
-          <?php print render($page['help']); ?>
+          <?php //print render($page['help']); ?>
           <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
           
           <?php 
             //echo render($page['above_content']);
             //echo render($page['content']); 
-            
-            echo gv_blocks_get_confMainSearch();
+            echo '<table class="conf-table"><tbody>',
+                    '<tr><td class="header">Logo</td></tr>',
+                    '<tr><td class="search">', gv_blocks_get_confMainSearch(), '</td></tr>',
+                    '<tr><td class="copy">Copy</td></tr>',
+                    '<tr><td class="add">Add/td></tr>',
+                    '<tr><td class="footer"><div class="c"><div>Copyright</div> 2014 GetVoIP.com | All Rights Reserved</div></td></tr>',
+                    
+                 '</tbody></table>';
+
             
           ?>
         
@@ -36,18 +43,6 @@
 
   </div> <!-- /#all-content -->
 
-   <div id="footer-inside">
-
-    <?php
-       
-        //echo render($page['footer']);
-        
-        echo '<div class="c"><div>Copyright</div> 2014 GetVoIP.com | All Rights Reserved</div>';
-        
-    ?>
-   </div>
-  
-  
    <div class="cd-overlay"></div>
   
 </div> <!-- <div id="bshadow"> -->
