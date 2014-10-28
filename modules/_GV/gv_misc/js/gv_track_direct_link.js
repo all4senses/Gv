f0da7d568bccc34418725ce6baaa3e7a422215ae
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-10-28 12:33:52 +0700

    Click tracking via ajax

--- modules/_GV/gv_misc/js/gv_track_direct_link.js
+++ modules/_GV/gv_misc/js/gv_track_direct_link.js
@@ -6,40 +6,48 @@
        
        // GV track provider direct link.
        //alert('.');
-       $(".direct-link").click(function(){
+       $(".direct-link").click(function(event){
          
-         //console.log('direct link click');
-         //console.log('href = ' + $(this).attr('href'));
-         //console.log('title = ' + $(this).attr('title'));
-
+         event.preventDefault();
+         
+         console.log('direct link click');
+         console.log('href = ' + $(this).attr('href'));
+         console.log('title = ' + $(this).attr('title'));
+         
+         var provider_direct_url = $(this).attr('href');
+         var click_page = window.location.href;
+         
          (jQuery).ajax({
             
                 url: '/click', 
                 data: {
                         type: 'provider',
                         oid: $(this).attr('id'),
-                        click_page: window.location.href,
-                        url: $(this).attr('href'),
+                        click_page: click_page,
+                        url: provider_direct_url,
                         title: $(this).attr('title')
                         //,referer: document.referrer
                        
                       }, 
                     type: 'POST', 
                     dataType: 'json'
-                    /*
+                    
                     , 
                     success: function(data) 
                             { 
                                 if(!data.error) {
-                                    console.log('The header is arrived!');
+                                    console.log('Provider click is tracked');
                                 }
                                 return false;
                             } 
-                     */
+                     
             }); // end of (jQuery).ajax
         
 
-
+            // Now we open an url in a new window.
+            window.open(provider_direct_url);
+            
+            
        });
 
        
