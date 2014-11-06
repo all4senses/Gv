c487eaf1324bd80c8453a5309a3c87c1aec9e0b2
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-11-07 04:21:03 +0700

    Conferences

--- modules/_GV/gv_blocks/js/gv_conf_main_page.js
+++ modules/_GV/gv_blocks/js/gv_conf_main_page.js
@@ -813,7 +813,7 @@
                   current_sw = new_sw ? new_sw : sw;
                   save_sw = new_sw = false;
                   
-                  //console.log(data);
+                  console.log(data);
                   $("input#conf").removeClass('ui-autocomplete-loading');
                   
                   if (data.type == 'conference') {
