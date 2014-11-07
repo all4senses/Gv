f960ea642c880fd1e9abdbc1b177b38821f90fd5
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-11-08 04:46:04 +0700

    Conferences

--- modules/_GV/gv_blocks/js/gv_conf_main_page.js
+++ modules/_GV/gv_blocks/js/gv_conf_main_page.js
@@ -939,7 +939,7 @@
         $('#cboxOverlay').css({
             'visibility': 'visible',
             'opacity': 0.2,
-            'height': 0,
+            //'height': 0,
             'cursor': 'pointer',
             
             // execute our original animation on the overlay!
@@ -948,7 +948,7 @@
         }).animate({
             //height: ['toggle', 'swing'],
             ////height: ['100%', 'swing'],
-            opacity: 'toggle',
+            //opacity: 'toggle',
             opacity: 0.7
         }, 1000 , function() {
             //$('#colorbox')
