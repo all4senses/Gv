aae67ecb027ac085f4420772544544f3e921d5c3
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-12-14 03:38:02 +0600

    Ckeditor enhance

--- modules/_Structure/oembed/oembed.api.php
+++ modules/_Structure/oembed/oembed.api.php
@@ -16,7 +16,7 @@
  *   The original URL or embed code to parse.
  */
 function hook_oembed_request_alter(&$parameters, &$provider, $url) {
-  if ($provider['name'] == 'default:youtube') {
+  if ($provider['name'] == 'youtube') {
     $parameters['iframe'] = '1';
   }
 }
@@ -41,54 +41,4 @@ function hook_oembedprovider_formats_alter(&$formats) {
     'mime' => 'text/javascript',
     'callback' => '_oembedprovider_formats_jsonp',
   );
-}
-
-/**
- * oEmbed filter replacement callback.
- *
- * Override basic function by setting Drupal system variable
- * `oembed_resolve_link_callback` to a new function name with this signature.
- *
- * @param string $url
- *   URL to embed.
- * @param array $options
- *   oEmbed request options.
- *
- * @return string
- *   Rendered oEmbed response.
- */
-function oembedinline_oembed_resolve_link($url, $options = array()) {
-
-  // If file_entity module is enabled, treat the URL as an uploaded file.
-  // Inline is used to defer the rendering of the embedded content until the
-  // entity is actually viewed. This technique allows content to be cached by
-  // Drupal's filter system.
-  $view_mode = 'full';
-  if (isset($options['view_mode'])) {
-    $view_mode = $options['view_mode'];
-    unset($options['view_mode']);
-  }
-
-  $url = decode_entities($url);
-
-  $element = array();
-
-  $file = oembed_url_to_file($url);
-  $file->override = $options;
-  if (isset($file->fid)) {
-    $macro_params = array();
-    $macro_params[] = 'entity';
-    $macro_params[] = 'type=file';
-    $macro_params[] = 'id=' . $file->fid;
-    $macro_params[] = 'view_mode=' . $view_mode;
-    $element = array('#markup' => "\r\n" . '[' . implode('|', $macro_params) . ']' . "\r\n");
-  }
-
-  $return = drupal_render($element);
-
-  if (empty($return)) {
-    $return = $url;
-  }
-
-  return $return;
 }
