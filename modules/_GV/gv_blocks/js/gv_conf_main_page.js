28a06bf669abcf3ad830b71307d5d89dc692271b
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-12-23 05:44:33 +0600

    Conf import

--- modules/_GV/gv_blocks/js/gv_conf_main_page.js
+++ modules/_GV/gv_blocks/js/gv_conf_main_page.js
@@ -1002,26 +1002,26 @@
   
   
   //$(".top .textfill").bigText({maximumFontSize: 40});
-  /*
+  
   //if (jQuery('.top .textfill').height() > 40) {
     initial_font_size = 20;
     jQuery('.top .textfill').css('font-size', initial_font_size + 'px');
-      jQuery('.top .textfill').css('line-height', 1);
+      //jQuery('.top .textfill').css('line-height', 1);
     while(jQuery('.top .textfill').height() < 80) {
       initial_font_size++;
       
       jQuery('.top .textfill').css('font-size', initial_font_size + 'px');
-      jQuery('.top .textfill').css('line-height', initial_font_size + 'px');
+      //jQuery('.top .textfill').css('line-height', initial_font_size + 'px');
       console.log('f:' + jQuery('.top .textfill').css('font-size'));
       console.log('h:' + jQuery('.top .textfill').height());
     }
-//    initial_font_size--;
-//    jQuery('.top .textfill').css('font-size', initial_font_size + 'px');
+    initial_font_size--;
+   jQuery('.top .textfill').css('font-size', initial_font_size + 'px');
 //    jQuery('.top .textfill').css('line-height', initial_font_size + 'px');
     console.log('f:' + jQuery('.top .textfill').css('font-size'));
     console.log('h:' + jQuery('.top .textfill').height());
   //}
-  */
+  
    //console.log('fill');                     
    
   // Uncomment to stop scrolling.
