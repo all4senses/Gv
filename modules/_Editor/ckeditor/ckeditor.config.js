6b6cf74e69e2f0148c0d410caa7204544f618d1c
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-12-23 03:04:38 +0600

    Conf import

--- modules/_Editor/ckeditor/ckeditor.config.js
+++ modules/_Editor/ckeditor/ckeditor.config.js
@@ -113,3 +113,13 @@ Drupal.settings.cke_toolbar_DrupalFull = [
   ['DrupalBreak', 'DrupalPageBreak'],
   ['oembed']
 ];
+
+CKEDITOR.stylesSet.add( 'my_styles', [
+    // Block-level styles.
+    { name: 'Blue Title--', element: 'h2', styles: { color: 'Blue' } },
+    { name: 'Red Title---',  element: 'h3', styles: { color: 'Red' } },
+
+    // Inline styles.
+    { name: 'CSS Style---', element: 'span', attributes: { 'class': 'my_style' } },
+    { name: 'Marker: Yellow---', element: 'span', styles: { 'background-color': 'Yellow' } }
+]);
