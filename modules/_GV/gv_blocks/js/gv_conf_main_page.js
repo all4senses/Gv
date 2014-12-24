b4dadff14181f68a44f86626d77aa31548d2a6e8
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-12-24 20:12:17 +0600

    Conf import

--- modules/_GV/gv_blocks/js/gv_conf_main_page.js
+++ modules/_GV/gv_blocks/js/gv_conf_main_page.js
@@ -1173,7 +1173,7 @@
         width: 532,//550,  //'40%'
         height: 518//500//545///470
         
-        ,right: 530
+        ,right: 548
         
         ,//added to remove initial flickering
        opacity : 0,
