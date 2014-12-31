1c7ccc36c81c2dfc99451b0a8f7100931111c002
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-12-31 07:18:45 +0600

    Conf import

--- modules/_GV/gv_blocks/js/gv_conf_main_page.js
+++ modules/_GV/gv_blocks/js/gv_conf_main_page.js
@@ -1205,9 +1205,31 @@
    } // End of if (!popup_stick_to_left) {
    else {
   
-    //console.log('popup stick to left');
+      //console.log('popup stick to left');
+      
+      // Usual appearing
+      cb1 = $.fn.colorbox({
+        transition: 'fade' , //'elastic',
+        speed: 800,
+        fadeOut: 1700,
+        inline:true, 
+        href:"#cb-popup_1", 
+        width: 532, 
+        height: 518
+        
+        ,right: 500
+
+        ,onClosed: function() {
+               //console.log('closed...');
+               $("body").css('overflow', 'inherit');
+               turned_off = true;
+               // Return to Side Slide window, if the popup was called from a side slide window.
+               return_to_sidewindow_if_needed();
+             }
+      }); 
     
-      // Usual appearing, in the middle with the background shading.
+      // Effect appearing, on the left with the background shading.
+      /*
       cb1 = $.fn.colorbox({
         transition: 'fade' , //'elastic',
         speed: 800,
@@ -1277,6 +1299,7 @@
                //cb_closing_in_progress = false;
              }
       }); 
+      */
       
    } // End of Else of if (!popup_stick_to_left) {
    
