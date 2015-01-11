e2ffb2d5dc49ba6154982afb6d1c8bbe293217c1
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-01-12 01:02:54 +0600

    Lp10

--- themes/gv_blue/templates/node--quote--v10.tpl.php
+++ themes/gv_blue/templates/node--quote--v10.tpl.php
@@ -46,122 +46,139 @@
 
       
       <main class="main section full-height" role="main" style="height: 728px;/* display: none; */">
-        <div class="form-main">
-          <div class="title-first">
-            <h1>Simplify your Business VoIP buying process by letting us do the work for you.</h1>
-            <h2>Answer a few questions and we'll find the best business phone solution for your specific needs.</h2>
-          </div>
-          <div class="title-loading">
-            <div class="loading-gif"></div>
-          </div>
-          <div class="title-second">
-            <h1>We have found <span class="result"><span class="number">4</span></span> VoIP companies matching your requirements!</h1>
-            <h2>Where do we send the details for your 4 matches?</h2>
-          </div>
-          <form action="default.php" class="form">
-            <div class="step one radio-filled" style="display: block;">
-              <div class="wrap">
-                <div class="fieldset choices service-type first">
-                  <p class="question">How can we help you?</p>
-                  <div class="wrap">
-                    <div class="choice phone-only first">
-                      <div class="icon-replace"></div>
-                      <label for="replace">Replace current system</label>
-                      <input type="radio" class="radio" name="service" id="replace" value="replace">
+        
+        <?php if (!$final): ?>
+          <div class="form-main">
+            <div class="title-first">
+              <h1>Simplify your Business VoIP buying process by letting us do the work for you.</h1>
+              <h2>Answer a few questions and we'll find the best business phone solution for your specific needs.</h2>
+            </div>
+            <div class="title-loading">
+              <div class="loading-gif"></div>
+            </div>
+            <div class="title-second">
+              <h1>We have found <span class="result"><span class="number">4</span></span> VoIP companies matching your requirements!</h1>
+              <h2>Where do we send the details for your 4 matches?</h2>
+            </div>
+            <form action="default.php" class="form">
+              <div class="step one radio-filled" style="display: block;">
+                <div class="wrap">
+                  <div class="fieldset choices service-type first">
+                    <p class="question">How can we help you?</p>
+                    <div class="wrap">
+                      <div class="choice phone-only first">
+                        <div class="icon-replace"></div>
+                        <label for="replace">Replace current system</label>
+                        <input type="radio" class="radio" name="service" id="replace" value="replace">
+                      </div>
+                      <div class="choice both checked">
+                        <div class="icon-new"></div>
+                        <label for="new"><span>Find</span> new system &amp; service</label>
+                        <input type="radio" class="radio" name="service" id="new" value="new" checked="">
+                      </div>
+                      <div class="choice service-only">
+                        <div class="icon-expand"></div>
+                        <label for="expand-only">Expand current system</label>
+                        <input type="radio" class="radio" name="service" id="expand-only" value="expand-only">
+                      </div>
                     </div>
-                    <div class="choice both checked">
-                      <div class="icon-new"></div>
-                      <label for="new"><span>Find</span> new system &amp; service</label>
-                      <input type="radio" class="radio" name="service" id="new" value="new" checked="">
+                    <div class="clearfix-dk"></div>
+                  </div>
+                  <div class="fieldset button-field next">
+                    <div class="wrap">
+                      <div class="button">
+                        <p>Let's Get Started <span class="icon-angle-double-right"></span></p>
+                      </div>
                     </div>
-                    <div class="choice service-only">
-                      <div class="icon-expand"></div>
-                      <label for="expand-only">Expand current system</label>
-                      <input type="radio" class="radio" name="service" id="expand-only" value="expand-only">
+                  </div>
+                </div>
+              </div>
+              <div class="step two" style="display: none;">
+                <div class="wrap">
+                  <div class="fieldset choices people first">
+                    <p class="question">How many people will be using your phone system?</p>
+                    <div class="wrap">
+                      <div class="choice first"><label for="1-4">1<span> </span>-<span> </span>4</label><input type="radio" class="radio" name="people" value="1-4" id="1-4"></div>
+                      <div class="choice second"><label for="5-10">5<span> </span>-<span> </span>10</label><input type="radio" class="radio" name="people" value="5-10" id="5-10"></div>
+                      <div class="choice third"><label for="11-20">11<span> </span>-<span> </span>20</label><input type="radio" class="radio" name="people" value="11-20" id="11-20"></div>
+                      <div class="choice fourth"><label for="21-49">21<span> </span>-<span> </span>49</label><input type="radio" class="radio" name="people" value="21-49" id="21-49"></div>
+                      <div class="choice fifth"><label for="50">50 +</label><input type="radio" class="radio" name="people" value="50" id="50"></div>
+                      <div class="clearfix-dk"></div>
                     </div>
+                  </div>
+                  <div class="fieldset checkboxes">
+                    <p class="question">What features are important to you?</p>
+                    <div class="wrap top5">
+                      <div class="wrap column left">
+                        <div class="choice-checkbox first"><div class="checkbox-new"></div><label for="mobile">Mobile Capabilities</label><input type="checkbox" class="checkbox" name="features" value="mobile" id="mobile"></div>
+                        <div class="choice-checkbox second"><div class="checkbox-new"></div><label for="fax">Online Fax</label><input type="checkbox" class="checkbox" name="features" value="fax" id="fax"></div>
+                        <div class="choice-checkbox third"><div class="checkbox-new"></div><label for="monitoring">Call Management<span>/Monitoring</span></label><input type="checkbox" class="checkbox" name="features" value="monitoring" id="monitoring"></div>
+                      </div>
+                      <div class="wrap column right">
+                        <div class="choice-checkbox first"><div class="checkbox-new"></div><label for="voicemail">Voicemail-to-email</label><input type="checkbox" class="checkbox" name="features" value="voicemail" id="voicemail"></div>
+                        <div class="choice-checkbox second"><div class="checkbox-new"></div><label for="auto">Auto Attendant</label><input type="checkbox" class="checkbox" name="features" value="auto" id="auto"></div>
+                        <div class="choice-checkbox third"><div class="checkbox-new"></div><label for="none">No Preference</label><input type="checkbox" class="checkbox" name="features" value="none" id="none"></div>
+                      </div>
+                      <div class="clearfix-dk"></div>
+                    </div>
+                  </div>
+                  <div class="fieldset button-field top10">
+                    <div class="wrap">
+                      <div class="button fixed left back to-step-1">
+                        <p>Back</p>
+                      </div>
+                      <div class="button right next to-step-3">
+                        <p>Find Me Solutions <span class="icon-angle-double-right"></span></p>
+                      </div>
+                      <div class="clearfix-dk"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="step three">
+                <div class="wrap">
+                  <div class="fieldset input half first firstname">
+                    <p class="question">First Name</p>
+                    <div class="wrap"><input type="text" class="field"></div>
+                  </div>
+                  <div class="fieldset input half first lastname">
+                    <p class="question">Last Name</p>
+                    <div class="wrap"><input type="text" class="field"></div>
                   </div>
                   <div class="clearfix-dk"></div>
-                </div>
-                <div class="fieldset button-field next">
-                  <div class="wrap">
-                    <div class="button">
-                      <p>Let's Get Started <span class="icon-angle-double-right"></span></p>
-                    </div>
+                  <div class="fieldset input half email">
+                    <p class="question">Email Address</p>
+                    <div class="wrap"><input type="text" class="field"></div>
                   </div>
+                  <div class="fieldset input half phone">
+                    <p class="question">Phone Number</p>
+                    <div class="wrap"><input type="text" class="field"></div>
+                  </div>
+                  <div class="clearfix-dk"></div>
+                  <div class="fieldset button-field">
+                    <div class="wrap"><input type="submit" value="Show Me Results" class="button"></div>
+                  </div>
+                  <p class="inform-text">You will also receive a FREE 6-Page business VoIP guide to help analyze and compare your results.</p>
                 </div>
               </div>
+            </form>
+          </div>
+        <?php else: ?>
+          <div class="form-main pdf-page">
+            <div class="title-first pdf">
+              <h1>Download Your Free VoIP Buyer's Guide</h1>
             </div>
-            <div class="step two" style="display: none;">
-              <div class="wrap">
-                <div class="fieldset choices people first">
-                  <p class="question">How many people will be using your phone system?</p>
-                  <div class="wrap">
-                    <div class="choice first"><label for="1-4">1<span> </span>-<span> </span>4</label><input type="radio" class="radio" name="people" value="1-4" id="1-4"></div>
-                    <div class="choice second"><label for="5-10">5<span> </span>-<span> </span>10</label><input type="radio" class="radio" name="people" value="5-10" id="5-10"></div>
-                    <div class="choice third"><label for="11-20">11<span> </span>-<span> </span>20</label><input type="radio" class="radio" name="people" value="11-20" id="11-20"></div>
-                    <div class="choice fourth"><label for="21-49">21<span> </span>-<span> </span>49</label><input type="radio" class="radio" name="people" value="21-49" id="21-49"></div>
-                    <div class="choice fifth"><label for="50">50 +</label><input type="radio" class="radio" name="people" value="50" id="50"></div>
-                    <div class="clearfix-dk"></div>
-                  </div>
+            <form class="form">
+              <div class="step one radio-filled">
+                <div class="wrap">
+                  <a href="http://getvoip.com/pdfs/SMBVOIPAdvantagesWhitePaper.pdf"><div class="download"><div class="icon-download"></div><span>Download PDF</span></div></a>
                 </div>
-                <div class="fieldset checkboxes">
-                  <p class="question">What features are important to you?</p>
-                  <div class="wrap top5">
-                    <div class="wrap column left">
-                      <div class="choice-checkbox first"><div class="checkbox-new"></div><label for="mobile">Mobile Capabilities</label><input type="checkbox" class="checkbox" name="features" value="mobile" id="mobile"></div>
-                      <div class="choice-checkbox second"><div class="checkbox-new"></div><label for="fax">Online Fax</label><input type="checkbox" class="checkbox" name="features" value="fax" id="fax"></div>
-                      <div class="choice-checkbox third"><div class="checkbox-new"></div><label for="monitoring">Call Management<span>/Monitoring</span></label><input type="checkbox" class="checkbox" name="features" value="monitoring" id="monitoring"></div>
-                    </div>
-                    <div class="wrap column right">
-                      <div class="choice-checkbox first"><div class="checkbox-new"></div><label for="voicemail">Voicemail-to-email</label><input type="checkbox" class="checkbox" name="features" value="voicemail" id="voicemail"></div>
-                      <div class="choice-checkbox second"><div class="checkbox-new"></div><label for="auto">Auto Attendant</label><input type="checkbox" class="checkbox" name="features" value="auto" id="auto"></div>
-                      <div class="choice-checkbox third"><div class="checkbox-new"></div><label for="none">No Preference</label><input type="checkbox" class="checkbox" name="features" value="none" id="none"></div>
-                    </div>
-                    <div class="clearfix-dk"></div>
-                  </div>
-                </div>
-                <div class="fieldset button-field top10">
-                  <div class="wrap">
-                    <div class="button fixed left back to-step-1">
-                      <p>Back</p>
-                    </div>
-                    <div class="button right next to-step-3">
-                      <p>Find Me Solutions <span class="icon-angle-double-right"></span></p>
-                    </div>
-                    <div class="clearfix-dk"></div>
-                  </div>
-                </div>
+                <p class="thanks"><span>Thank you</span> for requesting a quote. A dedicated VoIP specialist will be calling you very shortly to finalize the quote. <br><br>In the meantime, visit <a href="http://getvoip.com/business">GetVoIP.com</a> to browse featured business phone providers.</p>
               </div>
-            </div>
-            <div class="step three">
-              <div class="wrap">
-                <div class="fieldset input half first firstname">
-                  <p class="question">First Name</p>
-                  <div class="wrap"><input type="text" class="field"></div>
-                </div>
-                <div class="fieldset input half first lastname">
-                  <p class="question">Last Name</p>
-                  <div class="wrap"><input type="text" class="field"></div>
-                </div>
-                <div class="clearfix-dk"></div>
-                <div class="fieldset input half email">
-                  <p class="question">Email Address</p>
-                  <div class="wrap"><input type="text" class="field"></div>
-                </div>
-                <div class="fieldset input half phone">
-                  <p class="question">Phone Number</p>
-                  <div class="wrap"><input type="text" class="field"></div>
-                </div>
-                <div class="clearfix-dk"></div>
-                <div class="fieldset button-field">
-                  <div class="wrap"><input type="submit" value="Show Me Results" class="button"></div>
-                </div>
-                <p class="inform-text">You will also receive a FREE 6-Page business VoIP guide to help analyze and compare your results.</p>
-              </div>
-            </div>
-          </form>
-        </div>
-    
+            </form>
+          </div>
+        <?php endif; ?>
+        
         <div class="feature">
           <div class="wrap">
             <h5>Featured In:</h5>
@@ -275,7 +292,7 @@
             
             
             <!-- Added 14.08.2014 -->
-            <img src='https://trending.revcontent.com/ads/tracker.php?t=MzE5Njs2MjIy'/>
+            <!-- <img src='https://trending.revcontent.com/ads/tracker.php?t=MzE5Njs2MjIy'/> -->
             
             
             <!-- Added 14.08.2014 -->
