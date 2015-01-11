aa35a7096c8aea7403a2b046e5c8a5d0ce3210c0
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-01-12 04:46:38 +0600

    Lp10

--- modules/_GV/gv_blocks/js/gv_requestquote_page_v10.js
+++ modules/_GV/gv_blocks/js/gv_requestquote_page_v10.js
@@ -162,6 +162,12 @@
           $(this).prev().click();
         });
         
+        jQuery('.service-type .choice').click(function(){
+          jQuery(this).parent().find('.choice').removeClass('checked'); 
+          jQuery(this).addClass('checked').find('input').attr('checked',true);
+          console.log(jQuery("input[type='radio'][name='service']:checked").val());
+        });
+        
         $("#requestQuoteFormWrapper .multipartForm").formwizard({ 
 				 	formPluginEnabled: true,
 				 	validationEnabled: true,
