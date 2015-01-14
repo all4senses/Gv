6b75ea9adcc199b068efd7586361568860baed4c
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-01-14 22:04:49 +0600

    lpv10uk

--- modules/_GV/gv_blocks/js/gv_requestquote_page_v10.js
+++ modules/_GV/gv_blocks/js/gv_requestquote_page_v10.js
@@ -315,7 +315,9 @@
                   ========================================================================== */
 
                console.log('turn off: ' + Drupal.settings['gv_turn_on_phone_input_mask']);
-               //$('.fieldset.phone input').inputmask("mask", {"mask": "(999) 999-9999"});
+               if (Drupal.settings['gv_turn_on_phone_input_mask'] == 'undefined' || Drupal.settings['gv_turn_on_phone_input_mask'] == true) {
+                  $('.fieldset.phone input').inputmask("mask", {"mask": "(999) 999-9999"});
+               }
 
                /* ========================================================================== */
             }
