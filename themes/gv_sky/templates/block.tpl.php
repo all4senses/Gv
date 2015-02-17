23939b6b48925f58d7435fc855f25cf3a811ebb5
YOUR NAME <gazelle@Daniels-iMac.home>
2015-02-17 15:03:22 -0500

    Block global edit
    Removed ‘.top-line’

--- themes/gv_sky/templates/block.tpl.php
+++ themes/gv_sky/templates/block.tpl.php
@@ -1,7 +1,4 @@
 
-<?php if (!strpos($classes, 'first')): ?>
-  <div class="top-line <?php print $block_html_id; ?>"></div>
-<?php endif; ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
