6943b7785edb2b5c03998e48e8927dbddc1d947d
YOUR NAME <gazelle@Daniels-iMac.home>
2015-02-18 19:21:51 -0500

    Header and hero progress

--- themes/gv_sky/js/global.js
+++ themes/gv_sky/js/global.js
@@ -1,29 +1,26 @@
 !function(n,r){var $={};$$=function(u){var t=$[u];return t!==r?t:$[u]=n(u)},$$.clear=function(n){$[n]=r},$$.fresh=function(n){return $[n]=r,$$(n)}}(jQuery);
 
-jQuery(document).ready(function($){
+// function($){
 	var $window = $$(window);
 	var $this = $$(this);
 
 	$$('.solution-nav').insertAfter('.navigation');
 
-	$$('.main-menu-item.first').click(function(){
-		if (!$$('.solution-nav').hasClass('open')) {
-			$$('.solution-nav').addClass('open');
+	$$('.main-menu-item.first').on('click',function(){
+
+		var clicks = $this.data('clicks');
+
+		if (clicks) {
+			$$('.solution-nav').removeClass('open');
 		} else {
-			$$('.solution-nav').addClass('close');
-		}
+			$$('.solution-nav').addClass('open');
+		} 
+
+		$this.data('clicks', !clicks);
 	});
 
+	$$('.hero-header').on('click',function(){
+			$$('.solution-nav').removeClass('open');
+	});
 
-
-
-
-
-
-
-
-
-
-
-
-});
+// }
