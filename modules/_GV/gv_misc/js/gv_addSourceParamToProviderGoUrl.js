09826a36b554f2ec1093ce8741ca1c81c4ef5e7c
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-02-26 23:47:48 +0600

    Track click issue

--- modules/_GV/gv_misc/js/gv_addSourceParamToProviderGoUrl.js
+++ modules/_GV/gv_misc/js/gv_addSourceParamToProviderGoUrl.js
@@ -3,15 +3,15 @@
   Drupal.behaviors.gv_addSourceParamToProviderGoUrl = {
     attach: function (context, settings) {
        
-       console.log(Drupal.settings['gv_misc']['addParamToProviderUrl']['uid']);
+       //console.log(Drupal.settings['gv_misc']['addParamToProviderUrl']['uid']);
        
        
        $(".visit-site-btn").click(function(){
          
-         console.log('click');
-         console.log('href = ' + $(this).attr('href'));
+//         console.log('click');
+//         console.log('href = ' + $(this).attr('href'));
          if(!$(this).attr('href').split('from=')[1]) {
-           $(this).attr('href', $(this).attr('href') + '?from=' + window.location.href);
+           $(this).attr('href', $(this).attr('href') + '?from=' + window.location.href + '&ref=' + document.referrer);
          }
          //console.log('title = ' + $(this).attr('title'));
          
