6eb4cba213e657536f1c939e4b22f98f570b300c
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-03-03 05:46:56 +0600

    Quote requests list page improve

---
+++ modules/_GV/gv_pages/js/gv_quoteRequestsAdminPage_extraFilters.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+  Drupal.behaviors.gv_quoteRequestsAdminPage_extraFilters = {
+    attach: function (context, settings) {
+       
+       //console.log(Drupal.settings['gv_misc']['addParamToProviderUrl']['uid']);
+       
+       
+       $(".extra-filters .apply").click(function(){
+         
+//         console.log('click');
+         //console.log('href = ' + $(this).attr('href'));
+         
+//         if(!$(this).attr('href').split('from=')[1]) {
+//           $(this).attr('href', $(this).attr('href') + '?from=' + window.location.href + '&ref=' + document.referrer);
+//         }
+          
+         console.log(window.location);
+          
+         //console.log('href = ' + $(this).attr('href'));
+         //console.log('title = ' + $(this).attr('title'));
+   
+       });
+
+       
+    }
+  };
+
+}(jQuery));
