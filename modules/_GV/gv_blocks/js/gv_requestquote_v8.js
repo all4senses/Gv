2b36a06dbc1ff18472a212786044aa56c20d0df9
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-03-04 00:20:27 +0600

    Quote requests list page improve

--- modules/_GV/gv_blocks/js/gv_requestquote_v8.js
+++ modules/_GV/gv_blocks/js/gv_requestquote_v8.js
@@ -115,6 +115,12 @@
             }
 
         });
+        
+        
+        //console.log('turn off: ' + Drupal.settings['gv_turn_on_phone_input_mask']);
+        if (typeof Drupal.settings['gv_turn_on_phone_input_mask'] === "undefined" || Drupal.settings['gv_turn_on_phone_input_mask'] == true) {
+           $('.fieldset.phone input').inputmask("mask", {"mask": "(999) 999-9999"});
+        }
 
         jQuery.validator.addMethod("notEqualsTo", function(value, element, param) {
           return !(this.optional(element) || value === param);
