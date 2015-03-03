6ac80837a32ef8522fbd182767e4bfb92994a60e
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-03-03 13:37:34 +0600

    Quote requests list page improve

--- modules/_GV/gv_pages/js/gv_quoteRequestsAdminPage_extraFilters.js
+++ modules/_GV/gv_pages/js/gv_quoteRequestsAdminPage_extraFilters.js
@@ -15,6 +15,16 @@
 //           $(this).attr('href', $(this).attr('href') + '?from=' + window.location.href + '&ref=' + document.referrer);
 //         }
           
+          // if current url already has a query, add to the query
+         emailContains = jQuery(".extra-filters .email-contains input").val();
+         campaignContains = ''; //jQuery(".extra-filters .campaign-contains input").val();
+         if(window.location.href.split('?')[1]) {
+          hrefToGo = window.location.href + (emailContains ? '&email-contains=' + emailContains : '') + (campaignContains ? '&campaign-contains=' + campaignContains : '');
+         }
+         else {
+           hrefToGo = window.location.href + (emailContains ? '?email-contains=' + emailContains : '') + (emailContains && campaignContains ? '&campaign-contains=' + campaignContains : (!emailContains && campaignContains ? '?campaign-contains=' + campaignContains : ''));
+         }
+         console.log('hrefToGo = ' + hrefToGo);
          console.log(window.location);
           
          //console.log('href = ' + $(this).attr('href'));
