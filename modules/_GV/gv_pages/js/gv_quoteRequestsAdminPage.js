53db2e35ab3ab200ff131234ffb548f24a06a095
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-03-06 01:47:46 +0600

    Quote requests list page improve

--- modules/_GV/gv_pages/js/gv_quoteRequestsAdminPage.js
+++ modules/_GV/gv_pages/js/gv_quoteRequestsAdminPage.js
@@ -6,7 +6,7 @@
        //console.log(Drupal.settings['gv_misc']['addParamToProviderUrl']['uid']);
        
        
-       $(".extra-filters .apply").click(function(){
+       $("select.campaign_name").click(function(){
          
 //         console.log('click');
          //console.log('href = ' + $(this).attr('href'));
@@ -16,16 +16,17 @@
 //         }
           
           // if current url already has a query, add to the query
-//         emailContains = jQuery(".extra-filters .email-contains input").val();
-//         campaignContains = jQuery(".extra-filters .campaign-contains input").val();
-//         if(window.location.href.split('?')[1]) {
-//          hrefToGo = window.location.href + (emailContains ? '&email-contains=' + emailContains : '') + (campaignContains ? '&campaign-contains=' + campaignContains : '');
-//         }
-//         else {
-//           hrefToGo = window.location.href + (emailContains ? '?email-contains=' + emailContains : '') + (emailContains && campaignContains ? '&campaign-contains=' + campaignContains : (!emailContains && campaignContains ? '?campaign-contains=' + campaignContains : ''));
-//         }
-//         console.log('hrefToGo = ' + hrefToGo);
-//         console.log(window.location);
+         //emailContains = jQuery(".extra-filters .email-contains input").val();
+         //campaignContains = jQuery(".extra-filters .campaign-contains input").val();
+         if(window.location.href.split('?')[1]) {
+          hrefToGo = window.location.href + '&op=campaign-name-set&ts=';// + $(this);
+         }
+         else {
+           hrefToGo = window.location.href + '?op=campaign-name-set&ts=';// + $(this);
+         }
+         console.log($(this));
+         console.log('hrefToGo = ' + hrefToGo);
+         console.log(window.location);
           
          //console.log('href = ' + $(this).attr('href'));
          //console.log('title = ' + $(this).attr('title'));
