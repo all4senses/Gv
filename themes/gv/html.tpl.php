adc16a3194a5de296341b4ebcc81e4989ce9ee61
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-03-13 23:38:01 +0600

    Remarketing code for all pages

--- themes/gv/html.tpl.php
+++ themes/gv/html.tpl.php
@@ -49,8 +49,8 @@
       var google_custom_params = window.google_tag_params;
       var google_remarketing_only = true;
       /* ]]> */
-      </script>
-      <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+  </script>
+  <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
   </script>
   <noscript>
       <div style="display:inline;">
