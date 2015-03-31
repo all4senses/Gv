8d706f3e45799c0483260b8a093fcf335aea70ea
YOUR NAME <gazelle@Daniels-iMac.home>
2015-03-31 17:42:47 -0400

    Bug Fix - Chart Page Issues with mobile version

--- themes/gv_sky/templates/node--quote--v10_uk.tpl.php
+++ themes/gv_sky/templates/node--quote--v10_uk.tpl.php
@@ -39,7 +39,7 @@
         <div class="wrap">
 
           <div class="logo">
-            <img src="/sites/all/themes/gv_blue/css/sass/lpv10-img/logo.png" width="210" height="58" alt="GetVoIP" title="GetVoIP">
+            <img src="/sites/all/themes/gv_sky/images/lpv10/logo.png" width="210" height="58" alt="GetVoIP" title="GetVoIP">
           </div>
 
           <div class="slogan-wrap">
@@ -109,7 +109,7 @@
                 </div>
                 <div class="person">
                   <div class="photo">
-                    <div class="circle"><img src="/sites/all/themes/gv_blue/css/sass/lpv10-img/richard.png" alt="Richard Janes"></div>
+                    <div class="circle"><img src="/sites/all/themes/gv_sky/images/lpv10/richard.png" alt="Richard Janes"></div>
                   </div>
                   <div class="name-meta">
                     <p class="full-name">Richard Janes</p>
@@ -125,7 +125,7 @@
                 </div>
                 <div class="person">
                   <div class="photo">
-                    <div class="circle"><img src="/sites/all/themes/gv_blue/css/sass/lpv10-img/mark.png" alt="Mark Crosbie"></div>
+                    <div class="circle"><img src="/sites/all/themes/gv_sky/images/lpv10/mark.png" alt="Mark Crosbie"></div>
                   </div>
                   <div class="name-meta">
                     <p class="full-name">Mark Crosbie</p>
