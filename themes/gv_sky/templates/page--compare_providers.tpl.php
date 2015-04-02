4bd8c2c98515679fd876a0f81ffbaf35797b5615
YOUR NAME <gazelle@Daniels-iMac.home>
2015-04-02 15:15:17 -0400

    hero content fix

--- themes/gv_sky/templates/page--compare_providers.tpl.php
+++ themes/gv_sky/templates/page--compare_providers.tpl.php
@@ -36,13 +36,11 @@
     </nav> 
       <div class="hero-header left">
         <div class="hero-title">
-          <h1 class="hero-title-heading"><?php echo (NULL !== $node->field_hero_title['und'][0]['value'] ? $node->field_hero_title['und'][0]['value'] : render($title)); ?></h1>
-          <p class="hero-title-desc"><?php  echo (NULL !== $node->field_hero_subtitle['und'][0]['value'] ? $node->field_hero_subtitle['und'][0]['value'] : field_get_items('node',$node, 'body')[0]['value']); ?></p>
+          <h1 class="hero-title-heading"><?php echo (isset($node->field_hero_title['und'][0]['value']) ? $node->field_hero_title['und'][0]['value'] : render($title)); ?></h1>
+          <p class="hero-title-desc"><?php  echo (isset($node->field_hero_subtitle['und'][0]['value']) ? $node->field_hero_subtitle['und'][0]['value'] : field_get_items('node',$node, 'body')[0]['value']); ?></p>
         </div>
       </div>
   </header>
-<?php //echo $node->field_extra_data['und'][0]['value'] ?>
-<?php if (NULL !== $node->field_extra_data['und'][0]['value']) {echo 'null';} ?>
   <main role="main" id="main">
     <div class="main-wrap">
        
