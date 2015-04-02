d6097fc1cc21284b595dbbc36cc282f5b072e834
YOUR NAME <gazelle@Daniels-iMac.home>
2015-04-02 16:51:51 -0400

    Added block wrap for admins

--- themes/gv_sky/templates/block.tpl.php
+++ themes/gv_sky/templates/block.tpl.php
@@ -1,5 +1,9 @@
-<?php print render($title_suffix); ?>
-<?php print $content ?>
-  
-  
+<?php if ($is_admin){ ?>
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<?php } ?>
+	<?php print render($title_suffix); ?>
+	<?php print $content ?>
+<?php if ($is_admin){ ?>
+	</div> <!-- /.block -->
+<?php } ?>  
 
