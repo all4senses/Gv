a7e3e646907705f32ca9a6d9f208815ef2ab1389
YOUR NAME <gazelle@Daniels-iMac.home>
2015-04-03 17:24:46 -0400

    styling push

--- themes/gv_sky/html.tpl.php
+++ themes/gv_sky/html.tpl.php
@@ -18,39 +18,39 @@
         if ($args[0] == 'node' && (@$args[1] == 'add' || isset($args[2]) && $args[2] == 'edit')) { $top = TRUE; print $scripts; } 
   ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-  <link rel="stylesheet" href="sites/all/themes/gv_sky/css/global.css">
+  <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/global.css">
   <?php if ($is_front) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/home.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/home.css">
   <?php } ?>
   <?php if ( current_path() == 'blog' ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/blog.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/blog.css">
   <?php } ?>
   <?php if ( strpos(request_path(), 'blog/') !== FALSE || strpos(request_path(), 'library/') !== FALSE ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/blog.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/blog.css">
   <?php } ?>
   <?php if ( current_path() == 'node/add/review' ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/submit-review.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/submit-review.css">
   <?php } ?>
   <?php if ($is_admin) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/admin.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/admin.css">
   <?php } ?>
   <?php if ( strpos(request_path(), 'reviews/phone') !== FALSE ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/review.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/review.css">
   <?php } ?>
   <?php if ( current_path() == 'node/38' ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/about.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/about.css">
   <?php } ?>
   <?php if ( strpos(current_path(), 'user/') !== FALSE ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/team-member.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/team-member.css">
   <?php } ?>
   <?php if ( current_path() == 'node/62' ||  current_path() == 'node/91' ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/contact.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/contact.css">
   <?php } ?>
   <?php if ( strpos(request_path(), 'reviews') !== FALSE && strpos(request_path(), 'reviews/phone') === FALSE ) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/provider.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/provider.css">
   <?php } ?>
   <?php if ($is_front) {?>
-    <link rel="stylesheet" href="sites/all/themes/gv_sky/css/home.css">
+    <link rel="stylesheet" href="/sites/all/themes/gv_sky/css/home.css">
   <?php } ?>
 <!--[if IE 8]>
     <script type="text/javascript" src="/sites/all/themes/gv_sky/js/selectivizr-min.js"></script>
