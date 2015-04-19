ed2c2a3caacd91e1b9152eb00b07d72b915d8919
YOUR NAME <gazelle@Daniels-iMac.home>
2015-04-19 11:00:59 -0400

    Submit Review main menu fix

--- themes/gv_sky/js/submit-review.js
+++ themes/gv_sky/js/submit-review.js
@@ -4,27 +4,27 @@ jQuery(document).ready(function($){
 	!function(n,r){var $={};$$=function(u){var t=$[u];return t!==r?t:$[u]=n(u)},$$.clear=function(n){$[n]=r},$$.fresh=function(n){return $[n]=r,$$(n)}}(jQuery);
 
 
-	$$('.solution-nav').insertAfter('.navigation');
+	// $$('.solution-nav').insertAfter('.navigation');
 
-	$$('.main-menu-item.first').on('click',function(){
+	// $$('.main-menu-item.first').on('click',function(){
 
-		var clicks = $this.data('clicks');
+	// 	var clicks = $this.data('clicks');
 
-		if (clicks) {
-			$$('.solution-nav').removeClass('open');
-		} else {
-			$$('.solution-nav').addClass('open');
-		} 
+	// 	if (clicks) {
+	// 		$$('.solution-nav').removeClass('open');
+	// 	} else {
+	// 		$$('.solution-nav').addClass('open');
+	// 	} 
 
-		$this.data('clicks', !clicks);
-	});
+	// 	$this.data('clicks', !clicks);
+	// });
 
-	$$('.hero-header').on('click, touchstart',function(){
-		var clicks = $$('.main-menu-item.first').data('clicks');
+	// $$('.hero-header').on('click, touchstart',function(){
+	// 	var clicks = $$('.main-menu-item.first').data('clicks');
 
-		$$('.solution-nav').removeClass('open');
-		$$('.main-menu-item.first').data('clicks', !clicks);
-	});
+	// 	$$('.solution-nav').removeClass('open');
+	// 	$$('.main-menu-item.first').data('clicks', !clicks);
+	// });
 
 
 
