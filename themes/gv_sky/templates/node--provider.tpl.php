22e153e8fc22b0b473274fbf58e809006c3779db
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-05-07 23:58:58 +0600

    Rich snippet for a Provider page

--- themes/gv_sky/templates/node--provider.tpl.php
+++ themes/gv_sky/templates/node--provider.tpl.php
@@ -107,8 +107,8 @@
             </div>
 
               
-            <div class="provider-box-provider-details"<?php// rel="v:itemreviewed"?>>
-              <div<?php// typeof="Organization"?>>
+            <div class="provider-box-provider-details"<?php // rel="v:itemreviewed" ?>>
+              <div<?php // typeof="Organization" ?>>
                 <?php 
                     echo '<h1 class="provider-box-provider-details-name" property="v:summary">' . $provider_name . '</h1>';
                 ?>                
@@ -187,11 +187,11 @@
                         <div class="provider-box-provider-review-score-caption">Overall</div>
                         <!--<div class="provider-box-provider-review-score-number" rel="v:rating">-->
                         <div class="provider-box-provider-review-score-number" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
-                          <span<?php//typeof="v:Rating"?>>
-                            <meta<?php//property="v:worst"?>  itemprop="worstValue" content="0"/>
-                            <span<?php//property="v:value"?> itemprop="ratingValue"> <?php echo $node->gv_rating_overall; ?> </span>
-                            <meta<?php//property="v:best"?> itemprop="bestRating" content="5"/>
-                            <meta itemprop="ratingCount" content="<?php echo @$node->gv_voters;?>"/>
+                          <span<?php //typeof="v:Rating" ?>>
+                            <meta<?php //property="v:worst" ?>  itemprop="worstValue" content="0"/>
+                            <span<?php //property="v:value" ?> itemprop="ratingValue"> <?php echo $node->gv_rating_overall; ?> </span>
+                            <meta<?php //property="v:best" ?> itemprop="bestRating" content="5"/>
+                            <meta itemprop="ratingCount" content="<?php echo @$node->gv_voters; ?>"/>
                           </span>
                         </div>
                         <div class="provider-box-provider-review-score-fivestar"> <?php echo theme('gv_misc_fivestar_provider', array('rating' => $node->gv_rating_overall * 20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css'))); ?>
