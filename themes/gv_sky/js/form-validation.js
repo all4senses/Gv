1b6897c3cbec1127f720a39d3e5dbf23531a8194
YOUR NAME <gazelle@Daniels-iMac.home>
2015-05-14 15:39:31 -0400

    Newsletter markup, styling and logic.

--- themes/gv_sky/js/form-validation.js
+++ themes/gv_sky/js/form-validation.js
@@ -6,10 +6,10 @@
 		var back = false;
 		var submit = false;
 		
-		if ( $arg == "prepare" ) {var prepare = true;} 
-		if ( $arg == "next" ) {var next = true;} 
-		if ( $arg == "back" ) {var back = true;} 
-		if ( $arg == "submit" ) {var submit = true;} 
+		if ( $arg === "prepare" ) {var prepare = true;} 
+		if ( $arg === "next" ) {var next = true;} 
+		if ( $arg === "back" ) {var back = true;} 
+		if ( $arg === "submit" ) {var submit = true;} 
 
 
 		
@@ -20,7 +20,7 @@
 
 				if ( $(this).hasClass('required') ) {
 					$(this).find('.input').not('.phone, .email').on('change keydown', function(){
-						if ( $(this).val() == "" || $(this).val() == null) {
+						if ( $(this).val() === "" || $(this).val() === null) {
 							$(this).parents('.fieldset').removeClass('valid');
 						} else{
 							$(this).parents('.fieldset').addClass('valid').removeClass('error');
@@ -62,25 +62,25 @@
 
 			this.find('.phone').each(function(){
 				$(this).keydown(function (event) {
-		            if( !(     event.keyCode == 8                                // backspace
-		                    || event.keyCode == 9 								// tab
-		                    || event.keyCode == 16 								// shift
-		                    || event.keyCode == 17 								// ctrl
-		                    || event.keyCode == 18 								// alt
-		                    || event.keyCode == 46                              // delete
+		            if( !(     event.keyCode === 8                                // backspace
+		                    || event.keyCode === 9 								// tab
+		                    || event.keyCode === 16 								// shift
+		                    || event.keyCode === 17 								// ctrl
+		                    || event.keyCode === 18 								// alt
+		                    || event.keyCode === 46                              // delete
 		                    || (event.keyCode >= 35 && event.keyCode <= 40)     // arrow keys/home/end
 
 		                    || (event.keyCode >= 48 && event.keyCode <= 57)     // numbers on keyboard
 		                    || (event.keyCode >= 96 && event.keyCode <= 105)    // number on keypad
 		                  
-		                    || (event.keyCode == 32 || event.keyCode == 189 || event.keyCode == 190 || event.keyCode == 173)    // space, dash, dot
+		                    || (event.keyCode === 32 || event.keyCode === 189 || event.keyCode === 190 || event.keyCode === 173)    // space, dash, dot
 		                 )   
 		              ) {
 		                    event.preventDefault();     // Prevent character input
 			            }
 				});
 				$(this).change(function(){
-					if ( $(this).val() == "" || $(this).val().length < 7 ) {
+					if ( $(this).val() === "" || $(this).val().length < 7 ) {
 						$(this).parents('.fieldset').removeClass('valid');
 					} else {
 						$(this).parents('.fieldset').addClass('valid').removeClass('error');
@@ -92,7 +92,7 @@
 				$(this).on('change keydown', function(){
 				    var email = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 
-					if ( $(this).val() == "" || !email.test($(this).val()) ) {
+					if ( $(this).val() === "" || !email.test($(this).val()) ) {
 						$(this).parents('.fieldset').removeClass('valid');
 					} else {
 						$(this).parents('.fieldset').addClass('valid').removeClass('error');
