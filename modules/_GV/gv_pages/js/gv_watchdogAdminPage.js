d04845d26f92aa388bfbcb16ec59da37b87811cb
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-06-26 03:33:40 +0600

    Spam/attacks work around

--- modules/_GV/gv_pages/js/gv_watchdogAdminPage.js
+++ modules/_GV/gv_pages/js/gv_watchdogAdminPage.js
@@ -4,13 +4,18 @@
     attach: function (context, settings) {
        
        
-       $(".wd-links a").click(function(){
+       jQuery(".wd-links a").click(function(){
          
-         if (confirm("Are You sure to " + $(this).title + "?")) {
+         if (confirm("Are You sure to " + jQuery(this).title + "?")) {
            
-           console.log($(this).title);
+           console.log(jQuery(this)[0].title);
+           console.log(jQuery(this)[0].ip);
+           console.log(jQuery(this)[0].id);
+           console.log(jQuery(this)[0].class);
            
          } // End of if (confirm("Are You sure to " + $(this).title + "?")) {
+         
+         return false;
          
        }); // End of $(".wd-links a").click(function(){
        
