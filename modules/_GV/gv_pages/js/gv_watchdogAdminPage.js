5759112c3adefb9430b195340826385b6dc08c99
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-06-26 04:25:09 +0600

    Spam/attacks work around

--- modules/_GV/gv_pages/js/gv_watchdogAdminPage.js
+++ modules/_GV/gv_pages/js/gv_watchdogAdminPage.js
@@ -6,12 +6,17 @@
        
        jQuery(".wd-links a").click(function(){
          
-         if (confirm("Are You sure to " + jQuery(this).title + "?")) {
+         if (confirm("Are You sure to " + jQuery(this)[0].title + "?")) 
+         {
            
-           console.log(jQuery(this)[0].title);
-           console.log(jQuery(this)[0].ip);
-           console.log(jQuery(this)[0].id);
-           console.log(jQuery(this)[0].class);
+           //console.log(jQuery(this)[0].title);
+           //console.log(jQuery(this).parent()[0].attributes.ip.value);
+           //console.log(jQuery(this).parent()[0].id);
+           //console.log(jQuery(this)[0].className);
+           
+           concat_symbol = (window.location.href.indexOf("?") > 0) ? '&' : '?';
+           hrefToGo = window.location.href + concat_symbol + 'op=' + jQuery(this)[0].className + '&id=' + jQuery(this).parent()[0].id + '&ip=' + jQuery(this).parent()[0].attributes.ip.value;
+           //console.log(hrefToGo);
            
          } // End of if (confirm("Are You sure to " + $(this).title + "?")) {
          
